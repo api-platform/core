171e1bf3e3b05989276940577c1f7bca80a48498
Kévin Dunglas <dunglas@gmail.com>
2015-03-19 17:52:05 +0100

    Optimize Doctrine pagination

--- Doctrine/Orm/DataManipulator.php
+++ Doctrine/Orm/DataManipulator.php
@@ -121,7 +121,7 @@ class DataManipulator implements DataManipulatorInterface
             $queryBuilder->addOrderBy('o.id', $order);
         }
 
-        return new Paginator($queryBuilder);
+        return new Paginator($queryBuilder, false);
     }
 
     /**
