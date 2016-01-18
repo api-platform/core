1de9316e12a4c14cb3a3d40486e4e9cc04839c87
Kévin Dunglas <dunglas@gmail.com>
2016-01-18 16:28:20 +0100

    Fix CS

--- Api/IriConverter.php
+++ Api/IriConverter.php
@@ -142,7 +142,7 @@ class IriConverter implements IriConverterInterface
         }
 
         if ($resource instanceof RoutedResourceInterface) {
-            $method    = 'get' . ucfirst($key);
+            $method    = 'get'.ucfirst($key);
             $routeName = $resource->$method();
             if ($routeName) {
                 $data                        = $this->routeCache[$resource];
