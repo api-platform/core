8bf5f73eb745d24d4834cfac79fc16c042222928
Sam Van der Borght <sam@king-foo.be>
2016-03-16 11:04:44 +0100

    Fix pagination extension

--- src/Bridge/Doctrine/MongoDB/Extension/PaginationExtension.php
+++ src/Bridge/Doctrine/MongoDB/Extension/PaginationExtension.php
@@ -11,8 +11,8 @@
 
 namespace ApiPlatform\Core\Bridge\Doctrine\MongoDB\Extension;
 
-use ApiPlatform\Core\Metadata\Resource\Factory\ItemMetadataFactoryInterface;
-use ApiPlatform\Core\Metadata\Resource\ItemMetadata;
+use ApiPlatform\Core\Metadata\Resource\Factory\ResourceMetadataFactoryInterface;
+use ApiPlatform\Core\Metadata\Resource\ResourceMetadata;
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\ODM\MongoDB\Query\Builder;
 use Symfony\Component\HttpFoundation\Request;
@@ -29,7 +29,7 @@ class PaginationExtension implements QueryResultExtensionInterface
 {
     private $managerRegistry;
     private $requestStack;
-    private $itemMetadataFactory;
+    private $resourceMetadataFactory;
     private $enabled;
     private $clientEnabled;
     private $clientItemsPerPage;
@@ -38,11 +38,11 @@ class PaginationExtension implements QueryResultExtensionInterface
     private $enabledParameterName;
     private $itemsPerPageParameterName;
 
-    public function __construct(ManagerRegistry $managerRegistry, RequestStack $requestStack, ItemMetadataFactoryInterface $itemMetadataFactory, bool $enabled = true, bool $clientEnabled = false, bool $clientItemsPerPage = false, int $itemsPerPage = 30, string $pageParameterName = 'page', string $enabledParameterName = 'pagination', string $itemsPerPageParameterName = 'itemsPerPage')
+    public function __construct(ManagerRegistry $managerRegistry, RequestStack $requestStack, ResourceMetadataFactoryInterface $resourceMetadataFactory, bool $enabled = true, bool $clientEnabled = false, bool $clientItemsPerPage = false, int $itemsPerPage = 30, string $pageParameterName = 'page', string $enabledParameterName = 'pagination', string $itemsPerPageParameterName = 'itemsPerPage')
     {
         $this->managerRegistry = $managerRegistry;
         $this->requestStack = $requestStack;
-        $this->itemMetadataFactory = $itemMetadataFactory;
+        $this->resourceMetadataFactory = $resourceMetadataFactory;
         $this->enabled = $enabled;
         $this->clientEnabled = $clientEnabled;
         $this->clientItemsPerPage = $clientItemsPerPage;
@@ -62,13 +62,13 @@ class PaginationExtension implements QueryResultExtensionInterface
             return;
         }
 
-        $itemMetadata = $this->itemMetadataFactory->create($resourceClass);
-        if (!$this->isPaginationEnabled($request, $itemMetadata, $operationName)) {
+        $resourceMetadata = $this->resourceMetadataFactory->create($resourceClass);
+        if (!$this->isPaginationEnabled($request, $resourceMetadata, $operationName)) {
             return;
         }
 
-        $itemsPerPage = $itemMetadata->getCollectionOperationAttribute($operationName, 'pagination_items_per_page', $this->itemsPerPage, true);
-        if ($itemMetadata->getCollectionOperationAttribute($operationName, 'pagination_client_items_per_page', $this->clientItemsPerPage, true)) {
+        $itemsPerPage = $resourceMetadata->getCollectionOperationAttribute($operationName, 'pagination_items_per_page', $this->itemsPerPage, true);
+        if ($resourceMetadata->getCollectionOperationAttribute($operationName, 'pagination_client_items_per_page', $this->clientItemsPerPage, true)) {
             $itemsPerPage = (int) $request->query->get($this->itemsPerPageParameterName, $itemsPerPage);
         }
 
@@ -88,9 +88,9 @@ class PaginationExtension implements QueryResultExtensionInterface
             return false;
         }
 
-        $itemMetadata = $this->itemMetadataFactory->create($resourceClass);
+        $resourceMetadata = $this->resourceMetadataFactory->create($resourceClass);
 
-        return $this->isPaginationEnabled($request, $itemMetadata, $operationName);
+        return $this->isPaginationEnabled($request, $resourceMetadata, $operationName);
     }
 
     /**
@@ -101,49 +101,15 @@ class PaginationExtension implements QueryResultExtensionInterface
         return new Paginator($queryBuilder->getQuery()->execute());
     }
 
-    private function isPaginationEnabled(Request $request, ItemMetadata $itemMetadata, string $operationName = null) : bool
+    private function isPaginationEnabled(Request $request, ResourceMetadata $resourceMetadata, string $operationName = null) : bool
     {
-        $enabled = $itemMetadata->getCollectionOperationAttribute($operationName, 'pagination_enabled', $this->enabled, true);
-        $clientEnabled = $itemMetadata->getCollectionOperationAttribute($operationName, 'pagination_client_enabled', $this->clientEnabled, true);
+        $enabled = $resourceMetadata->getCollectionOperationAttribute($operationName, 'pagination_enabled', $this->enabled, true);
+        $clientEnabled = $resourceMetadata->getCollectionOperationAttribute($operationName, 'pagination_client_enabled', $this->clientEnabled, true);
 
         if ($clientEnabled) {
             $enabled = filter_var($request->query->get($this->enabledParameterName, $enabled), FILTER_VALIDATE_BOOLEAN);
         }
 
         return $enabled;
-    }
-
-    /**
-     * Determines whether output walkers should be used.
-     *
-     * @param Builder $queryBuilder
-     *
-     * @return bool
-     */
-    private function useOutputWalkers(Builder $queryBuilder) : bool
-    {
-        /*
-         * "Paginating an entity with foreign key as identifier only works when using the Output Walkers. Call Paginator#setUseOutputWalkers(true) before iterating the paginator."
-         *
-         * @see https://github.com/doctrine/doctrine2/blob/900b55d16afdcdeb5100d435a7166d3a425b9873/lib/Doctrine/ORM/Tools/Pagination/LimitSubqueryWalker.php#L87
-         */
-//        if (QueryChecker::hasRootEntityWithForeignKeyIdentifier($queryBuilder, $this->managerRegistry)) {
-//            return true;
-//        }
-
-        /*
-         * "Cannot select distinct identifiers from query with LIMIT and ORDER BY on a column from a fetch joined to-many association. Use output walkers."
-         *
-         * @see https://github.com/doctrine/doctrine2/blob/900b55d16afdcdeb5100d435a7166d3a425b9873/lib/Doctrine/ORM/Tools/Pagination/LimitSubqueryWalker.php#L149
-         */
-//        if (
-//            QueryChecker::hasMaxResults($queryBuilder) &&
-//            QueryChecker::hasOrderByOnToManyJoin($queryBuilder, $this->managerRegistry)
-//        ) {
-//            return true;
-//        }
-
-        // Disable output walkers by default (performance)
-        return false;
     }
 }
