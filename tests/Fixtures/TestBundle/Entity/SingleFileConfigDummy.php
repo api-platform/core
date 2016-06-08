4245ae87d66b4779ed255b5469ff2a2ec2a97aa3
Antoine Bluchet <soyuka@users.noreply.github.com>
2016-06-08 23:04:12 +0200

    Configuration filename to api_resources and allow single file declaration

---
+++ tests/Fixtures/TestBundle/Entity/SingleFileConfigDummy.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * FileConfigDummy.
+ *
+ * @ORM\Entity
+ */
+class SingleFileConfigDummy
+{
+    /**
+     * @var int The id.
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string The dummy name.
+     *
+     * @ORM\Column
+     */
+    private $name;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
