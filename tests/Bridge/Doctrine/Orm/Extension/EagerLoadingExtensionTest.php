a05cd31f34d6200f8c16c7d5478a54a32fac9280
Amrouche Hamza <hamza@les-tilleuls.coop>
2016-08-01 07:51:51 +0200

    feat: add test to doctrine extention namespace

---
+++ tests/Bridge/Doctrine/Orm/Extension/EagerLoadingExtensionTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ApiPlatform\Core\Tests\Doctrine\Orm\Extension;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\EagerLoadingExtension;
+use ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\Dummy;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\QueryBuilder;
+
+/**
+ * @author Amrouche Hamza <hamza.simperfit@gmail.com>
+ */
+class EagerLoadingExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testApplyToCollection()
+    {
+        $queryBuilderProphecy = $this->prophesize(QueryBuilder::class);
+
+        $classMetadataProphecy = $this->prophesize(ClassMetadata::class);
+        $classMetadataProphecy->getAssociationNames()->shouldBeCalled()->willReturn([0 => 'dummyRelation']);
+        $classMetadataProphecy->associationMappings = ['dummyRelation' => ['fetch' => 3]];
+        $emProphecy = $this->prophesize(EntityManager::class);
+        $emProphecy->getClassMetadata(Dummy::class)->shouldBeCalled()->willReturn($classMetadataProphecy->reveal());
+        $queryBuilderProphecy->leftJoin('o.dummyRelation', 'a0')->shouldBeCalled(1);
+        $queryBuilderProphecy->addSelect('a0')->shouldBeCalled(1);
+
+        $em = $queryBuilderProphecy->getEntityManager()->shouldBeCalled(1)->willReturn($emProphecy->reveal());
+
+        $queryBuilder = $queryBuilderProphecy->reveal();
+        $orderExtensionTest = new EagerLoadingExtension();
+        $orderExtensionTest->applyToCollection($queryBuilder, Dummy::class);
+    }
+
+    public function testApplyToItem()
+    {
+        $queryBuilderProphecy = $this->prophesize(QueryBuilder::class);
+
+        $classMetadataProphecy = $this->prophesize(ClassMetadata::class);
+        $classMetadataProphecy->getAssociationNames()->shouldBeCalled()->willReturn([0 => 'dummyRelation']);
+        $classMetadataProphecy->associationMappings = ['dummyRelation' => ['fetch' => 3]];
+        $emProphecy = $this->prophesize(EntityManager::class);
+        $emProphecy->getClassMetadata(Dummy::class)->shouldBeCalled()->willReturn($classMetadataProphecy->reveal());
+        $queryBuilderProphecy->leftJoin('o.dummyRelation', 'a0')->shouldBeCalled(1);
+        $queryBuilderProphecy->addSelect('a0')->shouldBeCalled(1);
+
+        $em = $queryBuilderProphecy->getEntityManager()->shouldBeCalled(1)->willReturn($emProphecy->reveal());
+
+        $queryBuilder = $queryBuilderProphecy->reveal();
+        $orderExtensionTest = new EagerLoadingExtension();
+        $orderExtensionTest->applyToItem($queryBuilder, Dummy::class, []);
+    }
+}
---
+++ tests/Bridge/Doctrine/Orm/Extension/OrderExtensionTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ApiPlatform\Core\Tests\Doctrine\Orm\Extension;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\OrderExtension;
+use ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\Dummy;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\QueryBuilder;
+
+/**
+ * @author Amrouche Hamza <hamza.simperfit@gmail.com>
+ */
+class OrderExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testApplyToCollectionWithValidOrder()
+    {
+        $queryBuilderProphecy = $this->prophesize(QueryBuilder::class);
+
+        $queryBuilderProphecy->addOrderBy('o.name', 'asc')->shouldBeCalled();
+
+        $classMetadataProphecy = $this->prophesize(ClassMetadata::class);
+        $classMetadataProphecy->getIdentifier()->shouldBeCalled()->willReturn(['name']);
+
+        $emProphecy = $this->prophesize(EntityManager::class);
+        $emProphecy->getClassMetadata(Dummy::class)->shouldBeCalled()->willReturn($classMetadataProphecy->reveal());
+
+        $em = $queryBuilderProphecy->getEntityManager()->shouldBeCalled()->willReturn($emProphecy->reveal());
+
+        $queryBuilder = $queryBuilderProphecy->reveal();
+        $orderExtensionTest = new OrderExtension('asc');
+        $orderExtensionTest->applyToCollection($queryBuilder, Dummy::class);
+    }
+
+    public function testApplyToCollectionWithWrongOrder()
+    {
+        $queryBuilderProphecy = $this->prophesize(QueryBuilder::class);
+
+        $queryBuilderProphecy->addOrderBy('o.name', 'asc')->shouldNotBeCalled();
+
+        $classMetadataProphecy = $this->prophesize(ClassMetadata::class);
+        $classMetadataProphecy->getIdentifier()->shouldBeCalled()->willReturn(['name']);
+
+        $emProphecy = $this->prophesize(EntityManager::class);
+        $emProphecy->getClassMetadata(Dummy::class)->shouldBeCalled()->willReturn($classMetadataProphecy->reveal());
+
+        $em = $queryBuilderProphecy->getEntityManager()->shouldBeCalled()->willReturn($emProphecy->reveal());
+
+        $queryBuilder = $queryBuilderProphecy->reveal();
+        $orderExtensionTest = new OrderExtension();
+        $orderExtensionTest->applyToCollection($queryBuilder, Dummy::class);
+    }
+}
