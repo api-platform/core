bc653fe8a980f2240e72f8e1713e39bad20140b7
Teoh Han Hui <teohhanhui@gmail.com>
2016-08-23 17:12:00 +0800

    Fix Swagger DocumentationNormalizer

--- src/Swagger/Serializer/DocumentationNormalizer.php
+++ src/Swagger/Serializer/DocumentationNormalizer.php
@@ -56,7 +56,6 @@ final class DocumentationNormalizer implements NormalizerInterface
      */
     public function normalize($object, $format = null, array $context = [])
     {
-        $classes = [];
         $itemOperationsDocs = [];
         $definitions = [];
 
@@ -64,45 +63,8 @@ final class DocumentationNormalizer implements NormalizerInterface
             $resourceMetadata = $this->resourceMetadataFactory->create($resourceClass);
 
             $shortName = $resourceMetadata->getShortName();
-            $prefixedShortName = ($iri = $resourceMetadata->getIri()) ? $iri : '#'.$shortName;
 
-            $class = [
-                'name' => $shortName,
-                'externalDocs' => ['url' => $prefixedShortName],
-            ];
-
-            if ($description = $resourceMetadata->getDescription()) {
-                $class = [
-                    'name' => $shortName,
-                    'description' => $description,
-                    'externalDocs' => ['url' => $prefixedShortName],
-                ];
-            }
-
-            $attributes = $resourceMetadata->getAttributes();
-            $context = [];
-
-            if (isset($attributes['normalization_context']['groups'])) {
-                $context['serializer_groups'] = $attributes['normalization_context']['groups'];
-            }
-
-            if (isset($attributes['denormalization_context']['groups'])) {
-                $context['serializer_groups'] = isset($context['serializer_groups']) ? array_merge($context['serializer_groups'], $attributes['denormalization_context']['groups']) : $context['serializer_groups'];
-            }
-
-            $definitions[$shortName] = [
-                'type' => 'object',
-                'xml' => ['name' => 'response'],
-            ];
-
-            foreach ($this->propertyNameCollectionFactory->create($resourceClass, $context) as $propertyName) {
-                $propertyMetadata = $this->propertyMetadataFactory->create($resourceClass, $propertyName);
-                $range = $this->getRange($propertyMetadata);
-                if (empty($range)) {
-                    continue;
-                }
-                $definitions = $this->getDefinitions($propertyMetadata, $propertyName, $shortName, $definitions, $range);
-            }
+            $definitions[$shortName] = $this->getDefinitionSchema($resourceClass, $resourceMetadata);
 
             $operations = $resourceMetadata->getCollectionOperations() ?? [];
             foreach ($operations as $operationName => $collectionOperation) {
@@ -121,11 +83,9 @@ final class DocumentationNormalizer implements NormalizerInterface
                 $swaggerOperation = $this->getSwaggerOperation($resourceClass, $resourceMetadata, $itemOperation, false, $method, $object->getMimeTypes());
                 $itemOperationsDocs[$path] = array_merge($itemOperationsDocs[$path] ?? [], $swaggerOperation);
             }
-
-            $classes[] = $class;
         }
 
-        return $this->computeDoc($object, $definitions, $classes, $itemOperationsDocs);
+        return $this->computeDoc($object, $definitions, $itemOperationsDocs);
     }
 
     private function getPath(string $resourceShortName, array $operation, bool $collection) : string
@@ -143,22 +103,22 @@ final class DocumentationNormalizer implements NormalizerInterface
      */
     private function getSwaggerOperation(string $resourceClass, ResourceMetadata $resourceMetadata, array $operation, bool $collection, string $method, array $mimeTypes) : array
     {
-        $methodSwagger = strtolower($method);
+        $swaggerMethod = strtolower($method);
         $swaggerOperation = $operation['swagger_context'] ?? [];
         $shortName = $resourceMetadata->getShortName();
-        $swaggerOperation[$methodSwagger] = [];
-        $swaggerOperation[$methodSwagger]['tags'] = [$shortName];
+        $swaggerOperation[$swaggerMethod] = [];
+        $swaggerOperation[$swaggerMethod]['tags'] = [$shortName];
 
         switch ($method) {
             case 'GET':
-                $swaggerOperation[$methodSwagger]['produces'] = $mimeTypes;
+                $swaggerOperation[$swaggerMethod]['produces'] = $mimeTypes;
 
                 if ($collection) {
-                    if (!isset($swaggerOperation[$methodSwagger]['title'])) {
-                        $swaggerOperation[$methodSwagger]['summary'] = sprintf('Retrieves the collection of %s resources.', $shortName);
+                    if (!isset($swaggerOperation[$swaggerMethod]['title'])) {
+                        $swaggerOperation[$swaggerMethod]['summary'] = sprintf('Retrieves the collection of %s resources.', $shortName);
                     }
 
-                    $swaggerOperation[$methodSwagger]['responses'] = [
+                    $swaggerOperation[$swaggerMethod]['responses'] = [
                         '200' => [
                             'description' => 'Successful operation',
                              'schema' => [
@@ -170,18 +130,18 @@ final class DocumentationNormalizer implements NormalizerInterface
                     break;
                 }
 
-                if (!isset($swaggerOperation[$methodSwagger]['title'])) {
-                    $swaggerOperation[$methodSwagger]['summary'] = sprintf('Retrieves %s resource.', $shortName);
+                if (!isset($swaggerOperation[$swaggerMethod]['title'])) {
+                    $swaggerOperation[$swaggerMethod]['summary'] = sprintf('Retrieves a %s resource.', $shortName);
                 }
 
-                $swaggerOperation[$methodSwagger]['parameters'][] = [
+                $swaggerOperation[$swaggerMethod]['parameters'][] = [
                     'name' => 'id',
                     'in' => 'path',
                     'required' => true,
                     'type' => 'integer',
                 ];
 
-                $swaggerOperation[$methodSwagger]['responses'] = [
+                $swaggerOperation[$swaggerMethod]['responses'] = [
                     '200' => [
                         'description' => 'Successful operation',
                         'schema' => ['$ref' => sprintf('#/definitions/%s', $shortName)],
@@ -191,24 +151,24 @@ final class DocumentationNormalizer implements NormalizerInterface
                 break;
 
             case 'POST':
-                $swaggerOperation[$methodSwagger]['consumes'] = $swaggerOperation[$methodSwagger]['produces'] = $mimeTypes;
+                $swaggerOperation[$swaggerMethod]['consumes'] = $swaggerOperation[$swaggerMethod]['produces'] = $mimeTypes;
 
-                if (!isset($swaggerOperation[$methodSwagger]['title'])) {
-                    $swaggerOperation[$methodSwagger]['summary'] = sprintf('Creates a %s resource.', $shortName);
+                if (!isset($swaggerOperation[$swaggerMethod]['title'])) {
+                    $swaggerOperation[$swaggerMethod]['summary'] = sprintf('Creates a %s resource.', $shortName);
                 }
 
                 if ($this->resourceClassResolver->isResourceClass($resourceClass)) {
-                    $swaggerOperation[$methodSwagger]['parameters'][] = [
+                    $swaggerOperation[$swaggerMethod]['parameters'][] = [
                         'in' => 'body',
                         'name' => 'body',
-                        'description' => sprintf('%s resource to be added', $shortName),
+                        'description' => sprintf('The new %s resource', $shortName),
                         'schema' => [
                             '$ref' => sprintf('#/definitions/%s', $shortName),
                         ],
                     ];
                 }
 
-                $swaggerOperation[$methodSwagger]['responses'] = [
+                $swaggerOperation[$swaggerMethod]['responses'] = [
                         '201' => [
                             'description' => 'Successful operation',
                             'schema' => ['$ref' => sprintf('#/definitions/%s', $shortName)],
@@ -216,17 +176,17 @@ final class DocumentationNormalizer implements NormalizerInterface
                         '400' => ['description' => 'Invalid input'],
                         '404' => ['description' => 'Resource not found'],
                 ];
-            break;
+                break;
 
             case 'PUT':
-                $swaggerOperation[$methodSwagger]['consumes'] = $swaggerOperation[$methodSwagger]['produces'] = $mimeTypes;
+                $swaggerOperation[$swaggerMethod]['consumes'] = $swaggerOperation[$swaggerMethod]['produces'] = $mimeTypes;
 
-                if (!isset($swaggerOperation[$methodSwagger]['title'])) {
-                    $swaggerOperation[$methodSwagger]['summary'] = sprintf('Replaces the %s resource.', $shortName);
+                if (!isset($swaggerOperation[$swaggerMethod]['title'])) {
+                    $swaggerOperation[$swaggerMethod]['summary'] = sprintf('Replaces the %s resource.', $shortName);
                 }
 
                 if ($this->resourceClassResolver->isResourceClass($resourceClass)) {
-                    $swaggerOperation[$methodSwagger]['parameters'] = [
+                    $swaggerOperation[$swaggerMethod]['parameters'] = [
                         [
                             'name' => 'id',
                             'in' => 'path',
@@ -236,7 +196,7 @@ final class DocumentationNormalizer implements NormalizerInterface
                         [
                             'in' => 'body',
                             'name' => 'body',
-                            'description' => sprintf('%s resource to be added', $shortName),
+                            'description' => sprintf('The updated %s resource', $shortName),
                             'schema' => [
                                 '$ref' => sprintf('#/definitions/%s', $shortName),
                             ],
@@ -244,7 +204,7 @@ final class DocumentationNormalizer implements NormalizerInterface
                     ];
                 }
 
-                $swaggerOperation[$methodSwagger]['responses'] = [
+                $swaggerOperation[$swaggerMethod]['responses'] = [
                     '200' => [
                         'description' => 'Successful operation',
                         'schema' => ['$ref' => sprintf('#/definitions/%s', $shortName)],
@@ -252,21 +212,25 @@ final class DocumentationNormalizer implements NormalizerInterface
                     '400' => ['description' => 'Invalid input'],
                     '404' => ['description' => 'Resource not found'],
                 ];
-            break;
+                break;
 
             case 'DELETE':
-                $swaggerOperation[$methodSwagger]['responses'] = [
+                if (!isset($swaggerOperation[$swaggerMethod]['title'])) {
+                    $swaggerOperation[$swaggerMethod]['summary'] = sprintf('Removes the %s resource.', $shortName);
+                }
+
+                $swaggerOperation[$swaggerMethod]['responses'] = [
                     '204' => ['description' => 'Deleted'],
                     '404' => ['description' => 'Resource not found'],
                 ];
 
-                $swaggerOperation[$methodSwagger]['parameters'] = [[
+                $swaggerOperation[$swaggerMethod]['parameters'] = [[
                     'name' => 'id',
                     'in' => 'path',
                     'required' => true,
                     'type' => 'integer',
                 ]];
-            break;
+                break;
         }
         ksort($swaggerOperation);
 
@@ -274,98 +238,146 @@ final class DocumentationNormalizer implements NormalizerInterface
     }
 
     /**
-     * Gets the range of the property.
+     * Gets a definition Schema Object.
+     *
+     * @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/2.0.md#schemaObject
+     *
+     * @param string           $resourceClass
+     * @param ResourceMetadata $resourceMetadata
+     *
+     * @return \ArrayObject
+     */
+    private function getDefinitionSchema(string $resourceClass, ResourceMetadata $resourceMetadata) : \ArrayObject
+    {
+        $definitionSchema = new \ArrayObject([
+            'type' => 'object',
+        ]);
+
+        if ($description = $resourceMetadata->getDescription()) {
+            $definitionSchema['description'] = $description;
+        }
+
+        if ($iri = $resourceMetadata->getIri()) {
+            $definitionSchema['externalDocs'] = [
+                'url' => $iri,
+            ];
+        }
+
+        $attributes = $resourceMetadata->getAttributes();
+        $context = [];
+
+        if (isset($attributes['normalization_context']['groups'])) {
+            $context['serializer_groups'] = $attributes['normalization_context']['groups'];
+        }
+
+        if (isset($attributes['denormalization_context']['groups'])) {
+            $context['serializer_groups'] = isset($context['serializer_groups']) ? array_merge($context['serializer_groups'], $attributes['denormalization_context']['groups']) : $context['serializer_groups'];
+        }
+
+        foreach ($this->propertyNameCollectionFactory->create($resourceClass, $context) as $propertyName) {
+            $propertyMetadata = $this->propertyMetadataFactory->create($resourceClass, $propertyName);
+
+            if ($propertyMetadata->isRequired()) {
+                $definitionSchema['required'][] = $propertyName;
+            }
+
+            $definitionSchema['properties'][$propertyName] = $this->getPropertySchema($propertyMetadata);
+        }
+
+        return $definitionSchema;
+    }
+
+    /**
+     * Gets a property Schema Object.
+     *
+     * @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/2.0.md#schemaObject
      *
      * @param PropertyMetadata $propertyMetadata
      *
-     * @return array
+     * @return \ArrayObject
      */
-    private function getRange(PropertyMetadata $propertyMetadata) : array
+    private function getPropertySchema(PropertyMetadata $propertyMetadata) : \ArrayObject
     {
+        $propertySchema = new \ArrayObject();
+
+        if ($description = $propertyMetadata->getDescription()) {
+            $propertySchema['description'] = $description;
+        }
+
         $type = $propertyMetadata->getType();
         if (!$type) {
-            return [];
+            return $propertySchema;
         }
 
-        if ($type->isCollection() && $collectionType = $type->getCollectionValueType()) {
-            $type = $collectionType;
-        }
+        $valueSchema = new \ArrayObject();
 
-        switch ($type->getBuiltinType()) {
+        $valueType = $type->isCollection() ? $type->getCollectionValueType() : $type;
+
+        switch ($valueType ? $valueType->getBuiltinType() : null) {
             case Type::BUILTIN_TYPE_STRING:
-                return ['complex' => false, 'value' => 'string'];
+                $valueSchema['type'] = 'string';
+                break;
 
             case Type::BUILTIN_TYPE_INT:
-                return ['complex' => false, 'value' => 'integer'];
+                $valueSchema['type'] = 'integer';
+                break;
 
             case Type::BUILTIN_TYPE_FLOAT:
-                return ['complex' => false, 'value' => 'number'];
+                $valueSchema['type'] = 'number';
+                break;
 
             case Type::BUILTIN_TYPE_BOOL:
-                return ['complex' => false, 'value' => 'boolean'];
+                $valueSchema['type'] = 'boolean';
+                break;
 
             case Type::BUILTIN_TYPE_OBJECT:
-                $className = $type->getClassName();
+                $className = $valueType->getClassName();
                 if (null === $className) {
-                    return [];
+                    break;
                 }
 
                 if (is_subclass_of($className, \DateTimeInterface::class)) {
-                    return ['complex' => false, 'value' => 'string', 'example' => '1988-01-21T00:00:00+00:00'];
+                    $valueSchema['type'] = 'string';
+                    $valueSchema['format'] = 'date-time';
+                    break;
                 }
 
                 if (!$this->resourceClassResolver->isResourceClass($className)) {
-                    return [];
+                    break;
                 }
 
                 if ($propertyMetadata->isReadableLink()) {
-                    return ['complex' => true, 'value' => sprintf('#/definitions/%s', $this->resourceMetadataFactory->create($className)->getShortName())];
+                    $valueSchema['$ref'] = sprintf('#/definitions/%s', $this->resourceMetadataFactory->create($className)->getShortName());
+                    break;
                 }
 
-                return ['complex' => false, 'value' => 'string', 'example' => '/my/iri'];
+                $valueSchema['type'] = 'string';
+                $valueSchema['format'] = 'uri';
+                break;
 
             default:
-                return ['complex' => false, 'value' => 'null'];
-        }
-    }
-
-    private function getDefinitions(PropertyMetadata $propertyMetadata, string $propertyName, string $shortName, array $definitions, array $range): array
-    {
-        if ($propertyMetadata->isRequired()) {
-            $definitions[$shortName]['required'][] = $propertyName;
+                break;
         }
 
-        if ($propertyMetadata->getDescription()) {
-            $definitions[$shortName]['properties'][$propertyName]['description'] = $propertyMetadata->getDescription();
-        }
-
-        if ($range['complex']) {
-            $definitions[$shortName]['properties'][$propertyName] = ['$ref' => $range['value']];
+        if ($type->isCollection()) {
+            $propertySchema['type'] = 'array';
+            $propertySchema['items'] = $valueSchema;
         } else {
-            $definitions[$shortName]['properties'][$propertyName] = ['type' => $range['value']];
-
-            if (isset($range['example'])) {
-                $definitions[$shortName]['properties'][$propertyName]['example'] = $range['example'];
-            }
+            $propertySchema = new \ArrayObject(array_merge((array) $propertySchema, (array) $valueSchema));
         }
 
-        return $definitions;
+        return $propertySchema;
     }
 
-    public function computeDoc(Documentation $object, array $definitions, array $classes, array $itemOperationsDocs): array
+    private function computeDoc(Documentation $object, array $definitions, array $itemOperationsDocs): array
     {
         $doc['swagger'] = self::SWAGGER_VERSION;
-        if ('' !== $object->getTitle()) {
-            $doc['info']['title'] = $object->getTitle();
-        }
-
+        $doc['info']['title'] = $object->getTitle();
         if ('' !== $object->getDescription()) {
             $doc['info']['description'] = $object->getDescription();
         }
-        $doc['info']['version'] = $object->getVersion() ?? '0.0.0';
+        $doc['info']['version'] = $object->getVersion();
         $doc['definitions'] = $definitions;
-        $doc['externalDocs'] = ['description' => 'Find more about API Platform', 'url' => 'https://api-platform.com'];
-        $doc['tags'] = $classes;
         $doc['paths'] = $itemOperationsDocs;
 
         return $doc;
