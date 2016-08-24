49d9856d3d70beb54f66dff09529523e4ff81585
Kévin Dunglas <dunglas@gmail.com>
2016-08-24 11:36:17 +0200

    Add more unit tests for metadata

---
+++ tests/Metadata/Resource/ResourceNameCollectionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ApiPlatform\Core\tests\Metadata\Resource;
+
+use ApiPlatform\Core\Metadata\Resource\ResourceNameCollection;
+
+/**
+ * @author Kévin Dunglas <dunglas@gmail.com>
+ */
+class ResourceNameCollectionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testValueObject()
+    {
+        $collection = new ResourceNameCollection(['foo', 'bar']);
+        $this->assertInstanceOf(\Countable::class, $collection);
+        $this->assertInstanceOf(\IteratorAggregate::class, $collection);
+        $this->assertCount(2, $collection);
+        $this->assertInstanceOf(\ArrayIterator::class, $collection->getIterator());
+    }
+}
