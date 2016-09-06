a97f11d9008ef62973057737e6eb90293bad10df
Kévin Dunglas <dunglas@gmail.com>
2016-09-06 08:12:14 +0200

    Improve quality of ApiPlatformExtension

--- src/Bridge/Symfony/Bundle/DependencyInjection/ApiPlatformExtension.php
+++ src/Bridge/Symfony/Bundle/DependencyInjection/ApiPlatformExtension.php
@@ -11,6 +11,7 @@
 
 namespace ApiPlatform\Core\Bridge\Symfony\Bundle\DependencyInjection;
 
+use Doctrine\ORM\Version;
 use phpDocumentor\Reflection\DocBlockFactoryInterface;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
 use Symfony\Component\Config\FileLocator;
@@ -53,16 +54,36 @@ final class ApiPlatformExtension extends Extension implements PrependExtensionIn
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
-        $container->setAlias('api_platform.operation_path_resolver.default', $config['default_operation_path_resolver']);
-
-        if ($config['name_converter']) {
-            $container->setAlias('api_platform.name_converter', $config['name_converter']);
-        }
-
         $formats = $this->getFormats($config['formats']);
         $errorFormats = $this->getFormats($config['error_formats']);
+        $this->handleConfig($container, $config, $formats, $errorFormats);
 
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('api.xml');
+        $loader->load('data_provider.xml');
+
+        $bundles = $container->getParameter('kernel.bundles');
+
+        $this->registerMetadataConfiguration($container, $loader);
+        $this->registerSwaggerConfiguration($container, $config, $loader);
+        $this->registerJsonLdConfiguration($formats, $loader);
+        $this->registerJsonHalConfiguration($formats, $loader);
+        $this->registerJsonProblemConfiguration($errorFormats, $loader);
+        $this->registerLoaders($container, $bundles);
+        $this->registerBundlesConfiguration($bundles, $config, $loader);
+        $this->registerCacheConfiguration($container);
+    }
+
+    /**
+     * Handles configuration.
+     *
+     * @param ContainerBuilder $container
+     * @param array            $config
+     * @param array            $formats
+     * @param array            $errorFormats
+     */
+    private function handleConfig(ContainerBuilder $container, array $config, array $formats, array $errorFormats)
+    {
         $container->setParameter('api_platform.title', $config['title']);
         $container->setParameter('api_platform.description', $config['description']);
         $container->setParameter('api_platform.version', $config['version']);
@@ -79,45 +100,107 @@ final class ApiPlatformExtension extends Extension implements PrependExtensionIn
         $container->setParameter('api_platform.collection.pagination.enabled_parameter_name', $config['collection']['pagination']['enabled_parameter_name']);
         $container->setParameter('api_platform.collection.pagination.items_per_page_parameter_name', $config['collection']['pagination']['items_per_page_parameter_name']);
 
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('api.xml');
+        $container->setAlias('api_platform.operation_path_resolver.default', $config['default_operation_path_resolver']);
+
+        if ($config['name_converter']) {
+            $container->setAlias('api_platform.name_converter', $config['name_converter']);
+        }
+    }
+
+    /**
+     * Registers metadata configuration.
+     *
+     * @param ContainerBuilder $container
+     * @param XmlFileLoader    $loader
+     */
+    private function registerMetadataConfiguration(ContainerBuilder $container, XmlFileLoader $loader)
+    {
         $loader->load('metadata.xml');
-        $loader->load('data_provider.xml');
-
-        if ($config['enable_swagger']) {
-            $loader->load('swagger.xml');
-            $container->setParameter('api_platform.enable_swagger', (string) $config['enable_swagger']);
-        }
-
-        if (isset($formats['jsonld'])) {
-            $loader->load('jsonld.xml');
-            $loader->load('hydra.xml');
-        }
-
-        if (isset($formats['jsonhal'])) {
-            $loader->load('hal.xml');
-        }
-
-        if (isset($errorFormats['jsonproblem'])) {
-            $loader->load('problem.xml');
-        }
-
-        $this->registerAnnotationLoaders($container);
-        $this->registerFileLoaders($container);
 
         if (!interface_exists(DocBlockFactoryInterface::class)) {
             $container->removeDefinition('api_platform.metadata.resource.metadata_factory.php_doc');
         }
+    }
 
-        $bundles = $container->getParameter('kernel.bundles');
+    /**
+     * Registers the Swagger and Swagger UI configuration.
+     *
+     * @param ContainerBuilder $container
+     * @param array            $config
+     * @param XmlFileLoader    $loader
+     */
+    private function registerSwaggerConfiguration(ContainerBuilder $container, array $config, XmlFileLoader $loader)
+    {
+        if (!$config['enable_swagger']) {
+            return;
+        }
 
+        $loader->load('swagger.xml');
+        $container->setParameter('api_platform.enable_swagger', (string) $config['enable_swagger']);
+    }
+
+    /**
+     * Registers the JSON-LD and Hydra configuration.
+     *
+     * @param array         $formats
+     * @param XmlFileLoader $loader
+     */
+    private function registerJsonLdConfiguration(array $formats, XmlFileLoader $loader)
+    {
+        if (!isset($formats['jsonld'])) {
+            return;
+        }
+
+        $loader->load('jsonld.xml');
+        $loader->load('hydra.xml');
+    }
+
+    /**
+     * Registers the HAL configuration.
+     *
+     * @param array         $formats
+     * @param XmlFileLoader $loader
+     */
+    private function registerJsonHalConfiguration(array $formats, XmlFileLoader $loader)
+    {
+        if (!isset($formats['jsonhal'])) {
+            return;
+        }
+
+        $loader->load('hal.xml');
+    }
+
+    /**
+     * Registers the JSON Problem configuration.
+     *
+     * @param array         $errorFormats
+     * @param XmlFileLoader $loader
+     */
+    private function registerJsonProblemConfiguration(array $errorFormats, XmlFileLoader $loader)
+    {
+        if (!isset($errorFormats['jsonproblem'])) {
+            return;
+        }
+
+        $loader->load('problem.xml');
+    }
+
+    /**
+     * Registers configuration for integration with third-party bundles.
+     *
+     * @param string[]      $bundles
+     * @param array         $config
+     * @param XmlFileLoader $loader
+     */
+    private function registerBundlesConfiguration(array $bundles, array $config, XmlFileLoader $loader)
+    {
         // Doctrine ORM support
-        if (isset($bundles['DoctrineBundle']) && class_exists('Doctrine\ORM\Version')) {
+        if (isset($bundles['DoctrineBundle']) && class_exists(Version::class)) {
             $loader->load('doctrine_orm.xml');
         }
 
         // FOSUser support
-        if ($config['enable_fos_user']) {
+        if (isset($bundles['FOSUserBundle']) && $config['enable_fos_user']) {
             $loader->load('fos_user.xml');
         }
 
@@ -125,69 +208,76 @@ final class ApiPlatformExtension extends Extension implements PrependExtensionIn
         if (isset($bundles['NelmioApiDocBundle']) && $config['enable_nelmio_api_doc']) {
             $loader->load('nelmio_api_doc.xml');
         }
-
-        // Don't use system cache pool in dev
-        if ($container->getParameter('kernel.debug')) {
-            $container->register('api_platform.cache.metadata.property', ArrayAdapter::class);
-            $container->register('api_platform.cache.metadata.resource', ArrayAdapter::class);
-            $container->register('api_platform.cache.route_name_resolver', ArrayAdapter::class);
-        }
     }
 
     /**
-     * Registers annotations loaders.
+     * Registers the cache configuration.
      *
      * @param ContainerBuilder $container
      */
-    private function registerAnnotationLoaders(ContainerBuilder $container)
+    private function registerCacheConfiguration(ContainerBuilder $container)
     {
-        $paths = [];
-        foreach ($container->getParameter('kernel.bundles') as $bundle) {
-            $reflectionClass = new \ReflectionClass($bundle);
-            $bundleDirectory = dirname($reflectionClass->getFileName());
-            $entityDirectory = $bundleDirectory.DIRECTORY_SEPARATOR.'Entity';
-
-            if (file_exists($entityDirectory)) {
-                $paths[] = $entityDirectory;
-                $container->addResource(new DirectoryResource($entityDirectory, '/\.php$/'));
-            }
+        // Don't use system cache pool in dev
+        if (!$container->getParameter('kernel.debug')) {
+            return;
         }
 
-        $container->getDefinition('api_platform.metadata.resource.name_collection_factory.annotation')->addArgument($paths);
+        $container->register('api_platform.cache.metadata.property', ArrayAdapter::class);
+        $container->register('api_platform.cache.metadata.resource', ArrayAdapter::class);
+        $container->register('api_platform.cache.route_name_resolver', ArrayAdapter::class);
     }
 
     /**
      * Registers configuration file loaders.
      *
      * @param ContainerBuilder $container
+     * @param string[]         $bundles
      */
-    private function registerFileLoaders(ContainerBuilder $container)
+    private function registerLoaders(ContainerBuilder $container, array $bundles)
     {
+        $annotationPaths = [];
         $yamlResources = [];
         $xmlResources = [];
 
-        foreach ($container->getParameter('kernel.bundles') as $bundle) {
-            $reflectionClass = new \ReflectionClass($bundle);
-            $configDirectory = dirname($reflectionClass->getFileName()).'/Resources/config/';
+        foreach ($bundles as $bundle) {
+            $bundleDirectory = dirname((new \ReflectionClass($bundle))->getFileName());
+            $this->addFileResources($bundleDirectory, $xmlResources, $yamlResources);
 
-            try {
-                foreach (Finder::create()->files()->in($configDirectory)->path('api_resources')->name('*.{yml,yaml,xml}') as $file) {
-                    if ('xml' === $file->getExtension()) {
-                        $xmlResources[] = $file->getRealPath();
-                    } else {
-                        $yamlResources[] = $file->getRealPath();
-                    }
-                }
-            } catch (\InvalidArgumentException $e) {
-                // Ignore invalid paths
+            if (file_exists($entityDirectory = $bundleDirectory.'/Entity')) {
+                $annotationPaths[] = $entityDirectory;
+                $container->addResource(new DirectoryResource($entityDirectory, '/\.php$/'));
             }
         }
+
+        $container->getDefinition('api_platform.metadata.resource.name_collection_factory.annotation')->addArgument($annotationPaths);
 
         $container->getDefinition('api_platform.metadata.resource.name_collection_factory.yaml')->replaceArgument(0, $yamlResources);
         $container->getDefinition('api_platform.metadata.resource.metadata_factory.yaml')->replaceArgument(0, $yamlResources);
 
         $container->getDefinition('api_platform.metadata.resource.name_collection_factory.xml')->replaceArgument(0, $xmlResources);
         $container->getDefinition('api_platform.metadata.resource.metadata_factory.xml')->replaceArgument(0, $xmlResources);
+    }
+
+    /**
+     * Populates file resources lists.
+     *
+     * @param string   $bundleDirectory
+     * @param string[] $xmlResources
+     * @param string[] $yamlResources
+     */
+    private function addFileResources(string $bundleDirectory, array &$xmlResources, array &$yamlResources)
+    {
+        try {
+            foreach (Finder::create()->files()->in($bundleDirectory.'/Resources/config/')->path('api_resources')->name('*.{yml,yaml,xml}') as $file) {
+                if ('xml' === $file->getExtension()) {
+                    $xmlResources[] = $file->getRealPath();
+                } else {
+                    $yamlResources[] = $file->getRealPath();
+                }
+            }
+        } catch (\InvalidArgumentException $e) {
+            // Ignore invalid paths
+        }
     }
 
     /**
