abba678e2fba68abf5fca11db1fc506aafaeb203
abluchet <abluchet@ds-restauration.com>
2017-04-03 12:06:39 +0200

    fix FilterEagerLoading when entity has FETCH_EAGER with no force eager

--- tests/Bridge/Doctrine/Orm/Extension/FilterEagerLoadingExtensionTest.php
+++ tests/Bridge/Doctrine/Orm/Extension/FilterEagerLoadingExtensionTest.php
@@ -15,6 +15,8 @@ use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\FilterEagerLoadingExtension;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use ApiPlatform\Core\Metadata\Resource\Factory\ResourceMetadataFactoryInterface;
 use ApiPlatform\Core\Metadata\Resource\ResourceMetadata;
+use ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\CompositeItem;
+use ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\CompositeLabel;
 use ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\CompositeRelation;
 use ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\DummyCar;
 use ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\Foo;
@@ -37,8 +39,12 @@ class FilterEagerLoadingExtensionTest extends \PHPUnit_Framework_TestCase
             ],
         ], null));
 
+        $em = $this->prophesize(EntityManager::class);
+        $em->getClassMetadata(DummyCar::class)->shouldBeCalled()->willReturn(new ClassMetadataInfo(DummyCar::class));
+
         $qb = $this->prophesize(QueryBuilder::class);
         $qb->getDQLPart('where')->shouldNotBeCalled();
+        $qb->getEntityManager()->willReturn($em);
 
         $queryNameGenerator = $this->prophesize(QueryNameGeneratorInterface::class);
 
@@ -48,6 +54,9 @@ class FilterEagerLoadingExtensionTest extends \PHPUnit_Framework_TestCase
 
     public function testIsNoForceEagerResource()
     {
+        $em = $this->prophesize(EntityManager::class);
+        $em->getClassMetadata(DummyCar::class)->shouldBeCalled()->willReturn(new ClassMetadataInfo(DummyCar::class));
+
         $resourceMetadataFactoryProphecy = $this->prophesize(ResourceMetadataFactoryInterface::class);
         $resourceMetadataFactoryProphecy->create(DummyCar::class)->willReturn(new ResourceMetadata(DummyCar::class, null, null, null, [
             'get' => [],
@@ -55,6 +64,7 @@ class FilterEagerLoadingExtensionTest extends \PHPUnit_Framework_TestCase
 
         $qb = $this->prophesize(QueryBuilder::class);
         $qb->getDQLPart('where')->shouldNotBeCalled();
+        $qb->getEntityManager()->willReturn($em);
 
         $queryNameGenerator = $this->prophesize(QueryNameGeneratorInterface::class);
 
@@ -64,6 +74,9 @@ class FilterEagerLoadingExtensionTest extends \PHPUnit_Framework_TestCase
 
     public function testIsForceEagerConfig()
     {
+        $em = $this->prophesize(EntityManager::class);
+        $em->getClassMetadata(DummyCar::class)->shouldBeCalled()->willReturn(new ClassMetadataInfo(DummyCar::class));
+
         $resourceMetadataFactoryProphecy = $this->prophesize(ResourceMetadataFactoryInterface::class);
         $resourceMetadataFactoryProphecy->create(DummyCar::class)->willReturn(new ResourceMetadata(DummyCar::class, null, null, null, [
             'get' => [],
@@ -71,6 +84,7 @@ class FilterEagerLoadingExtensionTest extends \PHPUnit_Framework_TestCase
 
         $qb = $this->prophesize(QueryBuilder::class);
         $qb->getDQLPart('where')->shouldNotBeCalled();
+        $qb->getEntityManager()->willReturn($em);
 
         $queryNameGenerator = $this->prophesize(QueryNameGeneratorInterface::class);
 
@@ -80,11 +94,15 @@ class FilterEagerLoadingExtensionTest extends \PHPUnit_Framework_TestCase
 
     public function testHasNoWherePart()
     {
+        $em = $this->prophesize(EntityManager::class);
+        $em->getClassMetadata(DummyCar::class)->shouldBeCalled()->willReturn(new ClassMetadataInfo(DummyCar::class));
+
         $resourceMetadataFactoryProphecy = $this->prophesize(ResourceMetadataFactoryInterface::class);
         $resourceMetadataFactoryProphecy->create(DummyCar::class)->willReturn(new ResourceMetadata(DummyCar::class));
 
         $qb = $this->prophesize(QueryBuilder::class);
         $qb->getDQLPart('where')->shouldBeCalled()->willReturn(null);
+        $qb->getEntityManager()->willReturn($em);
 
         $queryNameGenerator = $this->prophesize(QueryNameGeneratorInterface::class);
 
@@ -94,12 +112,16 @@ class FilterEagerLoadingExtensionTest extends \PHPUnit_Framework_TestCase
 
     public function testHasNoJoinPart()
     {
+        $em = $this->prophesize(EntityManager::class);
+        $em->getClassMetadata(DummyCar::class)->shouldBeCalled()->willReturn(new ClassMetadataInfo(DummyCar::class));
+
         $resourceMetadataFactoryProphecy = $this->prophesize(ResourceMetadataFactoryInterface::class);
         $resourceMetadataFactoryProphecy->create(DummyCar::class)->willReturn(new ResourceMetadata(DummyCar::class));
 
         $qb = $this->prophesize(QueryBuilder::class);
         $qb->getDQLPart('where')->shouldBeCalled()->willReturn(new Expr\Andx());
         $qb->getDQLPart('join')->shouldBeCalled()->willReturn(null);
+        $qb->getEntityManager()->willReturn($em);
 
         $queryNameGenerator = $this->prophesize(QueryNameGeneratorInterface::class);
 
@@ -162,12 +184,12 @@ class FilterEagerLoadingExtensionTest extends \PHPUnit_Framework_TestCase
         $filterEagerLoadingExtension->applyToCollection($qb, $queryNameGenerator->reveal(), DummyCar::class, 'get');
 
         $expected = <<<SQL
-SELECT o, CASE WHEN o.dateCreated IS NULL THEN 0 ELSE 1 END AS HIDDEN _o_dateCreated_null_rank 
-FROM ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\DummyCar o 
-LEFT JOIN o.colors colors 
+SELECT o, CASE WHEN o.dateCreated IS NULL THEN 0 ELSE 1 END AS HIDDEN _o_dateCreated_null_rank
+FROM ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\DummyCar o
+LEFT JOIN o.colors colors
 WHERE o IN(
-  SELECT o_2 FROM ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\DummyCar o_2 
-  LEFT JOIN o_2.colors colors_2 
+  SELECT o_2 FROM ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\DummyCar o_2
+  LEFT JOIN o_2.colors colors_2
   WHERE o_2.colors = :foo
 ) ORDER BY _o_dateCreated_null_rank DESC ASC
 SQL;
@@ -202,15 +224,15 @@ SQL;
         $filterEagerLoadingExtension->applyToCollection($qb, $queryNameGenerator->reveal(), DummyCar::class, 'get');
 
         $expected = <<<SQL
-SELECT o, count(o.id) as counter 
-FROM ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\DummyCar o 
-LEFT JOIN o.colors colors WHERE o 
+SELECT o, count(o.id) as counter
+FROM ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\DummyCar o
+LEFT JOIN o.colors colors WHERE o
 IN(
-  SELECT o_2 FROM ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\DummyCar o_2 
-  LEFT JOIN o_2.colors colors_2 
+  SELECT o_2 FROM ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\DummyCar o_2
+  LEFT JOIN o_2.colors colors_2
   WHERE o_2.colors = :foo
-) 
-GROUP BY o.colors HAVING counter > 3 
+)
+GROUP BY o.colors HAVING counter > 3
 ORDER BY o.colors ASC
 SQL;
 
@@ -248,19 +270,74 @@ SQL;
         $filterEagerLoadingExtension->applyToCollection($qb, $queryNameGenerator->reveal(), CompositeRelation::class, 'get');
 
         $expected = <<<SQL
-SELECT o 
-FROM ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\CompositeRelation o 
-INNER JOIN o.compositeItem item 
-INNER JOIN o.compositeLabel label 
+SELECT o
+FROM ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\CompositeRelation o
+INNER JOIN o.compositeItem item
+INNER JOIN o.compositeLabel label
 WHERE o.item IN(
-    SELECT IDENTITY(o_2.item) FROM ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\CompositeRelation o_2 
-    INNER JOIN o_2.compositeItem item_2 
-    INNER JOIN o_2.compositeLabel label_2 
+    SELECT IDENTITY(o_2.item) FROM ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\CompositeRelation o_2
+    INNER JOIN o_2.compositeItem item_2
+    INNER JOIN o_2.compositeLabel label_2
     WHERE item_2.field1 = :foo
 ) AND o.label IN(
-    SELECT IDENTITY(o_2.label) FROM ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\CompositeRelation o_2 
-    INNER JOIN o_2.compositeItem item_2 
-    INNER JOIN o_2.compositeLabel label_2 
+    SELECT IDENTITY(o_2.label) FROM ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\CompositeRelation o_2
+    INNER JOIN o_2.compositeItem item_2
+    INNER JOIN o_2.compositeLabel label_2
+    WHERE item_2.field1 = :foo
+)
+SQL;
+
+        $this->assertEquals($this->toDQLString($expected), $qb->getDQL());
+    }
+
+    public function testFetchEagerWithNoForceEager()
+    {
+        $resourceMetadataFactoryProphecy = $this->prophesize(ResourceMetadataFactoryInterface::class);
+        $resourceMetadataFactoryProphecy->create(CompositeRelation::class)->willReturn(new ResourceMetadata(CompositeRelation::class));
+
+        $classMetadata = new ClassMetadataInfo(CompositeRelation::class);
+        $classMetadata->isIdentifierComposite = true;
+        $classMetadata->identifier = ['item', 'label'];
+        $classMetadata->associationMappings = [
+            'item' => ['fetch' => 3, 'joinColumns' => [['nullable' => false]], 'targetEntity' => CompositeItem::class],
+            'label' => ['fetch' => 3, 'joinColumns' => [['nullable' => false]], 'targetEntity' => CompositeLabel::class],
+        ];
+
+        $em = $this->prophesize(EntityManager::class);
+        $em->getExpressionBuilder()->shouldBeCalled()->willReturn(new Expr());
+        $em->getClassMetadata(CompositeRelation::class)->shouldBeCalled()->willReturn($classMetadata);
+
+        $qb = new QueryBuilder($em->reveal());
+
+        $qb->select('o')
+            ->from(CompositeRelation::class, 'o')
+            ->innerJoin('o.compositeItem', 'item')
+            ->innerJoin('o.compositeLabel', 'label')
+            ->where('item.field1 = :foo')
+            ->setParameter('foo', 1);
+
+        $queryNameGenerator = $this->prophesize(QueryNameGeneratorInterface::class);
+        $queryNameGenerator->generateJoinAlias('item')->shouldBeCalled()->willReturn('item_2');
+        $queryNameGenerator->generateJoinAlias('label')->shouldBeCalled()->willReturn('label_2');
+        $queryNameGenerator->generateJoinAlias('o')->shouldBeCalled()->willReturn('o_2');
+
+        $filterEagerLoadingExtension = new FilterEagerLoadingExtension($resourceMetadataFactoryProphecy->reveal(), false);
+        $filterEagerLoadingExtension->applyToCollection($qb, $queryNameGenerator->reveal(), CompositeRelation::class, 'get');
+
+        $expected = <<<SQL
+SELECT o
+FROM ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\CompositeRelation o
+INNER JOIN o.compositeItem item
+INNER JOIN o.compositeLabel label
+WHERE o.item IN(
+    SELECT IDENTITY(o_2.item) FROM ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\CompositeRelation o_2
+    INNER JOIN o_2.compositeItem item_2
+    INNER JOIN o_2.compositeLabel label_2
+    WHERE item_2.field1 = :foo
+) AND o.label IN(
+    SELECT IDENTITY(o_2.label) FROM ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\CompositeRelation o_2
+    INNER JOIN o_2.compositeItem item_2
+    INNER JOIN o_2.compositeLabel label_2
     WHERE item_2.field1 = :foo
 )
 SQL;
@@ -270,6 +347,6 @@ SQL;
 
     private function toDQLString(string $dql): string
     {
-        return preg_replace('/\\r\\n|\\n/', '', str_replace('  ', '', $dql));
+        return preg_replace(['/\s+/', '/\(\s/', '/\s\)/'], [' ', '(', ')'], $dql);
     }
 }
