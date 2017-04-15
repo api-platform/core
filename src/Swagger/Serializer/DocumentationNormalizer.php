c61fceed3c6b71a7a58b4615e239f2b4c7ce58a7
Hector Hurtarte <hectorh30@gmail.com>
2017-04-15 01:14:22 -0600

    Fix CS

--- src/Swagger/Serializer/DocumentationNormalizer.php
+++ src/Swagger/Serializer/DocumentationNormalizer.php
@@ -182,6 +182,7 @@ final class DocumentationNormalizer implements NormalizerInterface
                 return $this->updatePutOperation($pathOperation, $mimeTypes, $collection, $resourceMetadata, $resourceClass, $resourceShortName, $operationName, $definitions);
             case 'PATCH':
                 $pathOperation['summary'] ?? $pathOperation['summary'] = sprintf('Updates the %s resource.', $resourceShortName);
+
                 return $this->updatePutOperation($pathOperation, $mimeTypes, $collection, $resourceMetadata, $resourceClass, $resourceShortName, $operationName, $definitions);
             case 'DELETE':
                 return $this->updateDeleteOperation($pathOperation, $resourceShortName);
