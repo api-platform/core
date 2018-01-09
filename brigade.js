a27c3c702e8daef467c697033eafdeb0d876f762
Kévin Dunglas <dunglas@gmail.com>
2018-01-09 17:05:13 +0100

    Don't set CI_BRANCH if PR

--- brigade.js
+++ brigade.js
@@ -27,7 +27,6 @@ const coverage = (event, project) => {
     const gh = JSON.parse(event.payload);
     if (gh.pull_request) {
       job.env.CI_PULL_REQUEST = gh.pull_request.number.toString();
-      job.env.CI_BRANCH = gh.pull_request.base.ref;
     } else if (gh.ref) {
       const parts = gh.ref.split('/');
       job.env.CI_BRANCH = parts[parts.length - 1];
