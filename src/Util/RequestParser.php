fb7cc479046c03352d96dedc57d97f1dbc3fe323
Anthony GRASSIOT <antograssiot@free.fr>
2018-02-16 13:59:34 +0100

    Ensure that OrderFilter preserves the query string parameters order

--- src/Util/RequestParser.php
+++ src/Util/RequestParser.php
@@ -38,7 +38,7 @@ final class RequestParser
      */
     public static function parseAndDuplicateRequest(Request $request): Request
     {
-        $query = self::parseRequestParams($request->getQueryString() ?? '');
+        $query = self::parseRequestParams(self::getQueryString($request) ?? '');
         $body = self::parseRequestParams($request->getContent());
 
         return $request->duplicate($query, $body);
@@ -73,5 +73,43 @@ final class RequestParser
         parse_str($source, $params);
 
         return array_combine(array_map('hex2bin', array_keys($params)), $params);
+    }
+
+    /**
+     * Generates the normalized query string for the Request.
+     *
+     * It builds a normalized query string, where keys/value pairs are alphabetized
+     * and have consistent escaping.
+     *
+     * @return string|null A normalized query string for the Request
+     */
+    public static function getQueryString(Request $request)
+    {
+        $qs = $request->server->get('QUERY_STRING', '');
+        if ('' === $qs) {
+            return null;
+        }
+
+        $parts = [];
+
+        foreach (explode('&', $qs) as $param) {
+            if ('' === $param || '=' === $param[0]) {
+                // Ignore useless delimiters, e.g. "x=y&".
+                // Also ignore pairs with empty key, even if there was a value, e.g. "=value", as such nameless values cannot be retrieved anyway.
+                // PHP also does not include them when building _GET.
+                continue;
+            }
+
+            $keyValuePair = explode('=', $param, 2);
+
+            // GET parameters, that are submitted from a HTML form, encode spaces as "+" by default (as defined in enctype application/x-www-form-urlencoded).
+            // PHP also converts "+" to spaces when filling the global _GET or when using the function parse_str. This is why we use urldecode and then normalize to
+            // RFC 3986 with rawurlencode.
+            $parts[] = isset($keyValuePair[1]) ?
+                rawurlencode(urldecode($keyValuePair[0])).'='.rawurlencode(urldecode($keyValuePair[1])) :
+                rawurlencode(urldecode($keyValuePair[0]));
+        }
+
+        return implode('&', $parts);
     }
 }
