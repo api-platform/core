010b826563ab826d28b8ac5b7c416962a78a4c63
Ben Davies <ben.davies@gmail.com>
2018-03-16 17:23:32 +0000

    replace anonymous with real one as anonymous classes are not serializable

---
+++ tests/Serializer/ResourceListTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Core\Tests\Serializer;
+
+use ApiPlatform\Core\Serializer\ResourceList;
+use PHPUnit\Framework\TestCase;
+
+class ResourceListTest extends TestCase
+{
+    /**
+     * @var ResourceList
+     */
+    private $resourceList;
+
+    public function setUp()
+    {
+        $this->resourceList = new ResourceList();
+    }
+
+    public function testImplementsArrayObject()
+    {
+        $this->assertInstanceOf(\ArrayObject::class, $this->resourceList);
+    }
+
+    public function testSerialize()
+    {
+        $this->resourceList['foo'] = 'bar';
+
+        $this->assertEquals('N;', \serialize($this->resourceList));
+    }
+}
