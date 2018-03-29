d2b657e2798c724ade0bd1bcd3bd089f7f143922
Baptiste Meyer <baptiste@les-tilleuls.coop>
2018-03-29 07:53:25 +0200

    Fallback attributes in the @ApiResource annotation (#1788)
    * Fallback attributes in the @ApiResource annotation
    
    * Refactor to work with the PHP(Storm) Annotation plugin

--- tests/Fixtures/AnnotatedClass.php
+++ tests/Fixtures/AnnotatedClass.php
@@ -11,7 +11,7 @@
 
 declare(strict_types=1);
 
-namespace ApiPlatform\Core\Tests\Annotation;
+namespace ApiPlatform\Core\Tests\Fixtures;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 
@@ -24,6 +24,9 @@ use ApiPlatform\Core\Annotation\ApiResource;
  *     collectionOperations={"bar"={"foo"}},
  *     graphql={"query"={"normalization_context"={"groups"={"foo", "bar"}}}},
  *     attributes={"foo"="bar", "route_prefix"="/whatever"},
+ *     routePrefix="/foo",
+ *     accessControl="has_role('ROLE_FOO')",
+ *     accessControlMessage="You are not foo."
  * )
  *
  * @author Marcus Speight <marcus@pmconnect.co.uk>
