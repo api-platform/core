ab7ca9b9e9dc09c79bd05487eb7f48a60b25b77f
Kévin Dunglas <dunglas@gmail.com>
2018-04-30 11:41:21 +0200

    Reorder normalizers (tiny perf improvement)

--- src/Bridge/Symfony/Bundle/DependencyInjection/ApiPlatformExtension.php
+++ src/Bridge/Symfony/Bundle/DependencyInjection/ApiPlatformExtension.php
@@ -128,11 +128,11 @@ final class ApiPlatformExtension extends Extension implements PrependExtensionIn
         $this->registerOAuthConfiguration($container, $config, $loader);
         $this->registerApiKeysConfiguration($container, $config, $loader);
         $this->registerSwaggerConfiguration($container, $config, $loader);
-        $this->registerJsonApiConfiguration($formats, $loader);
         $this->registerJsonLdConfiguration($formats, $loader);
+        $this->registerGraphqlConfiguration($container, $config, $loader);
         $this->registerJsonHalConfiguration($formats, $loader);
         $this->registerJsonProblemConfiguration($errorFormats, $loader);
-        $this->registerGraphqlConfiguration($container, $config, $loader);
+        $this->registerJsonApiConfiguration($formats, $loader);
         $this->registerBundlesConfiguration($bundles, $config, $loader, $useDoctrine);
         $this->registerCacheConfiguration($container);
         $this->registerDoctrineExtensionConfiguration($container, $config, $useDoctrine);
