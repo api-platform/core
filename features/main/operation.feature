bbbb3791699f43396242a05294713520fc34668c
abluchet <abluchet@ds-restauration.com>
2018-05-24 10:16:50 +0200

    Fix embeddable entities eager loading with groups

--- features/main/operation.feature
+++ features/main/operation.feature
@@ -12,3 +12,24 @@ Feature: Operation support
     """
     "This is a custom action for 42."
     """
+
+  @createSchema
+  @dropSchema
+  Scenario: Select a resource and it's embedded data
+    Given there are 1 embedded dummy objects
+    When I send a "GET" request to "/embedded_dummies_groups/1"
+    Then the response status code should be 200
+    And the response should be in JSON
+    And the header "Content-Type" should be equal to "application/ld+json; charset=utf-8"
+    And the JSON should be equal to:
+    """
+    {
+        "@context": "/contexts/EmbeddedDummy",
+        "@id": "/embedded_dummies/1",
+        "@type": "EmbeddedDummy",
+        "name": "Dummy #1",
+        "embeddedDummy": {
+            "dummyName": "Dummy #1"
+        }
+    }
+    """
--- src/Bridge/Doctrine/Orm/Extension/EagerLoadingExtension.php
+++ src/Bridge/Doctrine/Orm/Extension/EagerLoadingExtension.php
@@ -242,15 +242,17 @@ final class EagerLoadingExtension implements ContextAwareQueryCollectionExtensio
                 continue;
             }
 
-            //the field test allows to add methods to a Resource which do not reflect real database fields
-            if ($targetClassMetadata->hasField($property) && (true === $propertyMetadata->getAttribute('fetchable') || $propertyMetadata->isReadable())) {
-                $select[] = $property;
-            }
-
+            // If it's an embedded property see below
             if (!array_key_exists($property, $targetClassMetadata->embeddedClasses)) {
+                //the field test allows to add methods to a Resource which do not reflect real database fields
+                if ($targetClassMetadata->hasField($property) && (true === $propertyMetadata->getAttribute('fetchable') || $propertyMetadata->isReadable())) {
+                    $select[] = $property;
+                }
+
                 continue;
             }
 
+            // It's an embedded property, select relevent subfields
             foreach ($this->propertyNameCollectionFactory->create($targetClassMetadata->embeddedClasses[$property]['class']) as $embeddedProperty) {
                 $propertyMetadata = $this->propertyMetadataFactory->create($entity, $property, $propertyMetadataOptions);
                 $propertyName = "$property.$embeddedProperty";
