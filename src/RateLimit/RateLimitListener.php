2d248abd91a91ca4fc911b035c6aa76f71a7872e
Toby Griffiths <toby@cubicmushroom.co.uk>
2018-10-31 23:30:53 +0000

    Added timeframe window string to rate limit key

--- src/RateLimit/RateLimitListener.php
+++ src/RateLimit/RateLimitListener.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace ApiPlatform\Core\RateLimit;
 
-use function apcu_add;
-use function apcu_inc;
 use ApiPlatform\Core\Exception\RateLimitExceededException;
 use ApiPlatform\Core\Exception\RuntimeException;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use function apcu_add;
+use function apcu_inc;
 
 /**
  * Service to rate limit requests
@@ -33,6 +33,21 @@ class RateLimitListener implements RateLimitListenerInterface
      */
     private $rateLimiters;
 
+    /**
+     * Map of characters used for time_frame to characters used to retrieve DateTimeImmutable part value
+     *
+     * @var string[]
+     */
+    private $timeFrameScaleCharacterToDateFormatCharacterMap = [
+        'Y' => 'Y',
+        'M' => 'm',
+        'W' => 'W',
+        'D' => 'j',
+        'h' => 'H',
+        'm' => 'i',
+        's' => 's',
+    ];
+
 
     /**
      * Injects dependencies and sets default values.
@@ -40,7 +55,7 @@ class RateLimitListener implements RateLimitListenerInterface
     public function __construct(RequestStack $requestStack, array $config)
     {
         $this->requestStack = $requestStack;
-        $this->config = $config;
+        $this->config       = $config;
         $this->rateLimiters = [];
     }
 
@@ -70,7 +85,10 @@ class RateLimitListener implements RateLimitListenerInterface
         }
 
         foreach ($this->rateLimiters as $rateLimiter) {
-            $rateKey = $rateLimiter->getRateKey($request);
+            $limiterConfig = $this->config[$rateLimiter::getConfigKey()];
+
+            $rateKey       =
+                $rateLimiter->getRateKey($request) . ':' . $this->getTimeFrameString($limiterConfig['time_frame']);
             $hashedRateKey = hash('sha512', $rateKey);
             apcu_add($hashedRateKey, 0);
             $count = apcu_inc($hashedRateKey);
@@ -79,11 +97,40 @@ class RateLimitListener implements RateLimitListenerInterface
                 throw new RuntimeException('Unable to write rate limit to store');
             }
 
-            $limiterConfig = $this->config[$rateLimiter::getConfigKey()];
-
             if ($count > $limiterConfig['capacity']) {
                 throw new RateLimitExceededException();
             }
         }
+    }
+
+
+    /**
+     * Appends the current time frame string to the given rate key.
+     *
+     * @param array $timeFrame
+     *
+     * @return string
+     */
+    private function getTimeFrameString(array $timeFrameConfig): string
+    {
+        ['window' => $window, 'scale' => $scale] = $timeFrameConfig;
+
+        $now = new \DateTimeImmutable();
+
+        $timeFrameString = '';
+
+        foreach ($this->timeFrameScaleCharacterToDateFormatCharacterMap as $timeFrameCharacter => $dateFormatCharacter)
+        {
+            $value = (int)$now->format($dateFormatCharacter);
+
+            if ($timeFrameConfig['scale'] === $timeFrameCharacter) {
+                return $timeFrameString . '_' . ($value - ($value % $window)) . $timeFrameCharacter;
+            }
+
+            $timeFrameString .= '_' . $value . $timeFrameCharacter;
+        }
+
+        throw new RuntimeException('Should never get here!');
+
     }
 }
