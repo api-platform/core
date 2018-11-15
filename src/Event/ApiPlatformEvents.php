11babee83e57bd55b4b7ba3ff4818fcaf291c1bc
Jérôme Deuchnord <jerome@deuchnord.fr>
2018-11-15 13:50:46 +0100

    Add class to store the events’ names

---
+++ src/Event/ApiPlatformEvents.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace ApiPlatform\Core\Event;
+
+
+final class ApiPlatformEvents
+{
+    const PRE_READ = 'api_platform.pre_read';
+    const POST_READ = 'api_platform.post_read';
+
+    const PRE_WRITE = 'api_platform.pre_write';
+    const POST_WRITE = 'api_platform.post_write';
+
+    const PRE_SERIALIZE = 'api_platform.pre_serialize';
+    const POST_SERIALIZE = 'api_platform.post_serialize';
+
+    const PRE_RESPOND = 'api_platform.pre_respond';
+    const POST_RESPOND = 'api_platform.post_respond';
+}
