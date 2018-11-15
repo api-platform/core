134354b20502c5801bc2b9b9567d9e9154fed14c
rkaczmarczyk <rkaczmarczyk@2bepublished.at>
2018-11-15 17:04:59 +0100

    PHPUnit test fix

--- tests/Bridge/Symfony/Bundle/DependencyInjection/ConfigurationTest.php
+++ tests/Bridge/Symfony/Bundle/DependencyInjection/ConfigurationTest.php
@@ -180,7 +180,6 @@ class ConfigurationTest extends TestCase
             'http_cache' => [
                 'invalidation' => [
                     'enabled' => false,
-                    'resources' => [],
                     'varnish_urls' => [],
                     'request_options' => [],
                 ],
