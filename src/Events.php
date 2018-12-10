b06bef186982ae3a12023b71558beba297fcc603
Jérôme Deuchnord <jerome@deuchnord.fr>
2018-12-10 09:44:55 +0100

    Re-order the events

--- src/Events.php
+++ src/Events.php
@@ -18,21 +18,18 @@ final class Events
     const PRE_READ = 'api_platform.pre_read';
     const POST_READ = 'api_platform.post_read';
 
+    const PRE_DESERIALIZE = 'api_platform.pre_deserialize';
+    const POST_DESERIALIZE = 'api_platform.post_deserialize';
+
+    const PRE_VALIDATE = 'api_platform.pre_validate';
+    const POST_VALIDATE = 'api_platform.post_validate';
+
     const PRE_WRITE = 'api_platform.pre_write';
     const POST_WRITE = 'api_platform.post_write';
 
     const PRE_SERIALIZE = 'api_platform.pre_serialize';
     const POST_SERIALIZE = 'api_platform.post_serialize';
 
-    const PRE_DESERIALIZE = 'api_platform.pre_deserialize';
-    const POST_DESERIALIZE = 'api_platform.post_deserialize';
-
-    const PRE_RESPOND = 'api_platform.pre_respond';
-    const POST_RESPOND = 'api_platform.post_respond';
-
     const PRE_ADD_FORMAT = 'api_platform.pre_add_format';
     const POST_ADD_FORMAT = 'api_platform.post_add_format';
-
-    const PRE_VALIDATE = 'api_platform.pre_validate';
-    const POST_VALIDATE = 'api_platform.post_validate';
 }
