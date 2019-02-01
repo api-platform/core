402c6118233bc88199f7b6be78ba9be9add3f13f
Kévin Dunglas <dunglas@gmail.com>
2019-02-01 15:25:05 +0100

    Fix WriteListener

--- src/EventListener/WriteListener.php
+++ src/EventListener/WriteListener.php
@@ -15,6 +15,7 @@ namespace ApiPlatform\Core\EventListener;
 
 use ApiPlatform\Core\Api\IriConverterInterface;
 use ApiPlatform\Core\DataPersister\DataPersisterInterface;
+use ApiPlatform\Core\Metadata\Resource\Factory\ResourceMetadataFactoryInterface;
 use ApiPlatform\Core\Util\RequestAttributesExtractor;
 use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
 
@@ -28,11 +29,13 @@ final class WriteListener
 {
     private $dataPersister;
     private $iriConverter;
+    private $resourceMetadataFactory;
 
-    public function __construct(DataPersisterInterface $dataPersister, IriConverterInterface $iriConverter = null)
+    public function __construct(DataPersisterInterface $dataPersister, IriConverterInterface $iriConverter = null, ResourceMetadataFactoryInterface $resourceMetadataFactory = null)
     {
         $this->dataPersister = $dataPersister;
         $this->iriConverter = $iriConverter;
+        $this->resourceMetadataFactory = $resourceMetadataFactory;
     }
 
     /**
@@ -65,10 +68,21 @@ final class WriteListener
                 // Controller result must be immutable for _api_write_item_iri
                 // if it's class changed compared to the base class let's avoid calling the IriConverter
                 // especially that the Output class could be a DTO that's not referencing any route
-                if (null !== $this->iriConverter && (false !== $attributes['output_class'] ?? null) && $attributes['resource_class'] === ($class = \get_class($controllerResult)) && $class === \get_class($event->getControllerResult())) {
+                if (null === $this->iriConverter) {
+                    return;
+                }
+
+                $hasOutput = true;
+                if (null !== $this->resourceMetadataFactory) {
+                    $resourceMetadata = $this->resourceMetadataFactory->create($attributes['resource_class']);
+                    $hasOutput = false !== $resourceMetadata->getOperationAttribute($attributes, 'output_class', null, true);
+                }
+
+                $class = \get_class($controllerResult);
+                if ($hasOutput && $attributes['resource_class'] === $class && $class === \get_class($event->getControllerResult())) {
                     $request->attributes->set('_api_write_item_iri', $this->iriConverter->getIriFromItem($controllerResult));
                 }
-                break;
+            break;
             case 'DELETE':
                 $this->dataPersister->remove($controllerResult);
                 $event->setControllerResult(null);
