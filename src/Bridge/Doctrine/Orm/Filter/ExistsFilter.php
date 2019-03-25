api-platform/core

commit 19c08e0255ef4903332093401a9ca705730ec045

Update the ExistsFilter behavior (#2243)

src/Bridge/Doctrine/Orm/Filter/ExistsFilter.php
@@ -17,9 +17,13 @@ use ApiPlatform\Core\Bridge\Doctrine\Common\Filter\ExistsFilterInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Common\Filter\ExistsFilterTrait;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryBuilderHelper;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\ORM\Mapping\ClassMetadataInfo;
 use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\ORM\QueryBuilder;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Filters the collection by whether a property value exists or not.
@@ -28,7 +32,7 @@ use Doctrine\ORM\QueryBuilder;
  * the value is not one of ( "true" | "false" | "1" | "0" ) the property is ignored.
  *
  * A query parameter with key but no value is treated as `true`, e.g.:
- * Request: GET /products?brand[exists]
+ * Request: GET /products?exists[brand]
  * Interpretation: filter products which have a brand
  *
  * @author Teoh Han Hui <teohhanhui@gmail.com>
@@ -38,12 +42,39 @@ class ExistsFilter extends AbstractContextAwareFilter implements ExistsFilterInt
     use ExistsFilterTrait;
 
     /**
+     * @param RequestStack|null $requestStack No prefix to prevent autowiring of this deprecated property
+     */
+    public function __construct(ManagerRegistry $managerRegistry, $requestStack = null, LoggerInterface $logger = null, string $existsParameterName = self::QUERY_PARAMETER_KEY, array $properties = null)
+    {
+        parent::__construct($managerRegistry, $requestStack, $logger, $properties);
+
+        $this->existsParameterName = $existsParameterName;
+    }
+
+    /**
      * {@inheritdoc}
      */
-    protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
+    public function apply(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null, array $context = [])
+    {
+        if (!\is_array($context['filters'][$this->existsParameterName] ?? null)) {
+            $context['exists_deprecated_syntax'] = true;
+            parent::apply($queryBuilder, $queryNameGenerator, $resourceClass, $operationName, $context);
+
+            return;
+        }
+
+        foreach ($context['filters'][$this->existsParameterName] as $property => $value) {
+            $this->filterProperty($property, $value, $queryBuilder, $queryNameGenerator, $resourceClass, $operationName, $context);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null, array $context = []): void
     {
         if (
-            !isset($value[self::QUERY_PARAMETER_KEY]) ||
+            (($context['exists_deprecated_syntax'] ?? false) && !isset($value[self::QUERY_PARAMETER_KEY])) ||
             !$this->isPropertyEnabled($property, $resourceClass) ||
             !$this->isPropertyMapped($property, $resourceClass, true) ||
             !$this->isNullableField($property, $resourceClass)
@@ -61,7 +92,7 @@ class ExistsFilter extends AbstractContextAwareFilter implements ExistsFilterInt
 
         $associations = [];
         if ($this->isPropertyNested($property, $resourceClass)) {
-            list($alias, $field, $associations) = $this->addJoinsForNestedProperty($property, $alias, $queryBuilder, $queryNameGenerator, $resourceClass);
+            [$alias, $field, $associations] = $this->addJoinsForNestedProperty($property, $alias, $queryBuilder, $queryNameGenerator, $resourceClass);
         }
         $metadata = $this->getNestedMetadata($resourceClass, $associations);
 
@@ -148,5 +179,23 @@ class ExistsFilter extends AbstractContextAwareFilter implements ExistsFilterInt
         }
 
         return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function extractProperties(Request $request/*, string $resourceClass*/): array
+    {
+        if (!$request->query->has($this->existsParameterName)) {
+            $resourceClass = \func_num_args() > 1 ? (string) func_get_arg(1) : null;
+
+            return parent::extractProperties($request, $resourceClass);
+        }
+
+        @trigger_error(sprintf('The use of "%s::extractProperties()" is deprecated since 2.2. Use the "filters" key of the context instead.', __CLASS__), E_USER_DEPRECATED);
+
+        $properties = $request->query->get($this->existsParameterName);
+
+        return \is_array($properties) ? $properties : [];
     }
 }

src/Bridge/Doctrine/MongoDbOdm/Filter/ExistsFilter.php
@@ -15,8 +15,10 @@ namespace ApiPlatform\Core\Bridge\Doctrine\MongoDbOdm\Filter;
 
 use ApiPlatform\Core\Bridge\Doctrine\Common\Filter\ExistsFilterInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Common\Filter\ExistsFilterTrait;
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\ODM\MongoDB\Aggregation\Builder;
 use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
+use Psr\Log\LoggerInterface;
 
 /**
  * Filters the collection by whether a property value exists or not.
@@ -25,7 +27,7 @@ use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
  * the value is not one of ( "true" | "false" | "1" | "0" ) the property is ignored.
  *
  * A query parameter with key but no value is treated as `true`, e.g.:
- * Request: GET /products?brand[exists]
+ * Request: GET /products?exists[brand]
  * Interpretation: filter products which have a brand
  *
  * @experimental
@@ -37,13 +39,37 @@ final class ExistsFilter extends AbstractFilter implements ExistsFilterInterface
 {
     use ExistsFilterTrait;
 
+    public function __construct(ManagerRegistry $managerRegistry, LoggerInterface $logger = null, string $existsParameterName = self::QUERY_PARAMETER_KEY, array $properties = null)
+    {
+        parent::__construct($managerRegistry, $logger, $properties);
+
+        $this->existsParameterName = $existsParameterName;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply(Builder $aggregationBuilder, string $resourceClass, string $operationName = null, array &$context = [])
+    {
+        if (!\is_array($context['filters'][$this->existsParameterName] ?? null)) {
+            $context['exists_deprecated_syntax'] = true;
+            parent::apply($aggregationBuilder, $resourceClass, $operationName, $context);
+
+            return;
+        }
+
+        foreach ($context['filters'][$this->existsParameterName] as $property => $value) {
+            $this->filterProperty($property, $value, $aggregationBuilder, $resourceClass, $operationName, $context);
+        }
+    }
+
     /**
      * {@inheritdoc}
      */
     protected function filterProperty(string $property, $value, Builder $aggregationBuilder, string $resourceClass, string $operationName = null, array &$context = [])
     {
         if (
-            !isset($value[self::QUERY_PARAMETER_KEY]) ||
+            (($context['exists_deprecated_syntax'] ?? false) && !isset($value[self::QUERY_PARAMETER_KEY])) ||
             !$this->isPropertyEnabled($property, $resourceClass) ||
             !$this->isPropertyMapped($property, $resourceClass, true) ||
             !$this->isNullableField($property, $resourceClass)

features/doctrine/date_filter.feature
@@ -404,7 +404,7 @@ Feature: Date filter on collections
       },
       "hydra:search": {
         "@type": "hydra:IriTemplate",
-        "hydra:template": "/dummies{?dummyBoolean,relatedDummy.embeddedDummy.dummyBoolean,dummyDate[before],dummyDate[strictly_before],dummyDate[after],dummyDate[strictly_after],relatedDummy.dummyDate[before],relatedDummy.dummyDate[strictly_before],relatedDummy.dummyDate[after],relatedDummy.dummyDate[strictly_after],description[exists],relatedDummy.name[exists],dummyBoolean[exists],relatedDummy[exists],dummyFloat,dummyFloat[],dummyPrice,dummyPrice[],order[id],order[name],order[description],order[relatedDummy.name],order[relatedDummy.symfony],order[dummyDate],dummyFloat[between],dummyFloat[gt],dummyFloat[gte],dummyFloat[lt],dummyFloat[lte],dummyPrice[between],dummyPrice[gt],dummyPrice[gte],dummyPrice[lt],dummyPrice[lte],id,id[],name,alias,description,relatedDummy.name,relatedDummy.name[],relatedDummies,relatedDummies[],dummy,relatedDummies.name,relatedDummy.thirdLevel.level,relatedDummy.thirdLevel.level[],relatedDummy.thirdLevel.fourthLevel.level,relatedDummy.thirdLevel.fourthLevel.level[],relatedDummy.thirdLevel.badFourthLevel.level,relatedDummy.thirdLevel.badFourthLevel.level[],relatedDummy.thirdLevel.fourthLevel.badThirdLevel.level,relatedDummy.thirdLevel.fourthLevel.badThirdLevel.level[],properties[]}",
+        "hydra:template": "/dummies{?dummyBoolean,relatedDummy.embeddedDummy.dummyBoolean,dummyDate[before],dummyDate[strictly_before],dummyDate[after],dummyDate[strictly_after],relatedDummy.dummyDate[before],relatedDummy.dummyDate[strictly_before],relatedDummy.dummyDate[after],relatedDummy.dummyDate[strictly_after],exists[alias],exists[description],exists[relatedDummy.name],exists[dummyBoolean],exists[relatedDummy],dummyFloat,dummyFloat[],dummyPrice,dummyPrice[],order[id],order[name],order[description],order[relatedDummy.name],order[relatedDummy.symfony],order[dummyDate],dummyFloat[between],dummyFloat[gt],dummyFloat[gte],dummyFloat[lt],dummyFloat[lte],dummyPrice[between],dummyPrice[gt],dummyPrice[gte],dummyPrice[lt],dummyPrice[lte],id,id[],name,alias,description,relatedDummy.name,relatedDummy.name[],relatedDummies,relatedDummies[],dummy,relatedDummies.name,relatedDummy.thirdLevel.level,relatedDummy.thirdLevel.level[],relatedDummy.thirdLevel.fourthLevel.level,relatedDummy.thirdLevel.fourthLevel.level[],relatedDummy.thirdLevel.badFourthLevel.level,relatedDummy.thirdLevel.badFourthLevel.level[],relatedDummy.thirdLevel.fourthLevel.badThirdLevel.level,relatedDummy.thirdLevel.fourthLevel.badThirdLevel.level[],properties[]}",
         "hydra:variableRepresentation": "BasicRepresentation",
         "hydra:mapping": [
           {
@@ -469,25 +469,31 @@ Feature: Date filter on collections
           },
           {
             "@type": "IriTemplateMapping",
-            "variable": "description[exists]",
+            "variable": "exists[alias]",
+            "property": "alias",
+            "required": false
+          },
+          {
+            "@type": "IriTemplateMapping",
+            "variable": "exists[description]",
             "property": "description",
             "required": false
           },
           {
             "@type": "IriTemplateMapping",
-            "variable": "relatedDummy.name[exists]",
+            "variable": "exists[relatedDummy.name]",
             "property": "relatedDummy.name",
             "required": false
           },
           {
             "@type": "IriTemplateMapping",
-            "variable": "dummyBoolean[exists]",
+            "variable": "exists[dummyBoolean]",
             "property": "dummyBoolean",
             "required": false
           },
           {
             "@type": "IriTemplateMapping",
-            "variable": "relatedDummy[exists]",
+            "variable": "exists[relatedDummy]",
             "property": "relatedDummy",
             "required": false
           },
@@ -831,7 +837,7 @@ Feature: Date filter on collections
           },
           "hydra:search": {
               "@type": "hydra:IriTemplate",
-              "hydra:template": "/dummies{?dummyBoolean,dummyDate[before],dummyDate[after],relatedDummy.dummyDate[before],relatedDummy.dummyDate[strictly_before],relatedDummy.dummyDate[after],relatedDummy.dummyDate[strictly_after],description[exists],relatedDummy.name[exists],dummyBoolean[exists],relatedDummy[exists],dummyFloat,dummyPrice,order[id],order[name],order[relatedDummy.symfony],dummyFloat[between],dummyFloat[gt],dummyFloat[gte],dummyFloat[lt],dummyFloat[lte],dummyPrice[between],dummyPrice[gt],dummyPrice[gte],dummyPrice[lt],dummyPrice[lte],id,id[],name,alias,description,relatedDummy.name,relatedDummy.name[],relatedDummies,relatedDummies[],dummy,relatedDummies.name}",
+              "hydra:template": "/dummies{?dummyBoolean,dummyDate[before],dummyDate[after],relatedDummy.dummyDate[before],relatedDummy.dummyDate[strictly_before],relatedDummy.dummyDate[after],relatedDummy.dummyDate[strictly_after],exists[description],exists[relatedDummy.name],exists[dummyBoolean],exists[relatedDummy],dummyFloat,dummyPrice,order[id],order[name],order[relatedDummy.symfony],dummyFloat[between],dummyFloat[gt],dummyFloat[gte],dummyFloat[lt],dummyFloat[lte],dummyPrice[between],dummyPrice[gt],dummyPrice[gte],dummyPrice[lt],dummyPrice[lte],id,id[],name,alias,description,relatedDummy.name,relatedDummy.name[],relatedDummies,relatedDummies[],dummy,relatedDummies.name}",
               "hydra:variableRepresentation": "BasicRepresentation",
               "hydra:mapping": [
                   {
@@ -890,25 +896,25 @@ Feature: Date filter on collections
                   },
                   {
                       "@type": "IriTemplateMapping",
-                      "variable": "description[exists]",
+                      "variable": "exists[description]",
                       "property": "description",
                       "required": false
                   },
                   {
                       "@type": "IriTemplateMapping",
-                      "variable": "relatedDummy.name[exists]",
+                      "variable": "exists[relatedDummy.name]",
                       "property": "relatedDummy.name",
                       "required": false
                   },
                   {
                     "@type": "IriTemplateMapping",
-                    "variable": "relatedDummy[exists]",
+                    "variable": "exists[relatedDummy]",
                     "property": "relatedDummy",
                     "required": false
                   },
                   {
                       "@type": "IriTemplateMapping",
-                      "variable": "dummyBoolean[exists]",
+                      "variable": "exists[dummyBoolean]",
                       "property": "dummyBoolean",
                       "required": false
                   },

features/doctrine/exists_filter.feature
@@ -6,7 +6,7 @@ Feature: Exists filter on collections
   @createSchema
   Scenario: Get collection where exists does not exist
     Given there are 15 dummy objects with dummyBoolean true
-    When I send a "GET" request to "/dummies?dummyBoolean[exists]=0"
+    When I send a "GET" request to "/dummies?exists[dummyBoolean]=0"
     Then the response status code should be 200
     And the response should be in JSON
     And the header "Content-Type" should be equal to "application/ld+json; charset=utf-8"
@@ -26,7 +26,7 @@ Feature: Exists filter on collections
         "hydra:view": {
           "type": "object",
           "properties": {
-            "@id": {"pattern": "^/dummies\\?dummyBoolean%5Bexists%5D=0$"},
+            "@id": {"pattern": "^/dummies\\?exists%5BdummyBoolean%5D=0$"},
             "@type": {"pattern": "^hydra:PartialCollectionView$"}
           }
         }
@@ -35,7 +35,7 @@ Feature: Exists filter on collections
     """
 
   Scenario: Get collection where exists does exist
-    When I send a "GET" request to "/dummies?dummyBoolean[exists]=1"
+    When I send a "GET" request to "/dummies?exists[dummyBoolean]=1"
     Then the response status code should be 200
     And the response should be in JSON
     And the header "Content-Type" should be equal to "application/ld+json; charset=utf-8"
@@ -55,7 +55,7 @@ Feature: Exists filter on collections
         "hydra:view": {
           "type": "object",
           "properties": {
-            "@id": {"pattern": "^/dummies\\?dummyBoolean%5Bexists%5D=1&page=1$"},
+            "@id": {"pattern": "^/dummies\\?exists%5BdummyBoolean%5D=1&page=1$"},
             "@type": {"pattern": "^hydra:PartialCollectionView$"}
           }
         }

features/graphql/filters.feature
@@ -30,7 +30,7 @@ Feature: Collections filtering
     When I send the following GraphQL request:
     """
     {
-      dummies(relatedDummy: {exists: true}) {
+      dummies(exists: {relatedDummy: true}) {
         edges {
           node {
             id

features/main/crud.feature
@@ -137,7 +137,7 @@ Feature: Create-Retrieve-Update-Delete
       "hydra:totalItems": 1,
       "hydra:search": {
         "@type": "hydra:IriTemplate",
-        "hydra:template": "/dummies{?dummyBoolean,relatedDummy.embeddedDummy.dummyBoolean,dummyDate[before],dummyDate[strictly_before],dummyDate[after],dummyDate[strictly_after],relatedDummy.dummyDate[before],relatedDummy.dummyDate[strictly_before],relatedDummy.dummyDate[after],relatedDummy.dummyDate[strictly_after],description[exists],relatedDummy.name[exists],dummyBoolean[exists],relatedDummy[exists],dummyFloat,dummyFloat[],dummyPrice,dummyPrice[],order[id],order[name],order[description],order[relatedDummy.name],order[relatedDummy.symfony],order[dummyDate],dummyFloat[between],dummyFloat[gt],dummyFloat[gte],dummyFloat[lt],dummyFloat[lte],dummyPrice[between],dummyPrice[gt],dummyPrice[gte],dummyPrice[lt],dummyPrice[lte],id,id[],name,alias,description,relatedDummy.name,relatedDummy.name[],relatedDummies,relatedDummies[],dummy,relatedDummies.name,relatedDummy.thirdLevel.level,relatedDummy.thirdLevel.level[],relatedDummy.thirdLevel.fourthLevel.level,relatedDummy.thirdLevel.fourthLevel.level[],relatedDummy.thirdLevel.badFourthLevel.level,relatedDummy.thirdLevel.badFourthLevel.level[],relatedDummy.thirdLevel.fourthLevel.badThirdLevel.level,relatedDummy.thirdLevel.fourthLevel.badThirdLevel.level[],properties[]}",
+        "hydra:template": "/dummies{?dummyBoolean,relatedDummy.embeddedDummy.dummyBoolean,dummyDate[before],dummyDate[strictly_before],dummyDate[after],dummyDate[strictly_after],relatedDummy.dummyDate[before],relatedDummy.dummyDate[strictly_before],relatedDummy.dummyDate[after],relatedDummy.dummyDate[strictly_after],exists[alias],exists[description],exists[relatedDummy.name],exists[dummyBoolean],exists[relatedDummy],dummyFloat,dummyFloat[],dummyPrice,dummyPrice[],order[id],order[name],order[description],order[relatedDummy.name],order[relatedDummy.symfony],order[dummyDate],dummyFloat[between],dummyFloat[gt],dummyFloat[gte],dummyFloat[lt],dummyFloat[lte],dummyPrice[between],dummyPrice[gt],dummyPrice[gte],dummyPrice[lt],dummyPrice[lte],id,id[],name,alias,description,relatedDummy.name,relatedDummy.name[],relatedDummies,relatedDummies[],dummy,relatedDummies.name,relatedDummy.thirdLevel.level,relatedDummy.thirdLevel.level[],relatedDummy.thirdLevel.fourthLevel.level,relatedDummy.thirdLevel.fourthLevel.level[],relatedDummy.thirdLevel.badFourthLevel.level,relatedDummy.thirdLevel.badFourthLevel.level[],relatedDummy.thirdLevel.fourthLevel.badThirdLevel.level,relatedDummy.thirdLevel.fourthLevel.badThirdLevel.level[],properties[]}",
         "hydra:variableRepresentation": "BasicRepresentation",
         "hydra:mapping": [
           {
@@ -202,25 +202,31 @@ Feature: Create-Retrieve-Update-Delete
           },
           {
             "@type": "IriTemplateMapping",
-            "variable": "description[exists]",
+            "variable": "exists[alias]",
+            "property": "alias",
+            "required": false
+          },
+          {
+            "@type": "IriTemplateMapping",
+            "variable": "exists[description]",
             "property": "description",
             "required": false
           },
           {
             "@type": "IriTemplateMapping",
-            "variable": "relatedDummy.name[exists]",
+            "variable": "exists[relatedDummy.name]",
             "property": "relatedDummy.name",
             "required": false
           },
           {
             "@type": "IriTemplateMapping",
-            "variable": "dummyBoolean[exists]",
+            "variable": "exists[dummyBoolean]",
             "property": "dummyBoolean",
             "required": false
           },
           {
             "@type": "IriTemplateMapping",
-            "variable": "relatedDummy[exists]",
+            "variable": "exists[relatedDummy]",
             "property": "relatedDummy",
             "required": false
           },

src/Bridge/Doctrine/Common/Filter/ExistsFilterTrait.php
@@ -28,6 +28,11 @@ trait ExistsFilterTrait
     use PropertyHelperTrait;
 
     /**
+     * @var string Keyword used to retrieve the value
+     */
+    private $existsParameterName;
+
+    /**
      * {@inheritdoc}
      */
     public function getDescription(string $resourceClass): array
@@ -44,7 +49,7 @@ trait ExistsFilterTrait
                 continue;
             }
 
-            $description[sprintf('%s[%s]', $property, self::QUERY_PARAMETER_KEY)] = [
+            $description[sprintf('%s[%s]', $this->existsParameterName, $property)] = [
                 'property' => $property,
                 'type' => 'bool',
                 'required' => false,
@@ -65,16 +70,24 @@ trait ExistsFilterTrait
 
     private function normalizeValue($value, string $property): ?bool
     {
-        if (\in_array($value[self::QUERY_PARAMETER_KEY], [true, 'true', '1', '', null], true)) {
+        if (\is_array($value) && isset($value[self::QUERY_PARAMETER_KEY])) {
+            @trigger_error(
+                sprintf('The ExistsFilter syntax "%s[exists]=true/false" is deprecated since 2.5. Use the syntax "%s[%s]=true/false" instead.', $property, $this->existsParameterName, $property),
+                E_USER_DEPRECATED
+            );
+            $value = $value[self::QUERY_PARAMETER_KEY];
+        }
+
+        if (\in_array($value, [true, 'true', '1', '', null], true)) {
             return true;
         }
 
-        if (\in_array($value[self::QUERY_PARAMETER_KEY], [false, 'false', '0'], true)) {
+        if (\in_array($value, [false, 'false', '0'], true)) {
             return false;
         }
 
         $this->getLogger()->notice('Invalid filter ignored', [
-            'exception' => new InvalidArgumentException(sprintf('Invalid value for "%s[%s]", expected one of ( "%s" )', $property, self::QUERY_PARAMETER_KEY, implode('" | "', [
+            'exception' => new InvalidArgumentException(sprintf('Invalid value for "%s[%s]", expected one of ( "%s" )', $this->existsParameterName, $property, implode('" | "', [
                 'true',
                 'false',
                 '1',

src/Bridge/Symfony/Bundle/DependencyInjection/ApiPlatformExtension.php
@@ -168,6 +168,7 @@ final class ApiPlatformExtension extends Extension implements PrependExtensionIn
         $container->setParameter('api_platform.eager_loading.max_joins', $config['eager_loading']['max_joins']);
         $container->setParameter('api_platform.eager_loading.fetch_partial', $config['eager_loading']['fetch_partial']);
         $container->setParameter('api_platform.eager_loading.force_eager', $config['eager_loading']['force_eager']);
+        $container->setParameter('api_platform.collection.exists_parameter_name', $config['collection']['exists_parameter_name']);
         $container->setParameter('api_platform.collection.order', $config['collection']['order']);
         $container->setParameter('api_platform.collection.order_parameter_name', $config['collection']['order_parameter_name']);
         $container->setParameter('api_platform.collection.pagination.enabled', $config['collection']['pagination']['enabled']);

commit 8e1ee3474649046c21f080b3527dd9e15c8f481b

Remove a BC break introduced by #2243

src/Bridge/Doctrine/Orm/Filter/ExistsFilter.php
@@ -17,6 +17,7 @@ use ApiPlatform\Core\Bridge\Doctrine\Common\Filter\ExistsFilterInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Common\Filter\ExistsFilterTrait;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryBuilderHelper;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Core\Exception\InvalidArgumentException;
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\ORM\Mapping\ClassMetadataInfo;
 use Doctrine\ORM\Query\Expr\Join;
@@ -44,8 +45,31 @@ class ExistsFilter extends AbstractContextAwareFilter implements ExistsFilterInt
     /**
      * @param RequestStack|null $requestStack No prefix to prevent autowiring of this deprecated property
      */
-    public function __construct(ManagerRegistry $managerRegistry, $requestStack = null, LoggerInterface $logger = null, string $existsParameterName = self::QUERY_PARAMETER_KEY, array $properties = null)
+    public function __construct(ManagerRegistry $managerRegistry, $requestStack = null, LoggerInterface $logger = null, /* string $existsParameterName = self::QUERY_PARAMETER_KEY, array*/ $properties = null)
     {
+        $existsParameterName = self::QUERY_PARAMETER_KEY;
+
+        if (($funcNumArgs = \func_num_args()) > 3) {
+            $fourthArgument = func_get_arg(3);
+            if (4 <= $funcNumArgs && (null === $fourthArgument || \is_array($fourthArgument))) {
+                @trigger_error(sprintf('Passing the "$properties" argument as 4th argument of "%s" is deprecated since API Platform 2.5 and will not be possible anymore in API Platform 3. Pass the new "$existsParameterName" argument as 4th argument and the old "$properties" argument as 5th argument instead.', __CLASS__), E_USER_DEPRECATED);
+                $properties = $fourthArgument;
+            } elseif (\is_string($fourthArgument)) {
+                $existsParameterName = $fourthArgument;
+            } else {
+                throw new InvalidArgumentException(sprintf('The "$existsParameterName" argument of "%s" is expected to be a string.', __CLASS__));
+            }
+        }
+
+        if ($funcNumArgs > 4) {
+            $fifthArgument = func_get_arg(4);
+            if (null === $fifthArgument || \is_array($fifthArgument)) {
+                $properties = $fifthArgument;
+            } else {
+                throw new InvalidArgumentException(sprintf('The "$properties" argument of "%s" is expected to be an array or null.', __CLASS__));
+            }
+        }
+
         parent::__construct($managerRegistry, $requestStack, $logger, $properties);
 
         $this->existsParameterName = $existsParameterName;
@@ -71,8 +95,20 @@ class ExistsFilter extends AbstractContextAwareFilter implements ExistsFilterInt
     /**
      * {@inheritdoc}
      */
-    protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null, array $context = []): void
+    protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null/*, array $context = []*/)
     {
+        if (\func_num_args() > 6) {
+            $context = func_get_arg(6);
+        } else {
+            if (__CLASS__ !== \get_class($this)) {
+                $r = new \ReflectionMethod($this, __FUNCTION__);
+                if (__CLASS__ !== $r->getDeclaringClass()->getName()) {
+                    @trigger_error(sprintf('Method %s() will have a seventh `$context` argument in version API Platform 3.0. Not defining it is deprecated since API Platform 2.5.', __FUNCTION__), E_USER_DEPRECATED);
+                }
+            }
+            $context = [];
+        }
+
         if (
             (($context['exists_deprecated_syntax'] ?? false) && !isset($value[self::QUERY_PARAMETER_KEY])) ||
             !$this->isPropertyEnabled($property, $resourceClass) ||