32064291aa002c2b23edfc8fb4b1a8953387391f
Alan Poulain <contact@alanpoulain.eu>
2019-04-07 10:34:11 +0200

    Test input messenger with MongoDB (#2705)

---
+++ tests/Fixtures/TestBundle/Document/MessengerWithInput.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Core\Tests\Fixtures\TestBundle\Document;
+
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Tests\Fixtures\TestBundle\Dto\MessengerInput;
+
+/**
+ * @ApiResource(messenger="input", input=MessengerInput::class, graphql={"create"={"input"=MessengerInput::class, "messenger"="input"}})
+ */
+class MessengerWithInput
+{
+    /**
+     * @ApiProperty(identifier=true)
+     */
+    public $id;
+    /**
+     * @var string
+     */
+    public $name;
+}
