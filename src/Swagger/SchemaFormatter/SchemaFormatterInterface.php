366a9d9dddf100808069e1c3c31164247b747b27
Peter Smeets <p.e.smeets@gmail.com>
2019-04-26 08:06:28 +0200

    Added dock blocks

--- src/Swagger/SchemaFormatter/SchemaFormatterInterface.php
+++ src/Swagger/SchemaFormatter/SchemaFormatterInterface.php
@@ -17,9 +17,29 @@ use ApiPlatform\Core\Metadata\Property\PropertyMetadata;
 
 interface SchemaFormatterInterface
 {
+    /**
+     * Returns if the mimetype is supported by this formatter.
+     *
+     * @param string $mimeType
+     *
+     * @return bool
+     */
     public function supports(string $mimeType): bool;
 
+    /**
+     * Builds the basic schema if needed for this mimetype.
+     *
+     * @return array
+     */
     public function buildBaseSchemaFormat(): array;
 
+    /**
+     * Sets the property in the correct fields for this mime type.
+     *
+     * @param \ArrayObject $definitionSchema
+     * @param $normalizedPropertyName
+     * @param \ArrayObject $property
+     * @param PropertyMetadata $propertyMetadata
+     */
     public function setProperty(\ArrayObject $definitionSchema, $normalizedPropertyName, \ArrayObject $property, PropertyMetadata $propertyMetadata): void;
 }
