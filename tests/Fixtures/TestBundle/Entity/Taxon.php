c9d9c585de1ceac85831ce630be7853d22fcb0fb
Teoh Han Hui <teohhanhui@gmail.com>
2019-05-25 14:07:38 +0200

    Fix serialization when using interface as resource

---
+++ tests/Fixtures/TestBundle/Entity/Taxon.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity;
+
+use ApiPlatform\Core\Tests\Fixtures\TestBundle\Model\TaxonInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class Taxon implements TaxonInterface
+{
+    /**
+     * @var int|null
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(type="string")
+     */
+    private $code;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCode(?string $code): void
+    {
+        $this->code = $code;
+    }
+}
--- tests/Fixtures/TestBundle/Model/ResourceBarInterface.php
+++ tests/Fixtures/TestBundle/Model/ResourceBarInterface.php
@@ -11,7 +11,7 @@
 
 declare(strict_types=1);
 
-namespace ApiPlatform\Core\Tests\Fixtures\TestBundle\OtherResources;
+namespace ApiPlatform\Core\Tests\Fixtures\TestBundle\Model;
 
 interface ResourceBarInterface
 {
