1ddf3af90ea2c078b19684030267e829bf8adbdd
Kévin Dunglas <dunglas@gmail.com>
2019-08-19 22:54:50 +0200

    [Test] Throw a better error HttpClient is not installed

--- src/Bridge/Symfony/Bundle/DependencyInjection/ApiPlatformExtension.php
+++ src/Bridge/Symfony/Bundle/DependencyInjection/ApiPlatformExtension.php
@@ -46,7 +46,6 @@ use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\Finder\Finder;
-use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Yaml\Yaml;
@@ -129,11 +128,7 @@ final class ApiPlatformExtension extends Extension implements PrependExtensionIn
         $container->registerForAutoconfiguration(FilterInterface::class)
             ->addTag('api_platform.filter');
 
-        if ($container->hasParameter('test.client.parameters') && class_exists(AbstractBrowser::class)) {
-            if (!class_exists(HttpClient::class)) {
-                throw new RuntimeException('Run "composer require --dev symfony/http-client" to use the test client"');
-            }
-
+        if ($container->hasParameter('test.client.parameters') && class_exists(AbstractBrowser::class) && trait_exists('Symfony\Component\HttpClient\HttpClientTrait')) {
             $loader->load('test.xml');
         }
     }
