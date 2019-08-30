56bd72c3fd897e1e1a1515d56794ec774f47fa87
Teoh Han Hui <teohhanhui@gmail.com>
2019-08-30 17:37:20 +0200

    Override test.client service to remove null headers

---
+++ tests/Fixtures/TestBundle/BrowserKit/Client.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Core\Tests\Fixtures\TestBundle\BrowserKit;
+
+use Symfony\Bundle\FrameworkBundle\Client as BaseClient;
+use Symfony\Component\BrowserKit\Request as DomRequest;
+use Symfony\Component\HttpFoundation\Request;
+
+class Client extends BaseClient
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function filterRequest(DomRequest $request): Request
+    {
+        $request = parent::filterRequest($request);
+
+        foreach ($request->headers->all() as $key => $value) {
+            if ([null] === $value) {
+                $request->headers->remove($key);
+            }
+        }
+
+        return $request;
+    }
+}
