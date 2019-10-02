73d85e11992ca71404f5be13050a6dee2fe1a646
Lukas Lücke <lukas@luecke.me>
2019-10-02 15:11:30 +0200

    Only skip root types if resource has no operations
    this will allow access through relations

--- src/GraphQl/Type/TypeConverter.php
+++ src/GraphQl/Type/TypeConverter.php
@@ -105,7 +105,7 @@ final class TypeConverter implements TypeConverterInterface
 
         try {
             $resourceMetadata = $this->resourceMetadataFactory->create($resourceClass);
-            if ([] === ($resourceMetadata->getGraphql() ?? [])) {
+            if (0 === $depth && [] === ($resourceMetadata->getGraphql() ?? [])) {
                 return null;
             }
         } catch (ResourceClassNotFoundException $e) {
