788f203ad034f037cd3df2637879bc8d3ca5c866
Nicolas Grekas <nicolas.grekas@gmail.com>
2019-11-12 17:07:57 +0100

    fix compat with Symfony 5

--- src/Bridge/Symfony/Validator/EventListener/ValidationExceptionListener.php
+++ src/Bridge/Symfony/Validator/EventListener/ValidationExceptionListener.php
@@ -40,7 +40,7 @@ final class ValidationExceptionListener
      */
     public function onKernelException(ExceptionEvent $event): void
     {
-        $exception = $event->getException();
+        $exception = method_exists($event, 'getThrowable') ? $event->getThrowable() : $event->getException();
         if (!$exception instanceof ValidationException) {
             return;
         }
