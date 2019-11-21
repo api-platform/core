ef1f7f4ac666d9f1faf3e426db061cd5cc182c7e
Alan Poulain <contact@alanpoulain.eu>
2019-11-21 14:51:44 +0100

    Fix PHPStan

--- tests/Serializer/AbstractItemNormalizerTest.php
+++ tests/Serializer/AbstractItemNormalizerTest.php
@@ -181,7 +181,7 @@ class AbstractItemNormalizerTest extends TestCase
         ]);
         $normalizer->setSerializer($serializerProphecy->reveal());
 
-        if (!interface_exists(AdvancedNameConverterInterface::class)) {
+        if (!interface_exists(AdvancedNameConverterInterface::class) && method_exists($normalizer, 'setIgnoredAttributes')) {
             $normalizer->setIgnoredAttributes(['alias']);
         }
 
