399cc19e87ec15656fa1de569988a9cce1932e6a
Francois Herbin <f.herbin@noreade.fr>
2020-01-27 15:22:02 +0100

    Revert "Fix just add my name"
    This reverts commit 043818e7

--- tests/Fixtures/TestBundle/Entity/DummyTableNonDoctrineInheritance.php
+++ tests/Fixtures/TestBundle/Entity/DummyTableNonDoctrineInheritance.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the API Platform project.
  *
- * (c) Herbin Francois <francois.rb1@gmail.com>
+ * (c) Kévin Dunglas <dunglas@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
