043818e716d6459b214afdd1ed09e79b5f59d2df
Francois Herbin <f.herbin@noreade.fr>
2020-01-27 15:17:41 +0100

    Fix just add my name

--- tests/Fixtures/TestBundle/Entity/DummyTableNonDoctrineInheritance.php
+++ tests/Fixtures/TestBundle/Entity/DummyTableNonDoctrineInheritance.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the API Platform project.
  *
- * (c) Kévin Dunglas <dunglas@gmail.com>
+ * (c) Herbin Francois <francois.rb1@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
