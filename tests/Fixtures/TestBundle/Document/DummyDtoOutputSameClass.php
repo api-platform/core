d5d1104eb6f8be9c82abd09d00acd2f3067d2d1d
Daniel West <daniel@silverback.is>
2020-04-03 15:16:12 +0100

    Add ODM docs for tests

---
+++ tests/Fixtures/TestBundle/Document/DummyDtoOutputSameClass.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Core\Tests\Fixtures\TestBundle\Document;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+/**
+ * Dummy InputOutput.
+ *
+ * @author Kévin Dunglas <dunglas@gmail.com>
+ *
+ * @ApiResource(attributes={"output"=DummyDtoOutputSameClass::class})
+ * @ODM\Document
+ */
+class DummyDtoOutputSameClass
+{
+    /**
+     * @var int The id
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column
+     */
+    public $lorem;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column
+     */
+    public $ipsum;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+}
