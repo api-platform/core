11109cdd107a43251ea65f4cbbc87bda17b845c8
MartkCz <martk494@gmail.com>
2020-04-04 15:19:19 +0200

    Revert "SerializedName used in message in ValidationExceptionNormalizer"
    This reverts commit 51bc4ba5c66ca97cf15d78613daae5cd89422f3a.

--- src/GraphQl/Serializer/Exception/ValidationExceptionNormalizer.php
+++ src/GraphQl/Serializer/Exception/ValidationExceptionNormalizer.php
@@ -17,7 +17,6 @@ use ApiPlatform\Core\Bridge\Symfony\Validator\Exception\ValidationException;
 use GraphQL\Error\Error;
 use GraphQL\Error\FormattedError;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Serializer\NameConverter\NameConverterInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Validator\ConstraintViolation;
 
@@ -31,13 +30,6 @@ use Symfony\Component\Validator\ConstraintViolation;
  */
 final class ValidationExceptionNormalizer implements NormalizerInterface
 {
-    private $nameConverter;
-
-    public function __construct(NameConverterInterface $nameConverter = null)
-    {
-        $this->nameConverter = $nameConverter;
-    }
-
     /**
      * {@inheritdoc}
      */
@@ -46,7 +38,7 @@ final class ValidationExceptionNormalizer implements NormalizerInterface
         /** @var ValidationException */
         $validationException = $object->getPrevious();
         $error = FormattedError::createFromException($object);
-        $error['message'] = $this->normalizeValidationException($validationException);
+        $error['message'] = $validationException->getMessage();
         $error['extensions']['status'] = Response::HTTP_BAD_REQUEST;
         $error['extensions']['category'] = 'user';
         $error['extensions']['violations'] = [];
@@ -68,25 +60,5 @@ final class ValidationExceptionNormalizer implements NormalizerInterface
     public function supportsNormalization($data, $format = null): bool
     {
         return $data instanceof Error && $data->getPrevious() instanceof ValidationException;
-    }
-
-    private function normalizeValidationException(ValidationException $exception): string
-    {
-        $message = '';
-        foreach ($exception->getConstraintViolationList() as $violation) {
-            if ('' !== $message) {
-                $message .= "\n";
-            }
-
-            $class = \is_object($root = $violation->getRoot()) ? \get_class($root) : null;
-            $propertyPath = $this->nameConverter ? $this->nameConverter->normalize($violation->getPropertyPath(), $class) : $violation->getPropertyPath();
-            if ($propertyPath) {
-                $message .= "$propertyPath: ";
-            }
-
-            $message .= $violation->getMessage();
-        }
-
-        return $message;
     }
 }
