464244462e5479fe503da9104a378edf079a8ef6
Ashura <CoalaJoe@users.noreply.github.com>
2020-04-09 17:40:45 +0200

    Respect method signature in favor of interface

--- src/Bridge/Symfony/Bundle/Test/Client.php
+++ src/Bridge/Symfony/Bundle/Test/Client.php
@@ -84,7 +84,7 @@ final class Client implements HttpClientInterface
      *
      * @return Response
      */
-    public function request(string $method, string $url, array $options = [], array $files = []): ResponseInterface
+    public function request(string $method, string $url, array $options = []): ResponseInterface
     {
         $basic = $options['auth_basic'] ?? null;
         [$url, $options] = self::prepareRequest($method, $url, $options, $this->defaultOptions);
@@ -121,7 +121,8 @@ final class Client implements HttpClientInterface
             'url' => $resolvedUrl,
             'primary_port' => 'http:' === $url['scheme'] ? 80 : 443,
         ];
-        $this->kernelBrowser->request($method, $resolvedUrl, [], $files, $server, $options['body'] ?? null);
+        $extra = $options['extra'] ?? [];
+        $this->kernelBrowser->request($method, $resolvedUrl, [], $extra['files'] ?? [], $server, $options['body'] ?? null);
 
         return $this->response = new Response($this->kernelBrowser->getResponse(), $this->kernelBrowser->getInternalResponse(), $info);
     }
