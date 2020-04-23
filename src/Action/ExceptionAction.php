efa26049ca488a86dab07193d8295fbbd56a7b99
Julien Falque <julien.falque@gmail.com>
2020-04-23 20:31:36 +0200

    Allow defining exception_to_status per operation

--- src/Action/ExceptionAction.php
+++ src/Action/ExceptionAction.php
@@ -13,7 +13,9 @@ declare(strict_types=1);
 
 namespace ApiPlatform\Core\Action;
 
+use ApiPlatform\Core\Metadata\Resource\Factory\ResourceMetadataFactoryInterface;
 use ApiPlatform\Core\Util\ErrorFormatGuesser;
+use ApiPlatform\Core\Util\RequestAttributesExtractor;
 use Symfony\Component\Debug\Exception\FlattenException as LegacyFlattenException;
 use Symfony\Component\ErrorHandler\Exception\FlattenException;
 use Symfony\Component\HttpFoundation\Request;
@@ -31,16 +33,18 @@ final class ExceptionAction
     private $serializer;
     private $errorFormats;
     private $exceptionToStatus;
+    private $resourceMetadataFactory;
 
     /**
      * @param array $errorFormats      A list of enabled error formats
      * @param array $exceptionToStatus A list of exceptions mapped to their HTTP status code
      */
-    public function __construct(SerializerInterface $serializer, array $errorFormats, array $exceptionToStatus = [])
+    public function __construct(SerializerInterface $serializer, array $errorFormats, array $exceptionToStatus = [], ?ResourceMetadataFactoryInterface $resourceMetadataFactory = null)
     {
         $this->serializer = $serializer;
         $this->errorFormats = $errorFormats;
         $this->exceptionToStatus = $exceptionToStatus;
+        $this->resourceMetadataFactory = $resourceMetadataFactory;
     }
 
     /**
@@ -53,7 +57,12 @@ final class ExceptionAction
         $exceptionClass = $exception->getClass();
         $statusCode = $exception->getStatusCode();
 
-        foreach ($this->exceptionToStatus as $class => $status) {
+        $exceptionToStatus = array_merge(
+            $this->exceptionToStatus,
+            $this->getOperationExceptionToStatus($request)
+        );
+
+        foreach ($exceptionToStatus as $class => $status) {
             if (is_a($exceptionClass, $class, true)) {
                 $statusCode = $status;
 
@@ -68,5 +77,27 @@ final class ExceptionAction
         $headers['X-Frame-Options'] = 'deny';
 
         return new Response($this->serializer->serialize($exception, $format['key'], ['statusCode' => $statusCode]), $statusCode, $headers);
+    }
+
+    private function getOperationExceptionToStatus(Request $request): array
+    {
+        $attributes = RequestAttributesExtractor::extractAttributes($request);
+
+        if ([] === $attributes || null === $this->resourceMetadataFactory) {
+            return [];
+        }
+
+        $resourceMetadata = $this->resourceMetadataFactory->create($attributes['resource_class']);
+        $operationExceptionToStatus = $resourceMetadata->getOperationAttribute($attributes, 'exception_to_status', [], false);
+        $resourceExceptionToStatus = $resourceMetadata->getAttribute('exception_to_status', []);
+
+        if (!\is_array($operationExceptionToStatus) || !\is_array($resourceExceptionToStatus)) {
+            throw new \LogicException('"exception_to_status" attribute should be an array.');
+        }
+
+        return array_merge(
+            $resourceExceptionToStatus,
+            $operationExceptionToStatus
+        );
     }
 }
--- tests/Action/ExceptionActionTest.php
+++ tests/Action/ExceptionActionTest.php
@@ -15,7 +15,10 @@ namespace ApiPlatform\Core\Tests\Action;
 
 use ApiPlatform\Core\Action\ExceptionAction;
 use ApiPlatform\Core\Exception\InvalidArgumentException;
+use ApiPlatform\Core\Metadata\Resource\Factory\ResourceMetadataFactoryInterface;
+use ApiPlatform\Core\Metadata\Resource\ResourceMetadata;
 use ApiPlatform\Core\Tests\ProphecyTrait;
+use DomainException;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Debug\Exception\FlattenException as LegacyFlattenException;
 use Symfony\Component\ErrorHandler\Exception\FlattenException;
@@ -55,6 +58,153 @@ class ExceptionActionTest extends TestCase
         $this->assertTrue($response->headers->contains('Content-Type', 'application/problem+json; charset=utf-8'));
         $this->assertTrue($response->headers->contains('X-Content-Type-Options', 'nosniff'));
         $this->assertTrue($response->headers->contains('X-Frame-Options', 'deny'));
+    }
+
+    /**
+     * @dataProvider provideOperationExceptionToStatusCases
+     */
+    public function testActionWithOperationExceptionToStatus(
+        array $globalExceptionToStatus,
+        ?array $resourceExceptionToStatus,
+        ?array $operationExceptionToStatus,
+        int $expectedStatusCode
+    ) {
+        $exception = new DomainException();
+        $flattenException = FlattenException::create($exception);
+
+        $serializer = $this->prophesize(SerializerInterface::class);
+        $serializer->serialize($flattenException, 'jsonproblem', ['statusCode' => $expectedStatusCode])->willReturn();
+
+        $resourceMetadataFactory = $this->prophesize(ResourceMetadataFactoryInterface::class);
+        $resourceMetadataFactory->create('Foo')->willReturn(new ResourceMetadata(
+            'Foo',
+            null,
+            null,
+            [
+                'operation' => null !== $operationExceptionToStatus ? ['exception_to_status' => $operationExceptionToStatus] : [],
+            ],
+            null,
+            null !== $resourceExceptionToStatus ? ['exception_to_status' => $resourceExceptionToStatus] : []
+        ));
+
+        $exceptionAction = new ExceptionAction(
+            $serializer->reveal(),
+            [
+                'jsonproblem' => ['application/problem+json'],
+                'jsonld' => ['application/ld+json'],
+            ],
+            $globalExceptionToStatus,
+            $resourceMetadataFactory->reveal()
+        );
+
+        $request = new Request();
+        $request->setFormat('jsonproblem', 'application/problem+json');
+        $request->attributes->replace([
+            '_api_resource_class' => 'Foo',
+            '_api_item_operation_name' => 'operation',
+        ]);
+
+        $response = $exceptionAction($flattenException, $request);
+
+        $this->assertSame('', $response->getContent());
+        $this->assertSame($expectedStatusCode, $response->getStatusCode());
+        $this->assertTrue($response->headers->contains('Content-Type', 'application/problem+json; charset=utf-8'));
+        $this->assertTrue($response->headers->contains('X-Content-Type-Options', 'nosniff'));
+        $this->assertTrue($response->headers->contains('X-Frame-Options', 'deny'));
+    }
+
+    public function provideOperationExceptionToStatusCases()
+    {
+        yield 'no mapping' => [
+            [],
+            null,
+            null,
+            500,
+        ];
+
+        yield 'on global attributes' => [
+            [DomainException::class => 100],
+            null,
+            null,
+            100,
+        ];
+
+        yield 'on global attributes with empty resource and operation attributes' => [
+            [DomainException::class => 100],
+            [],
+            [],
+            100,
+        ];
+
+        yield 'on global attributes and resource attributes' => [
+            [DomainException::class => 100],
+            [DomainException::class => 200],
+            null,
+            200,
+        ];
+
+        yield 'on global attributes and resource attributes with empty operation attributes' => [
+            [DomainException::class => 100],
+            [DomainException::class => 200],
+            [],
+            200,
+        ];
+
+        yield 'on global attributes and operation attributes' => [
+            [DomainException::class => 100],
+            null,
+            [DomainException::class => 300],
+            300,
+        ];
+
+        yield 'on global attributes and operation attributes with empty resource attributes' => [
+            [DomainException::class => 100],
+            [],
+            [DomainException::class => 300],
+            300,
+        ];
+
+        yield 'on global, resource and operation attributes' => [
+            [DomainException::class => 100],
+            [DomainException::class => 200],
+            [DomainException::class => 300],
+            300,
+        ];
+
+        yield 'on resource attributes' => [
+            [],
+            [DomainException::class => 200],
+            null,
+            200,
+        ];
+
+        yield 'on resource attributes with empty operation attributes' => [
+            [],
+            [DomainException::class => 200],
+            [],
+            200,
+        ];
+
+        yield 'on resource and operation attributes' => [
+            [],
+            [DomainException::class => 200],
+            [DomainException::class => 300],
+            300,
+        ];
+
+        yield 'on operation attributes' => [
+            [],
+            null,
+            [DomainException::class => 300],
+            300,
+        ];
+
+        yield 'on operation attributes with empty resource attributes' => [
+            [],
+            [],
+            [DomainException::class => 300],
+            300,
+        ];
     }
 
     public function testActionWithUncatchableException()
