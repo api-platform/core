98a16d02392326714fadd59d7212ce5feac86295
hubert.lenoir <hubert.lenoir@sensiolabs.com>
2020-07-01 14:28:21 +0200

    Check change tracking deferred for ODM

--- tests/Bridge/Doctrine/Common/DataPersisterTest.php
+++ tests/Bridge/Doctrine/Common/DataPersisterTest.php
@@ -18,6 +18,7 @@ use ApiPlatform\Core\DataPersister\DataPersisterInterface;
 use ApiPlatform\Core\Tests\Fixtures\TestBundle\Entity\Dummy;
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
 use Doctrine\ORM\Mapping\ClassMetadataInfo;
 use PHPUnit\Framework\TestCase;
 use Prophecy\Prediction\CallPrediction;
@@ -117,19 +118,21 @@ class DataPersisterTest extends TestCase
     public function getTrackingPolicyParameters()
     {
         return [
-            'deferred explicit' => [true, true],
-            'deferred implicit' => [false, false],
+            'deferred explicit ORM' => [ClassMetadataInfo::class, true, true],
+            'deferred implicit ORM' => [ClassMetadataInfo::class, false, false],
+            'deferred explicit ODM' => [ClassMetadata::class, true, true],
+            'deferred implicit ODM' => [ClassMetadata::class, false, false],
         ];
     }
 
     /**
      * @dataProvider getTrackingPolicyParameters
      */
-    public function testTrackingPolicy($deferredExplicit, $persisted)
+    public function testTrackingPolicy($metadataClass, $deferredExplicit, $persisted)
     {
         $dummy = new Dummy();
 
-        $classMetadataInfo = $this->prophesize(ClassMetadataInfo::class);
+        $classMetadataInfo = $this->prophesize($metadataClass);
         $classMetadataInfo->isChangeTrackingDeferredExplicit()->willReturn($deferredExplicit)->shouldBeCalled();
 
         $objectManagerProphecy = $this->prophesize(ObjectManager::class);
