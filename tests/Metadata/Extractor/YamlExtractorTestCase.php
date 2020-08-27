4df54c0b5a388ddb39138bbbfdd6458895ac679d
Kévin Dunglas <dunglas@gmail.com>
2020-08-27 16:54:15 +0200

    Use the master version of PHPUnit Bridge

--- tests/Metadata/Extractor/YamlExtractorTestCase.php
+++ tests/Metadata/Extractor/YamlExtractorTestCase.php
@@ -15,7 +15,6 @@ namespace ApiPlatform\Core\Tests\Metadata\Extractor;
 
 use ApiPlatform\Core\Exception\InvalidArgumentException;
 use ApiPlatform\Core\Metadata\Extractor\YamlExtractor;
-use ApiPlatform\Core\Tests\PhpUnitPolyfillTrait;
 use Generator;
 
 /**
@@ -24,8 +23,6 @@ use Generator;
  */
 class YamlExtractorTestCase extends ExtractorTestCase
 {
-    use PhpUnitPolyfillTrait;
-
     public function testInvalidProperty()
     {
         $this->expectException(InvalidArgumentException::class);
