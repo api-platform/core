18884eb47180b7ba62e97d1a27f3f92bcfb38566
Kévin Dunglas <dunglas@gmail.com>
2020-08-28 11:52:16 +0200

    Fix CI

--- tests/Fixtures/app/bootstrap.php
+++ tests/Fixtures/app/bootstrap.php
@@ -16,7 +16,14 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 date_default_timezone_set('UTC');
 
 // PHPUnit's autoloader
-require __DIR__.'/../../../vendor/bin/.phpunit/phpunit-9.2-0/vendor/autoload.php';
+// This glob hack will not be necessary anymore when https://github.com/symfony/symfony/pull/37974 will have been merged.
+$phpunitInstalls = glob(__DIR__.'/../../../vendor/bin/.phpunit/phpunit-*');
+if (!$phpunitInstalls) {
+    die('PHPUnit is not installed. Please run ./vendor/bin/simple-phpunit to install it');
+}
+
+// Use the most recent version available
+require end($phpunitInstalls).'/vendor/autoload.php';
 
 $loader = require __DIR__.'/../../../vendor/autoload.php';
 require __DIR__.'/AppKernel.php';
