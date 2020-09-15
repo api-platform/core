e05018d959f71e152301e375c3378cfbfd127e57
Javier Spagnoletti <phansys@gmail.com>
2020-09-15 10:36:58 -0300

    Apply "static_lambda" CS rule under `src/` dir

--- src/Security/Core/Authorization/ExpressionLanguageProvider.php
+++ src/Security/Core/Authorization/ExpressionLanguageProvider.php
@@ -26,9 +26,9 @@ final class ExpressionLanguageProvider implements ExpressionFunctionProviderInte
     public function getFunctions(): array
     {
         return [
-            new ExpressionFunction('is_granted', function ($attributes, $object = 'null') {
+            new ExpressionFunction('is_granted', static function ($attributes, $object = 'null') {
                 return sprintf('$auth_checker->isGranted(%s, %s)', $attributes, $object);
-            }, function (array $variables, $attributes, $object = null) {
+            }, static function (array $variables, $attributes, $object = null) {
                 return $variables['auth_checker']->isGranted($attributes, $object);
             }),
         ];
