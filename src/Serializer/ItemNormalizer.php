d89c24fec94bf29f8fb52ef7f4d5e38a29e76ac5
Tamás Nagy <nagy.tomi@gmail.com>
2020-10-02 11:03:39 +0200

    Fix undefined index

--- src/Serializer/ItemNormalizer.php
+++ src/Serializer/ItemNormalizer.php
@@ -51,7 +51,7 @@ class ItemNormalizer extends AbstractItemNormalizer
     public function denormalize($data, $class, $format = null, array $context = [])
     {
         // Avoid issues with proxies if we populated the object
-        if (isset($data['id']) && !isset($context[self::OBJECT_TO_POPULATE])) {
+        if (isset($data['@id']) && !isset($context[self::OBJECT_TO_POPULATE])) {
             if (isset($context['api_allow_update']) && true !== $context['api_allow_update']) {
                 throw new NotNormalizableValueException('Update is not allowed for this operation.');
             }
