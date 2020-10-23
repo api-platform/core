ebf2297c79426f99ab9998205036cf9a6f99c4a3
Toby Griffiths <toby@cubicmushroom.co.uk>
2020-10-24 00:44:50 +0100

    Update tests to add coverage

--- tests/DataProvider/TraversablePaginatorTest.php
+++ tests/DataProvider/TraversablePaginatorTest.php
@@ -17,6 +17,7 @@ use ApiPlatform\Core\DataProvider\TraversablePaginator;
 use ArrayIterator;
 use PHPUnit\Framework\TestCase;
 use Traversable;
+use function iterator_to_array;
 
 class TraversablePaginatorTest extends TestCase
 {
@@ -24,35 +25,33 @@ class TraversablePaginatorTest extends TestCase
      * @dataProvider initializeProvider
      */
     public function testInitialize(
-        Traversable $results,
+        array $results,
         float $currentPage,
         float $perPage,
         float $totalItems,
         float $lastPage,
         float $currentItems
-    ) {
-        $paginator = new TraversablePaginator($results, $currentPage, $perPage, $totalItems);
+    ): void {
+        $traversable = new ArrayIterator($results);
+
+        $paginator = new TraversablePaginator($traversable, $currentPage, $perPage, $totalItems);
 
         self::assertEquals($totalItems, $paginator->getTotalItems());
         self::assertEquals($currentPage, $paginator->getCurrentPage());
         self::assertEquals($lastPage, $paginator->getLastPage());
         self::assertEquals($perPage, $paginator->getItemsPerPage());
         self::assertEquals($currentItems, $paginator->count());
+
+        self::assertSame($results, iterator_to_array($paginator));
     }
 
-    public function initializeProvider()
+    public function initializeProvider(): array
     {
-        $data = [
-            'First of three pages of 3 items each' => [[0, 1, 2, 3, 4, 5, 6], 1, 3, 7, 3, 3],
-            'Second of two pages of 3 items for the first page and 2 for the second' => [[0, 1, 2, 3, 4], 2, 3, 5, 2, 2],
+        return [
+            'First of three pages of 3 items each' => [[0, 1, 2], 1, 3, 7, 3, 3],
+            'Second of two pages of 3 items for the first page and 2 for the second' => [[3, 4], 2, 3, 5, 2, 2],
             'Empty results' => [[], 1, 2, 0, 1, 0],
             '0 for max results' => [[0, 1, 2, 3], 1, 0, 4, 1, 4],
         ];
-
-        array_walk($data, static function (&$value) {
-            $value[0] = new ArrayIterator($value[0]);
-        });
-
-        return $data;
     }
 }
