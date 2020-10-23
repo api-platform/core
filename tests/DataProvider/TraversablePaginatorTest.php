f89c9c861f6de3013e3e6ce416ac24e2b76cdb4d
Toby Griffiths <toby@cubicmushroom.co.uk>
2020-10-23 23:02:15 +0100

    Add copyright to test file

--- tests/DataProvider/TraversablePaginatorTest.php
+++ tests/DataProvider/TraversablePaginatorTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Toby Griffiths <toby@cubicmushroom.co.uk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace ApiPlatform\Core\Tests\DataProvider;
