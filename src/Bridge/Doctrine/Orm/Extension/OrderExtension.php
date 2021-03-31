f67233412f381d8fb92f83634465eafdfd04b770
Guilliam Xavier <guilliamxavier@users.noreply.github.com>
2021-03-31 16:01:42 +0200

    Revert using defaults.order as collection.order (#4178)

--- src/Bridge/Doctrine/Orm/Extension/OrderExtension.php
+++ src/Bridge/Doctrine/Orm/Extension/OrderExtension.php
@@ -56,7 +56,7 @@ final class OrderExtension implements ContextAwareQueryCollectionExtensionInterf
             if (empty($defaultOrder)) {
                 $defaultOrder = $this->resourceMetadataFactory->create($resourceClass)->getAttribute('order');
             }
-            if (\is_array($defaultOrder)) {
+            if (null !== $defaultOrder) {
                 foreach ($defaultOrder as $field => $order) {
                     if (\is_int($field)) {
                         // Default direction
