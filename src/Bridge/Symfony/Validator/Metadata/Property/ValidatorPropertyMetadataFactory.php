9feb90207b16f7af026548768da4577542e48ab1
Tomas <norkunas.tom@gmail.com>
2021-04-01 07:30:53 +0300

    Fix parameter name changes when implementing PropertyMetadataFactoryInterface

--- src/Bridge/Symfony/Validator/Metadata/Property/ValidatorPropertyMetadataFactory.php
+++ src/Bridge/Symfony/Validator/Metadata/Property/ValidatorPropertyMetadataFactory.php
@@ -87,9 +87,9 @@ final class ValidatorPropertyMetadataFactory implements PropertyMetadataFactoryI
     /**
      * {@inheritdoc}
      */
-    public function create(string $resourceClass, string $name, array $options = []): PropertyMetadata
+    public function create(string $resourceClass, string $property, array $options = []): PropertyMetadata
     {
-        $propertyMetadata = $this->decorated->create($resourceClass, $name, $options);
+        $propertyMetadata = $this->decorated->create($resourceClass, $property, $options);
 
         $required = $propertyMetadata->isRequired();
         $iri = $propertyMetadata->getIri();
@@ -108,7 +108,7 @@ final class ValidatorPropertyMetadataFactory implements PropertyMetadataFactoryI
         $validationGroups = $this->getValidationGroups($validatorClassMetadata, $options);
         $restrictions = [];
 
-        foreach ($validatorClassMetadata->getPropertyMetadata($name) as $validatorPropertyMetadata) {
+        foreach ($validatorClassMetadata->getPropertyMetadata($property) as $validatorPropertyMetadata) {
             foreach ($this->getPropertyConstraints($validatorPropertyMetadata, $validationGroups) as $constraint) {
                 if (null === $required && $this->isRequired($constraint)) {
                     $required = true;
