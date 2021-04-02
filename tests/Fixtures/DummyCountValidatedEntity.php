0f3974e94d6a3387a66ba533bc9cb7437b942c20
Tomas <norkunas.tom@gmail.com>
2021-04-02 10:32:36 +0300

    Add support for generating property schema with Count restriction

---
+++ tests/Fixtures/DummyCountValidatedEntity.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Core\Tests\Fixtures;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class DummyCountValidatedEntity
+{
+    /**
+     * @var array
+     *
+     * @Assert\Count(min=1)
+     */
+    public $dummyMin;
+
+    /**
+     * @var array
+     *
+     * @Assert\Count(max=10)
+     */
+    public $dummyMax;
+
+    /**
+     * @var array
+     *
+     * @Assert\Count(min=1, max=10)
+     */
+    public $dummyMinMax;
+}
