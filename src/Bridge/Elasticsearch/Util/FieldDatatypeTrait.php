de639c6cc3cda0b0e0976c88fc0514e194ece916
LoicComparet <comparet.loic@gmail.com>
2021-06-17 14:15:37 +0200

    Fix the path of the nested property
    Allows you to filter on nested properties going up the path

--- src/Bridge/Elasticsearch/Util/FieldDatatypeTrait.php
+++ src/Bridge/Elasticsearch/Util/FieldDatatypeTrait.php
@@ -76,7 +76,7 @@ trait FieldDatatypeTrait
         ) {
             $nestedPath = $this->getNestedFieldPath($nextResourceClass, implode('.', $properties));
 
-            return null === $nestedPath ? $nestedPath : "$currentProperty.$nestedPath";
+            return null === $nestedPath ? $currentProperty : "$currentProperty.$nestedPath";
         }
 
         if (
