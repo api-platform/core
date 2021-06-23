6336f6446722cd6728683c36fb70e0074553296d
Guilliam Xavier <guilliamxavier@users.noreply.github.com>
2021-06-23 18:10:31 +0200

    feat(openapi): handle Regex match=false (#4336)

--- src/Bridge/Symfony/Validator/Metadata/Property/Restriction/PropertySchemaRegexRestriction.php
+++ src/Bridge/Symfony/Validator/Metadata/Property/Restriction/PropertySchemaRegexRestriction.php
@@ -26,10 +26,16 @@ class PropertySchemaRegexRestriction implements PropertySchemaRestrictionMetadat
 {
     /**
      * {@inheritdoc}
+     *
+     * @param Regex $constraint
      */
     public function create(Constraint $constraint, PropertyMetadata $propertyMetadata): array
     {
-        return $constraint instanceof Regex && $constraint->getHtmlPattern() ? ['pattern' => '^('.$constraint->getHtmlPattern().')$'] : [];
+        if (null !== ($htmlPattern = $constraint->getHtmlPattern())) {
+            return ['pattern' => '^('.$htmlPattern.')$'];
+        }
+
+        return [];
     }
 
     /**
@@ -37,6 +43,6 @@ class PropertySchemaRegexRestriction implements PropertySchemaRestrictionMetadat
      */
     public function supports(Constraint $constraint, PropertyMetadata $propertyMetadata): bool
     {
-        return $constraint instanceof Regex && $constraint->match;
+        return $constraint instanceof Regex;
     }
 }
