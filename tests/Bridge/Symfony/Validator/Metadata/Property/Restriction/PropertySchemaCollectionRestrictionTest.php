e67a97a7c6c73be817ed7c89fcd430658d94dd24
Guilliam Xavier <guilliamxavier@users.noreply.github.com>
2021-06-23 17:30:35 +0200

    fix(openapi): handle Collection allowMissingFields=true (#4335)

--- tests/Bridge/Symfony/Validator/Metadata/Property/Restriction/PropertySchemaCollectionRestrictionTest.php
+++ tests/Bridge/Symfony/Validator/Metadata/Property/Restriction/PropertySchemaCollectionRestrictionTest.php
@@ -77,44 +77,88 @@ final class PropertySchemaCollectionRestrictionTest extends TestCase
     {
         yield 'empty' => [new Collection(['fields' => []]), new PropertyMetadata(), ['type' => 'object', 'properties' => [], 'additionalProperties' => false]];
 
+        $fields = [
+            'name' => new Required([
+                new NotBlank(),
+            ]),
+            'email' => [
+                new NotNull(),
+                new Length(['min' => 2, 'max' => 255]),
+                new Email(['mode' => Email::VALIDATION_MODE_LOOSE]),
+            ],
+            'phone' => new Optional([
+                new \Symfony\Component\Validator\Constraints\Type(['type' => 'string']),
+                new Regex(['pattern' => '/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\.\/0-9]*$/']),
+            ]),
+            'age' => new Optional([
+                new \Symfony\Component\Validator\Constraints\Type(['type' => 'int']),
+            ]),
+            'social' => new Collection([
+                'fields' => [
+                    'githubUsername' => new NotNull(),
+                ],
+            ]),
+        ];
+        $properties = [
+            'name' => [],
+            'email' => ['format' => 'email'],
+            'phone' => ['pattern' => '^([+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*)$'],
+            'age' => [],
+            'social' => ['type' => 'object', 'properties' => ['githubUsername' => []], 'additionalProperties' => false, 'required' => ['githubUsername']],
+        ];
+        $required = ['name', 'email', 'social'];
+
         yield 'with fields' => [
             new Collection([
-                'allowExtraFields' => true,
-                'fields' => [
-                    'name' => new Required([
-                        new NotBlank(),
-                    ]),
-                    'email' => [
-                        new NotNull(),
-                        new Length(['min' => 2, 'max' => 255]),
-                        new Email(['mode' => Email::VALIDATION_MODE_LOOSE]),
-                    ],
-                    'phone' => new Optional([
-                        new \Symfony\Component\Validator\Constraints\Type(['type' => 'string']),
-                        new Regex(['pattern' => '/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\.\/0-9]*$/']),
-                    ]),
-                    'age' => new Optional([
-                        new \Symfony\Component\Validator\Constraints\Type(['type' => 'int']),
-                    ]),
-                    'social' => new Collection([
-                        'fields' => [
-                            'githubUsername' => new NotNull(),
-                        ],
-                    ]),
-                ],
+                'fields' => $fields,
             ]),
             new PropertyMetadata(),
             [
                 'type' => 'object',
-                'properties' => [
-                    'name' => [],
-                    'email' => ['format' => 'email'],
-                    'phone' => ['pattern' => '^([+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*)$'],
-                    'age' => [],
-                    'social' => ['type' => 'object', 'properties' => ['githubUsername' => []], 'additionalProperties' => false, 'required' => ['githubUsername']],
-                ],
+                'properties' => $properties,
+                'additionalProperties' => false,
+                'required' => $required,
+            ],
+        ];
+
+        yield 'with fields + allowExtraFields' => [
+            new Collection([
+                'fields' => $fields,
+                'allowExtraFields' => true,
+            ]),
+            new PropertyMetadata(),
+            [
+                'type' => 'object',
+                'properties' => $properties,
                 'additionalProperties' => true,
-                'required' => ['name', 'email', 'social'],
+                'required' => $required,
+            ],
+        ];
+
+        yield 'with fields + allowMissingFields' => [
+            new Collection([
+                'fields' => $fields,
+                'allowMissingFields' => true,
+            ]),
+            new PropertyMetadata(),
+            [
+                'type' => 'object',
+                'properties' => $properties,
+                'additionalProperties' => false,
+            ],
+        ];
+
+        yield 'with fields + allowExtraFields + allowMissingFields' => [
+            new Collection([
+                'fields' => $fields,
+                'allowExtraFields' => true,
+                'allowMissingFields' => true,
+            ]),
+            new PropertyMetadata(),
+            [
+                'type' => 'object',
+                'properties' => $properties,
+                'additionalProperties' => true,
             ],
         ];
     }
