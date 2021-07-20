c05483d9483411a5cc221bb906fb6ba59ee6e6f9
Arne De Smedt <arnedesmdt@gmail.com>
2021-07-20 21:53:48 +0200

    fix: add nullable $ref for json schema

--- src/JsonSchema/TypeFactory.php
+++ src/JsonSchema/TypeFactory.php
@@ -169,6 +169,15 @@ final class TypeFactory implements TypeFactoryInterface
         }
 
         if (\array_key_exists('$ref', $jsonSchema)) {
+            if ($schema && Schema::VERSION_JSON_SCHEMA === $schema->getVersion()) {
+                return [
+                    'anyOf' => [
+                        $jsonSchema,
+                        ['type' => 'null'],
+                    ],
+                ];
+            }
+
             return [
                 'nullable' => true,
                 'anyOf' => [$jsonSchema],
