ccc297b44ec4c4dfd0d7f5fa64b71d8d4d1e84d3
Grégoire Pineau <lyrixx@lyrixx.info>
2021-08-17 12:00:10 +0200

    Throw UnexpectedValueException when value is not valid (#4380)

--- tests/Core/Serializer/AbstractItemNormalizerTest.php
+++ tests/Core/Serializer/AbstractItemNormalizerTest.php
@@ -925,7 +925,7 @@ class AbstractItemNormalizerTest extends TestCase
 
     public function testBadType()
     {
-        $this->expectException(InvalidArgumentException::class);
+        $this->expectException(UnexpectedValueException::class);
         $this->expectExceptionMessage('The type of the "foo" attribute must be "float", "integer" given.');
 
         $data = [
