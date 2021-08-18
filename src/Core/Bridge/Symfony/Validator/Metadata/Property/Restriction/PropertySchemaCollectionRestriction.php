dunglas/DunglasApiBundle

fix(openapi): Collection fields VS property metadata

src/Bridge/Symfony/Validator/Metadata/Property/Restriction/PropertySchemaCollectionRestriction.php
@@ -57,7 +57,7 @@ final class PropertySchemaCollectionRestriction implements PropertySchemaRestric
                 $required[] = $field;
             }
 
-            $restriction['properties'][$field] = $this->mergeConstraintRestrictions($baseConstraint, $propertyMetadata);
+            $restriction['properties'][$field] = $this->mergeConstraintRestrictions($baseConstraint, new PropertyMetadata());
         }
 
         if ($required) {

api-platform/core

src/Core/Bridge/Symfony/Validator/Metadata/Property/Restriction/PropertySchemaCollectionRestriction.php
@@ -57,7 +57,7 @@ final class PropertySchemaCollectionRestriction implements PropertySchemaRestric
                 $required[] = $field;
             }
 
-            $restriction['properties'][$field] = $this->mergeConstraintRestrictions($baseConstraint, $propertyMetadata);
+            $restriction['properties'][$field] = $this->mergeConstraintRestrictions($baseConstraint, new ApiProperty());
         }
 
         if ($required) {