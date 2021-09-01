cace631a1b4d335276106b09e016bab01af0cc07
Kévin Dunglas <dunglas@gmail.com>
2021-09-01 17:34:52 +0200

    feat: add a collection state provider for Doctrine ORM

---
+++ src/Bridge/Doctrine/Orm/State/CollectionProvider.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Bridge\Doctrine\Orm\State;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryResultCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGenerator;
+use ApiPlatform\Exception\RuntimeException;
+use ApiPlatform\Metadata\Resource\Factory\ResourceMetadataCollectionFactoryInterface;
+use ApiPlatform\State\ProviderInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * Collection state provider using the Doctrine ORM.
+ *
+ * @author Kévin Dunglas <kevin@dunglas.fr>
+ * @author Samuel ROZE <samuel.roze@gmail.com>
+ */
+final class CollectionProvider implements ProviderInterface
+{
+    private $resourceMetadataCollectionFactory;
+    private $managerRegistry;
+    private $collectionExtensions;
+
+    /**
+     * @param QueryCollectionExtensionInterface[] $collectionExtensions
+     */
+    public function __construct(ResourceMetadataCollectionFactoryInterface $resourceMetadataCollectionFactory, ManagerRegistry $managerRegistry, iterable $collectionExtensions = []) {
+        $this->resourceMetadataCollectionFactory = $resourceMetadataCollectionFactory;
+        $this->managerRegistry = $managerRegistry;
+        $this->collectionExtensions = $collectionExtensions;
+    }
+
+    public function provide(string $resourceClass, array $identifiers = [], ?string $operationName = null, array $context = [])
+    {
+        /** @var EntityManagerInterface $manager */
+        $manager = $this->managerRegistry->getManagerForClass($resourceClass);
+
+        $repository = $manager->getRepository($resourceClass);
+        if (!method_exists($repository, 'createQueryBuilder')) {
+            throw new RuntimeException('The repository class must have a "createQueryBuilder" method.');
+        }
+
+        $queryBuilder = $repository->createQueryBuilder('o');
+        $queryNameGenerator = new QueryNameGenerator();
+        foreach ($this->collectionExtensions as $extension) {
+            $extension->applyToCollection($queryBuilder, $queryNameGenerator, $resourceClass, $operationName, $context);
+
+            if ($extension instanceof QueryResultCollectionExtensionInterface && $extension->supportsResult($resourceClass, $operationName, $context)) {
+                return $extension->getResult($queryBuilder, $resourceClass, $operationName, $context);
+            }
+        }
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    public function supports(string $resourceClass, array $identifiers = [], ?string $operationName = null, array $context = []): bool
+    {
+        if (!$this->managerRegistry->getManagerForClass($resourceClass) instanceof EntityManagerInterface) {
+            return false;
+        }
+
+        $operation = $context['operation'] ?? $this->resourceMetadataCollectionFactory->create($resourceClass)->getOperation($operationName);
+
+        return $operation->isCollection();
+    }
+}
