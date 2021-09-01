b3751243356647caa0cabf46a536ba6b4d07334f
Kévin Dunglas <dunglas@gmail.com>
2021-09-01 22:37:26 +0200

    feat: add an item state provider for Doctrine ORM

---
+++ src/Bridge/Doctrine/Orm/State/ItemProvider.php
@@ -0,0 +1,110 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Bridge\Doctrine\Orm\State;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryResultItemExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGenerator;
+use ApiPlatform\Exception\RuntimeException;
+use ApiPlatform\Metadata\Resource\Factory\ResourceMetadataCollectionFactoryInterface;
+use ApiPlatform\State\ProviderInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Persistence\Mapping\ClassMetadata;
+
+/**
+ * Item state provider using the Doctrine ORM.
+ *
+ * @author Kévin Dunglas <kevin@dunglas.fr>
+ * @author Samuel ROZE <samuel.roze@gmail.com>
+ */
+final class ItemProvider implements ProviderInterface
+{
+    private $resourceMetadataCollectionFactory;
+    private $managerRegistry;
+    private $itemExtensions;
+
+    /**
+     * @param QueryItemExtensionInterface[] $itemExtensions
+     */
+    public function __construct(ResourceMetadataCollectionFactoryInterface $resourceMetadataCollectionFactory, ManagerRegistry $managerRegistry, iterable $itemExtensions = [])
+    {
+        $this->resourceMetadataCollectionFactory = $resourceMetadataCollectionFactory;
+        $this->managerRegistry = $managerRegistry;
+        $this->itemExtensions = $itemExtensions;
+    }
+
+    public function provide(string $resourceClass, array $identifiers = [], ?string $operationName = null, array $context = [])
+    {
+        /** @var EntityManagerInterface $manager */
+        $manager = $this->managerRegistry->getManagerForClass($resourceClass);
+
+        $fetchData = $context['fetch_data'] ?? true;
+        if (!$fetchData) {
+            return $manager->getReference($resourceClass, $identifiers);
+        }
+
+        $repository = $manager->getRepository($resourceClass);
+        if (!method_exists($repository, 'createQueryBuilder')) {
+            throw new RuntimeException('The repository class must have a "createQueryBuilder" method.');
+        }
+
+        $queryBuilder = $repository->createQueryBuilder('o');
+        $queryNameGenerator = new QueryNameGenerator();
+        $doctrineClassMetadata = $manager->getClassMetadata($resourceClass);
+
+        $this->addWhereForIdentifiers($identifiers, $queryBuilder, $doctrineClassMetadata);
+
+        foreach ($this->itemExtensions as $extension) {
+            $extension->applyToItem($queryBuilder, $queryNameGenerator, $resourceClass, $identifiers, $operationName, $context);
+
+            if ($extension instanceof QueryResultItemExtensionInterface && $extension->supportsResult($resourceClass, $operationName, $context)) {
+                return $extension->getResult($queryBuilder, $resourceClass, $operationName, $context);
+            }
+        }
+
+        return $queryBuilder->getQuery()->getOneOrNullResult();
+    }
+
+    public function supports(string $resourceClass, array $identifiers = [], ?string $operationName = null, array $context = []): bool
+    {
+        if (!$this->managerRegistry->getManagerForClass($resourceClass) instanceof EntityManagerInterface) {
+            return false;
+        }
+
+        $operation = $context['operation'] ?? $this->resourceMetadataCollectionFactory->create($resourceClass)->getOperation($operationName);
+
+        return !$operation->isCollection();
+    }
+
+    /**
+     * Add WHERE conditions to the query for one or more identifiers (simple or composite).
+     */
+    private function addWhereForIdentifiers(array $identifiers, QueryBuilder $queryBuilder, ClassMetadata $classMetadata)
+    {
+        $alias = $queryBuilder->getRootAliases()[0];
+        foreach ($identifiers as $identifier => $value) {
+            $placeholder = ':id_'.$identifier;
+            $expression = $queryBuilder->expr()->eq(
+                "{$alias}.{$identifier}",
+                $placeholder
+            );
+
+            $queryBuilder->andWhere($expression);
+
+            $queryBuilder->setParameter($placeholder, $value, $classMetadata->getTypeOfField($identifier));
+        }
+    }
+}
