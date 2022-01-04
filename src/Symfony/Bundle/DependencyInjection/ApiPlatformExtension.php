e416d09de04eebab0f11350e12dafed95e3b9a33
soyuka <soyuka@users.noreply.github.com>
2022-01-04 11:00:00 +0100

    fix: add legacy bridge pagination

--- src/Symfony/Bundle/DependencyInjection/ApiPlatformExtension.php
+++ src/Symfony/Bundle/DependencyInjection/ApiPlatformExtension.php
@@ -839,6 +839,9 @@ final class ApiPlatformExtension extends Extension implements PrependExtensionIn
         $container->removeAlias('api_platform.graphql.schema_builder');
         $container->setAlias('api_platform.graphql.schema_builder', 'api_platform.graphql.schema_builder.legacy');
 
+        $container->removeDefinition('api_platform.pagination');
+        $container->setDefinition('api_platform.pagination', $container->getDefinition('api_platform.pagination.legacy'));
+
         foreach ([
             'api_platform.metadata.property.metadata_factory.serializer',
             'api_platform.route_loader',
