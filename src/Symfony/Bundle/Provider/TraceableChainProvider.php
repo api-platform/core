20f2f559c00ce8c9009c9b713b1284f6781b4e17
Antoine Bluchet <soyuka@users.noreply.github.com>
2022-03-11 16:18:33 +0100

    feat(symfony): debug profiler state provider and processor (#4677)
    * feat(metadata): Show state providers in profiler
    
    * feat(metadata): test state provider tab in profiler
    
    * test: fix cs + requires php 8
    
    Co-authored-by: emmanuel <emmanuel@les-tilleuls.coop>

---
+++ src/Symfony/Bundle/Provider/TraceableChainProvider.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Symfony\Bundle\Provider;
+
+use ApiPlatform\State\ChainProvider;
+use ApiPlatform\State\ProviderInterface;
+
+final class TraceableChainProvider implements ProviderInterface
+{
+    private $providers;
+    private $context;
+    private $providersResponse = [];
+    private $decorated;
+
+    public function __construct(ProviderInterface $provider)
+    {
+        if ($provider instanceof ChainProvider) {
+            $this->decorated = $provider;
+            $this->providers = $provider->providers;
+        }
+    }
+
+    public function getProvidersResponse()
+    {
+        return $this->providersResponse;
+    }
+
+    public function getContext()
+    {
+        return $this->context;
+    }
+
+    private function traceProviders(string $resourceClass, array $identifiers = [], ?string $operationName = null, array $context = [])
+    {
+        foreach ($this->providers as $provider) {
+            $this->providersResponse[\get_class($provider)] = $provider->supports($resourceClass, $identifiers, $operationName, $context);
+        }
+    }
+
+    private function traceContext(array $context)
+    {
+        $this->context = $context;
+    }
+
+    public function provide(string $resourceClass, array $identifiers = [], ?string $operationName = null, array $context = [])
+    {
+        $this->traceProviders($resourceClass, $identifiers, $operationName, $context);
+        $this->traceContext($context);
+
+        return $this->decorated->provide($resourceClass, $identifiers, $operationName, $context);
+    }
+
+    public function supports(string $resourceClass, array $identifiers = [], ?string $operationName = null, array $context = []): bool
+    {
+        return $this->decorated->supports($resourceClass, $identifiers, $operationName, $context);
+    }
+}
