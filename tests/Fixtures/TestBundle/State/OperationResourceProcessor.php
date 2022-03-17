d2967c959687435167b3c102f5b8bcbee5790a36
soyuka <soyuka@users.noreply.github.com>
2022-03-17 16:56:44 +0100

    fix: remove resumable method

--- tests/Fixtures/TestBundle/State/OperationResourceProcessor.php
+++ tests/Fixtures/TestBundle/State/OperationResourceProcessor.php
@@ -33,11 +33,6 @@ final class OperationResourceProcessor implements ProcessorInterface
         $this->managerRegistry = $managerRegistry;
     }
 
-    public function resumable(?string $operationName = null, array $context = []): bool
-    {
-        return false;
-    }
-
     public function supports($data, array $identifiers = [], ?string $operationName = null, array $context = []): bool
     {
         return $data instanceof OperationResource;
