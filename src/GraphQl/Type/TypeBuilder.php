25bee3ce4fb86be622e313b1b6a2fb78892b1f40
Martin Kirilov <wucdbm@gmail.com>
2022-03-22 02:05:51 +0200

    Ensure collection array is not be typed as nullable Ensure collection items are not be as nullable Ensure edges array is not typed nullable Ensure SomethingEdge is not typed nullable Ensure Edge's node field is not typed nullable

--- src/GraphQl/Type/TypeBuilder.php
+++ src/GraphQl/Type/TypeBuilder.php
@@ -24,6 +24,7 @@ use ApiPlatform\State\Pagination\Pagination;
 use GraphQL\Type\Definition\InputObjectType;
 use GraphQL\Type\Definition\InterfaceType;
 use GraphQL\Type\Definition\NonNull;
+use GraphQL\Type\Definition\NullableType;
 use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\Type as GraphQLType;
 use Psr\Container\ContainerInterface;
@@ -257,6 +258,12 @@ final class TypeBuilder implements TypeBuilderInterface
     {
         $shortName = $resourceType->name;
 
+        // TODO Do we need this guard?
+        // TODO If so, what to do with the callable
+        if ($resourceType instanceof NullableType) {
+            $resourceType = GraphQLType::nonNull($resourceType);
+        }
+
         $edgeObjectTypeConfiguration = [
             'name' => "{$shortName}Edge",
             'description' => "Edge of $shortName.",
@@ -265,7 +272,9 @@ final class TypeBuilder implements TypeBuilderInterface
                 'cursor' => GraphQLType::nonNull(GraphQLType::string()),
             ],
         ];
-        $edgeObjectType = new ObjectType($edgeObjectTypeConfiguration);
+        $edgeObjectType = GraphQLType::nonNull(
+            new ObjectType($edgeObjectTypeConfiguration)
+        );
         $this->typesContainer->set("{$shortName}Edge", $edgeObjectType);
 
         $pageInfoObjectTypeConfiguration = [
@@ -282,7 +291,11 @@ final class TypeBuilder implements TypeBuilderInterface
         $this->typesContainer->set("{$shortName}PageInfo", $pageInfoObjectType);
 
         return [
-            'edges' => GraphQLType::listOf($edgeObjectType),
+            'edges' => GraphQLType::nonNull(
+                GraphQLType::listOf(
+                    $edgeObjectType
+                )
+            ),
             'pageInfo' => GraphQLType::nonNull($pageInfoObjectType),
             'totalCount' => GraphQLType::nonNull(GraphQLType::int()),
         ];
@@ -304,10 +317,16 @@ final class TypeBuilder implements TypeBuilderInterface
         $paginationInfoObjectType = new ObjectType($paginationInfoObjectTypeConfiguration);
         $this->typesContainer->set("{$shortName}PaginationInfo", $paginationInfoObjectType);
 
+        // TODO Do we need this guard?
+        // TODO If so, what to do with the callable
+        if ($resourceType instanceof NullableType) {
+            $resourceType = GraphQLType::nonNull($resourceType);
+        }
+
         return [
             'collection' => GraphQLType::nonNull(
                 GraphQLType::listOf(
-                    GraphQLType::nonNull($resourceType)
+                    $resourceType
                 )
             ),
             'paginationInfo' => GraphQLType::nonNull($paginationInfoObjectType),
