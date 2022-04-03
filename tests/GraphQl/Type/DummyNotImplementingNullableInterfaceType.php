7c7e749d7fb63616a9705297f066849ef78a2bf3
Martin Kirilov <wucdbm@gmail.com>
2022-04-03 09:32:39 +0300

    Code style on dummy

--- tests/GraphQl/Type/DummyNotImplementingNullableInterfaceType.php
+++ tests/GraphQl/Type/DummyNotImplementingNullableInterfaceType.php
@@ -1,13 +1,24 @@
-<?php declare(strict_types=1);
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
 
 namespace ApiPlatform\Tests\GraphQl\Type;
 
 use GraphQL\Type\Definition\Type;
 
-class DummyNotImplementingNullableInterfaceType extends Type {
-
-    public function __construct() {
+class DummyNotImplementingNullableInterfaceType extends Type
+{
+    public function __construct()
+    {
         $this->name = 'Dummy';
     }
-
 }
