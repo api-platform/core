98087e5508248cbf8db166723af6addf227b57b1
Daniel West <daniel@silverback.is>
2022-04-04 16:47:38 +0100

    CS fix

--- src/Metadata/Resource/Factory/AttributesResourceMetadataCollectionFactory.php
+++ src/Metadata/Resource/Factory/AttributesResourceMetadataCollectionFactory.php
@@ -31,7 +31,6 @@ use ApiPlatform\Metadata\Post;
 use ApiPlatform\Metadata\Put;
 use ApiPlatform\Metadata\Resource\DeprecationMetadataTrait;
 use ApiPlatform\Metadata\Resource\ResourceMetadataCollection;
-use ApiPlatform\Tests\Fixtures\TestBundle\Entity\PaginationEntity;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
 
