f966ddea3bb719597606d31f92d3cb5f0679da20
Antoine Bluchet <soyuka@users.noreply.github.com>
2022-04-20 15:52:36 +0200

    fix(metadata): delete operation bad constructor (#4721)
    * fix(metadata): delete operation bad constructor
    
    * temp

--- tests/Metadata/Resource/OperationTest.php
+++ tests/Metadata/Resource/OperationTest.php
@@ -14,8 +14,18 @@ declare(strict_types=1);
 namespace ApiPlatform\Tests\Metadata\Resource;
 
 use ApiPlatform\Metadata\CollectionOperationInterface;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\GraphQl\Mutation;
+use ApiPlatform\Metadata\GraphQl\Query;
+use ApiPlatform\Metadata\GraphQl\QueryCollection;
+use ApiPlatform\Metadata\GraphQl\Subscription;
 use ApiPlatform\Metadata\HttpOperation;
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use PHPUnit\Framework\TestCase;
 
 final class OperationTest extends TestCase
@@ -27,5 +37,29 @@ final class OperationTest extends TestCase
         $this->assertEquals($operation->getShortName(), 'test');
         $this->assertEquals($operation->canRead(), false);
         $this->assertEquals($operation instanceof CollectionOperationInterface, true);
+    }
+
+    /**
+     * @dataProvider operationProvider
+     */
+    public function testOperationConstructor(Operation $operation)
+    {
+        $this->assertInstanceOf(Operation::class, $operation);
+    }
+
+    public function operationProvider()
+    {
+        $args = [];
+
+        yield [new Get(...$args)];
+        yield [new GetCollection(...$args)];
+        yield [new Post(...$args)];
+        yield [new Put(...$args)];
+        yield [new Patch(...$args)];
+        yield [new Delete(...$args)];
+        yield [new Query(...$args)];
+        yield [new QueryCollection(...$args)];
+        yield [new Mutation(...$args)];
+        yield [new Subscription(...$args)];
     }
 }
