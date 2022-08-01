29849f8a1b5248c317043bcd003307f042508902
Danny van Wijk <dannyvanwijk@gmail.com>
2022-08-01 14:41:15 +0200

    Fix nullable OpenApiFactoryInterface

--- tests/Documentation/Action/DocumentationActionTest.php
+++ tests/Documentation/Action/DocumentationActionTest.php
@@ -14,7 +14,9 @@ declare(strict_types=1);
 namespace ApiPlatform\Tests\Documentation\Action;
 
 use ApiPlatform\Documentation\Action\DocumentationAction;
+use ApiPlatform\Documentation\Documentation;
 use ApiPlatform\Metadata\Resource\Factory\ResourceNameCollectionFactoryInterface;
+use ApiPlatform\Metadata\Resource\ResourceNameCollection;
 use ApiPlatform\OpenApi\Factory\OpenApiFactoryInterface;
 use ApiPlatform\OpenApi\Model\Info;
 use ApiPlatform\OpenApi\Model\Paths;
@@ -46,11 +48,31 @@ class DocumentationActionTest extends TestCase
         $requestProphecy->query = $queryProphecy->reveal();
         $requestProphecy->getBaseUrl()->willReturn('/api')->shouldBeCalledTimes(1);
         $queryProphecy->getBoolean('api_gateway')->willReturn(true)->shouldBeCalledTimes(1);
-        $attributesProphecy->all()->willReturn(['_api_normalization_context' => ['foo' => 'bar', 'base_url' => '/api', 'api_gateway' => true]])->shouldBeCalledTimes(1);
         $attributesProphecy->get('_api_normalization_context', [])->willReturn(['foo' => 'bar'])->shouldBeCalledTimes(1);
         $attributesProphecy->set('_api_normalization_context', ['foo' => 'bar', 'base_url' => '/api', 'api_gateway' => true])->shouldBeCalledTimes(1);
 
         $documentation = new DocumentationAction($this->prophesize(ResourceNameCollectionFactoryInterface::class)->reveal(), 'my api', '', '1.0.0', $openApiFactoryProphecy->reveal());
         $this->assertInstanceOf(OpenApi::class, $documentation($requestProphecy->reveal()));
+    }
+
+    public function testDocumentationActionWithoutOpenApiFactory(): void
+    {
+        $openApiFactoryProphecy = $this->prophesize(OpenApiFactoryInterface::class);
+        $openApiFactoryProphecy->__invoke(Argument::any())->shouldNotBeCalled();
+        $requestProphecy = $this->prophesize(Request::class);
+        $requestProphecy->getRequestFormat()->willReturn('json');
+        $attributesProphecy = $this->prophesize(ParameterBagInterface::class);
+        $queryProphecy = $this->prophesize(ParameterBag::class);
+        $requestProphecy->attributes = $attributesProphecy->reveal();
+        $requestProphecy->query = $queryProphecy->reveal();
+        $requestProphecy->getBaseUrl()->willReturn('/api')->shouldBeCalledTimes(1);
+        $queryProphecy->getBoolean('api_gateway')->willReturn(true)->shouldBeCalledTimes(1);
+        $attributesProphecy->get('_api_normalization_context', [])->willReturn(['foo' => 'bar'])->shouldBeCalledTimes(1);
+        $attributesProphecy->set('_api_normalization_context', ['foo' => 'bar', 'base_url' => '/api', 'api_gateway' => true])->shouldBeCalledTimes(1);
+        $resourceNameCollectionFactoryProphecy = $this->prophesize(ResourceNameCollectionFactoryInterface::class);
+        $resourceNameCollectionFactoryProphecy->create()->willReturn(new ResourceNameCollection(['dummies']))->shouldBeCalled();
+
+        $documentation = new DocumentationAction($resourceNameCollectionFactoryProphecy->reveal(), 'my api', '', '1.0.0');
+        $this->assertInstanceOf(Documentation::class, $documentation($requestProphecy->reveal()));
     }
 }
