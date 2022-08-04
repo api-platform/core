674f4e4edf6091ee1bc20006f0895341f6b56e31
Danny v W <dannyvanwijk@gmail.com>
2022-08-04 16:39:19 +0200

    chore: cleanup phpstan (#4865)

--- tests/Fixtures/TestBundle/BrowserKit/Client.php
+++ tests/Fixtures/TestBundle/BrowserKit/Client.php
@@ -13,48 +13,25 @@ declare(strict_types=1);
 
 namespace ApiPlatform\Tests\Fixtures\TestBundle\BrowserKit;
 
-use Symfony\Bundle\FrameworkBundle\Client as BaseClient;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
-use Symfony\Component\BrowserKit\AbstractBrowser;
 use Symfony\Component\BrowserKit\Request as DomRequest;
 use Symfony\Component\HttpFoundation\Request;
 
-if (class_exists(AbstractBrowser::class)) {
-    class Client extends KernelBrowser
+class Client extends KernelBrowser
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function filterRequest(DomRequest $request): Request
     {
-        /**
-         * {@inheritdoc}
-         */
-        protected function filterRequest(DomRequest $request): Request
-        {
-            $request = parent::filterRequest($request);
+        $request = parent::filterRequest($request);
 
-            foreach ($request->headers->all() as $key => $value) {
-                if ([null] === $value) {
-                    $request->headers->remove($key);
-                }
+        foreach ($request->headers->all() as $key => $value) {
+            if ([null] === $value) {
+                $request->headers->remove($key);
             }
-
-            return $request;
         }
-    }
-} else {
-    class Client extends BaseClient
-    {
-        /**
-         * {@inheritdoc}
-         */
-        protected function filterRequest(DomRequest $request): Request
-        {
-            $request = parent::filterRequest($request);
 
-            foreach ($request->headers->all() as $key => $value) {
-                if ([null] === $value) {
-                    $request->headers->remove($key);
-                }
-            }
-
-            return $request;
-        }
+        return $request;
     }
 }
