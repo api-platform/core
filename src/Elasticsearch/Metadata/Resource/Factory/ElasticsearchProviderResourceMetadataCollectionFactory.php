4a5122aecab5b15db02b4f092458bb513af49ab0
soyuka <soyuka@users.noreply.github.com>
2022-09-08 13:57:42 +0200

    chore: phpstan issues

--- src/Elasticsearch/Metadata/Resource/Factory/ElasticsearchProviderResourceMetadataCollectionFactory.php
+++ src/Elasticsearch/Metadata/Resource/Factory/ElasticsearchProviderResourceMetadataCollectionFactory.php
@@ -91,8 +91,6 @@ final class ElasticsearchProviderResourceMetadataCollectionFactory implements Re
             return false;
         } catch (NoNodesAvailableException) {
             return false;
-        } catch (NoNodesAvailableException) {
-            return false;
         }
     }
 }
