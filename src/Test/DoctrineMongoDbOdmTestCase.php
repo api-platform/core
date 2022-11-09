27f98e27265ff40d3695ffc4f23a71142b3f5fc3
Antoine Bluchet <soyuka@users.noreply.github.com>
2022-11-09 09:22:33 +0100

    chore: cs fixes (#5173)

--- src/Test/DoctrineMongoDbOdmTestCase.php
+++ src/Test/DoctrineMongoDbOdmTestCase.php
@@ -30,8 +30,8 @@ class DoctrineMongoDbOdmTestCase extends TestCase
     {
         $config = new Configuration();
         $config->setAutoGenerateProxyClasses(Configuration::AUTOGENERATE_FILE_NOT_EXISTS);
-        $config->setProxyDir(\sys_get_temp_dir());
-        $config->setHydratorDir(\sys_get_temp_dir());
+        $config->setProxyDir(sys_get_temp_dir());
+        $config->setHydratorDir(sys_get_temp_dir());
         $config->setProxyNamespace('SymfonyTests\Doctrine');
         $config->setHydratorNamespace('SymfonyTests\Doctrine');
         $config->setMetadataDriverImpl(new AttributeDriver($paths, new AttributeReader())); // @phpstan-ignore-line
