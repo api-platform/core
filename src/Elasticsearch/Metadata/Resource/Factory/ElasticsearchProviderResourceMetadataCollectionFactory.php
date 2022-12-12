47087b8717c78a26599d71137b070d420ea27af4
Tiana <rapaelec@gmail.com>
2022-12-12 17:41:40 +0100

    fix(elasticsearch): check for indices only when enabled
    by default the elasticsearch is not false but null so it will create confusion with indices and try to check the index as the shortName.
    But the shortName by default is the model class and it is not true for every cases , even if the index is different of the class and create an issue

--- src/Elasticsearch/Metadata/Resource/Factory/ElasticsearchProviderResourceMetadataCollectionFactory.php
+++ src/Elasticsearch/Metadata/Resource/Factory/ElasticsearchProviderResourceMetadataCollectionFactory.php
@@ -82,7 +82,7 @@ final class ElasticsearchProviderResourceMetadataCollectionFactory implements Re
 
     private function hasIndices(Operation $operation): bool
     {
-        if (false === $operation->getElasticsearch()) {
+        if (false === $operation->getElasticsearch() || null === $operation->getElasticsearch()) {
             return false;
         }
 
