650dd056c9ed63b128dd9c1c9caf4b9474c6c6a9
Mathieu <mathieu.lechat@les-tilleuls.coop>
2023-01-13 15:07:30 +0100

    chore: homogenize `GetCollection` constructor

--- src/Metadata/GetCollection.php
+++ src/Metadata/GetCollection.php
@@ -16,11 +16,6 @@ namespace ApiPlatform\Metadata;
 #[\Attribute(\Attribute::TARGET_CLASS | \Attribute::IS_REPEATABLE)]
 final class GetCollection extends HttpOperation implements CollectionOperationInterface
 {
-    private $itemUriTemplate;
-
-    /**
-     * {@inheritdoc}
-     */
     public function __construct(
         ?string $uriTemplate = null,
         ?array $types = null,
@@ -42,13 +37,10 @@ final class GetCollection extends HttpOperation implements CollectionOperationIn
         ?string $condition = null,
         ?string $controller = null,
         ?array $cacheHeaders = null,
-        ?array $paginationViaCursor = null,
-
         ?array $hydraContext = null,
         ?array $openapiContext = null,
         ?bool $openapi = null,
         ?array $exceptionToStatus = null,
-
         ?bool $queryParameterValidationEnabled = null,
 
         ?string $shortName = null,
@@ -63,6 +55,7 @@ final class GetCollection extends HttpOperation implements CollectionOperationIn
         ?bool $paginationClientPartial = null,
         ?bool $paginationFetchJoinCollection = null,
         ?bool $paginationUseOutputWalkers = null,
+        ?array $paginationViaCursor = null,
         ?array $order = null,
         ?string $description = null,
         ?array $normalizationContext = null,
@@ -94,10 +87,81 @@ final class GetCollection extends HttpOperation implements CollectionOperationIn
         $provider = null,
         $processor = null,
         array $extraProperties = [],
-        ?string $itemUriTemplate = null
+
+        private ?string $itemUriTemplate = null,
     ) {
-        parent::__construct(self::METHOD_GET, ...\func_get_args());
-        $this->itemUriTemplate = $itemUriTemplate;
+        parent::__construct(
+            self::METHOD_GET,
+            $uriTemplate,
+            $types,
+            $formats,
+            $inputFormats,
+            $outputFormats,
+            $uriVariables,
+            $routePrefix,
+            $routeName,
+            $defaults,
+            $requirements,
+            $options,
+            $stateless,
+            $sunset,
+            $acceptPatch,
+            $status,
+            $host,
+            $schemes,
+            $condition,
+            $controller,
+            $cacheHeaders,
+            $hydraContext,
+            $openapiContext,
+            $openapi,
+            $exceptionToStatus,
+            $queryParameterValidationEnabled,
+            $shortName,
+            $class,
+            $paginationEnabled,
+            $paginationType,
+            $paginationItemsPerPage,
+            $paginationMaximumItemsPerPage,
+            $paginationPartial,
+            $paginationClientEnabled,
+            $paginationClientItemsPerPage,
+            $paginationClientPartial,
+            $paginationFetchJoinCollection,
+            $paginationUseOutputWalkers,
+            $paginationViaCursor,
+            $order,
+            $description,
+            $normalizationContext,
+            $denormalizationContext,
+            $security,
+            $securityMessage,
+            $securityPostDenormalize,
+            $securityPostDenormalizeMessage,
+            $securityPostValidation,
+            $securityPostValidationMessage,
+            $deprecationReason,
+            $filters,
+            $validationContext,
+            $input,
+            $output,
+            $mercure,
+            $messenger,
+            $elasticsearch,
+            $urlGenerationStrategy,
+            $read,
+            $deserialize,
+            $validate,
+            $write,
+            $serialize,
+            $fetchPartial,
+            $forceEager,
+            $priority,
+            $name,
+            $provider,
+            $processor,
+            $extraProperties
+        );
     }
 
     public function getItemUriTemplate(): ?string
