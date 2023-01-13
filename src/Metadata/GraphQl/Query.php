a039f102293bd2d543bee1d321109a1ba018656c
Mathieu <mathieu.lechat@les-tilleuls.coop>
2023-01-13 15:19:48 +0100

    chore: homogenize `Query` constructor

--- src/Metadata/GraphQl/Query.php
+++ src/Metadata/GraphQl/Query.php
@@ -16,15 +16,11 @@ namespace ApiPlatform\Metadata\GraphQl;
 #[\Attribute(\Attribute::TARGET_CLASS | \Attribute::IS_REPEATABLE)]
 class Query extends Operation
 {
-    /**
-     * {@inheritdoc}
-     */
     public function __construct(
         ?string $resolver = null,
         ?array $args = null,
         ?array $links = null,
 
-        // abstract operation arguments
         ?string $shortName = null,
         ?string $class = null,
         ?bool $paginationEnabled = null,
@@ -37,6 +33,7 @@ class Query extends Operation
         ?bool $paginationClientPartial = null,
         ?bool $paginationFetchJoinCollection = null,
         ?bool $paginationUseOutputWalkers = null,
+        ?array $paginationViaCursor = null,
         ?array $order = null,
         ?string $description = null,
         ?array $normalizationContext = null,
@@ -71,8 +68,55 @@ class Query extends Operation
 
         protected ?bool $nested = null,
     ) {
-        parent::__construct(...\func_get_args());
-        $this->name = $name ?: 'item_query';
+        parent::__construct(
+            $resolver,
+            $args,
+            $links,
+            $shortName,
+            $class,
+            $paginationEnabled,
+            $paginationType,
+            $paginationItemsPerPage,
+            $paginationMaximumItemsPerPage,
+            $paginationPartial,
+            $paginationClientEnabled,
+            $paginationClientItemsPerPage,
+            $paginationClientPartial,
+            $paginationFetchJoinCollection,
+            $paginationUseOutputWalkers,
+            $paginationViaCursor,
+            $order,
+            $description,
+            $normalizationContext,
+            $denormalizationContext,
+            $security,
+            $securityMessage,
+            $securityPostDenormalize,
+            $securityPostDenormalizeMessage,
+            $securityPostValidation,
+            $securityPostValidationMessage,
+            $deprecationReason,
+            $filters,
+            $validationContext,
+            $input,
+            $output,
+            $mercure,
+            $messenger,
+            $elasticsearch,
+            $urlGenerationStrategy,
+            $read,
+            $deserialize,
+            $validate,
+            $write,
+            $serialize,
+            $fetchPartial,
+            $forceEager,
+            $priority,
+            $name ?: 'item_query',
+            $provider,
+            $processor,
+            $extraProperties
+        );
     }
 
     public function getNested(): ?bool
