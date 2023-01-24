148d29d747ac3fff6a7acfaf6c8125d108c1bc64
soyuka <soyuka@users.noreply.github.com>
2023-01-24 10:18:27 +0100

    fix(metadata): defaults extra properties

--- src/Metadata/Resource/Factory/OperationDefaultsTrait.php
+++ src/Metadata/Resource/Factory/OperationDefaultsTrait.php
@@ -42,7 +42,7 @@ trait OperationDefaultsTrait
 
     private function addGlobalDefaults(ApiResource|Operation $operation): ApiResource|Operation
     {
-        $extraProperties = [];
+        $extraProperties = $this->defaults['extra_properties'] ?? [];
         foreach ($this->defaults as $key => $value) {
             $upperKey = ucfirst($this->camelCaseToSnakeCaseNameConverter->denormalize($key));
             $getter = 'get'.$upperKey;
