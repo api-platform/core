99e2c9e97f00b6272d2816eda4d0f04565cdc413
soyuka <soyuka@users.noreply.github.com>
2023-01-26 15:50:09 +0100

    chore: phpstan

--- tests/Core/Behat/GraphqlContext.php
+++ tests/Core/Behat/GraphqlContext.php
@@ -141,8 +141,8 @@ final class GraphqlContext implements Context
         $params['operations'] = $string->getRaw();
         $params['map'] = $this->graphqlRequest['map'];
 
-        $this->request->setHttpHeader('Content-type', 'multipart/form-data'); // @phpstan-ignore-line
-        $this->request->send('POST', '/graphql', $params, $this->graphqlRequest['files']); // @phpstan-ignore-line
+        $this->request->setHttpHeader('Content-type', 'multipart/form-data');
+        $this->request->send('POST', '/graphql', $params, $this->graphqlRequest['files']);
     }
 
     /**
@@ -159,7 +159,7 @@ final class GraphqlContext implements Context
      */
     public function theGraphQLFieldIsDeprecatedForTheReason(string $fieldName, string $reason)
     {
-        foreach (json_decode($this->request->getContent(), true)['data']['__type']['fields'] as $field) { // @phpstan-ignore-line
+        foreach (json_decode($this->request->getContent(), true)['data']['__type']['fields'] as $field) {
             if ($fieldName === $field['name'] && $field['isDeprecated'] && $reason === $field['deprecationReason']) {
                 return;
             }
