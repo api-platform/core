e10cd1a803f480e7bbc26c66c056a044976b0535
Antoine Bluchet <soyuka@users.noreply.github.com>
2023-02-27 19:00:42 +0100

    ci: fix tests (#5422)
    * ci: fix tests
    
    * remove doctrine annotations

--- tests/Fixtures/app/bootstrap.php
+++ tests/Fixtures/app/bootstrap.php
@@ -11,8 +11,6 @@
 
 declare(strict_types=1);
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
-
 date_default_timezone_set('UTC');
 
 // PHPUnit's autoloader
@@ -27,7 +25,5 @@ $phpunitLoader->register();
 
 $loader = require __DIR__.'/../../../vendor/autoload.php';
 require __DIR__.'/AppKernel.php';
-
-AnnotationRegistry::registerLoader('class_exists');
 
 return $loader;
