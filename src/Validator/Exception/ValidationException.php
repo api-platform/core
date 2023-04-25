70062fd4a38d2a1587dbb86f1037c634f62c3707
Pierre Rebeilleau <49146882+PierreRebeilleau@users.noreply.github.com>
2023-04-25 14:27:38 +0200

    chore(validation): api-platform/validator (#5529)

--- src/Validator/Exception/ValidationException.php
+++ src/Validator/Exception/ValidationException.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 
 namespace ApiPlatform\Validator\Exception;
 
-use ApiPlatform\Exception\RuntimeException;
+use ApiPlatform\Metadata\Exception\RuntimeException;
 
 /**
  * Thrown when a validation error occurs.
