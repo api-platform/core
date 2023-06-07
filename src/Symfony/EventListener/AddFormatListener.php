cfc12190847e2a07c1b1d5fc4ac035993b780121
Andrey Lebedev <andrew.lebedev@gmail.com>
2023-06-07 16:23:42 +0400

    BUG: Error 500 on request with 'empty' Accept headers, e.g. 'Accept: 0' or 'Accept:   '
    (cherry picked from commit a04688a78554fed85e696820ef79a9765bacfc3d)

--- src/Symfony/EventListener/AddFormatListener.php
+++ src/Symfony/EventListener/AddFormatListener.php
@@ -73,7 +73,7 @@ final class AddFormatListener
         // First, try to guess the format from the Accept header
         /** @var string|null $accept */
         $accept = $request->headers->get('Accept');
-        if (null !== $accept) {
+        if (!empty($accept)) {
             if (null === $mediaType = $this->negotiator->getBest($accept, $mimeTypes)) {
                 throw $this->getNotAcceptableHttpException($accept, $flattenedMimeTypes);
             }
