b6acbceb8b0f6aadbe869853461df16f4795dc47
Antoine Bluchet <soyuka@users.noreply.github.com>
2023-06-13 02:33:21 -0700

    style: maker bundle now applies php-cs-fixer (#5628)

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -16,7 +16,6 @@ $finder = PhpCsFixer\Finder::create()
     ->exclude([
         'src/Core/Bridge/Symfony/Maker/Resources/skeleton',
         'tests/Fixtures/app/var',
-        'tests/Fixtures/Symfony/Maker',
     ])
     ->notPath('src/Symfony/Bundle/DependencyInjection/Configuration.php')
     ->notPath('src/Annotation/ApiFilter.php') // temporary
