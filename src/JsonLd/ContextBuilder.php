5d9ec80786fbf6c01480911afa37efa950989985
Romain <romain.herault@les-tilleuls.coop>
2023-07-18 11:15:37 +0200

    fix(jsonld): check if docs enabled when generate vocab in context

--- src/JsonLd/ContextBuilder.php
+++ src/JsonLd/ContextBuilder.php
@@ -35,7 +35,7 @@ final class ContextBuilder implements AnonymousContextBuilderInterface
 
     public const FORMAT = 'jsonld';
 
-    public function __construct(private readonly ResourceNameCollectionFactoryInterface $resourceNameCollectionFactory, private readonly ResourceMetadataCollectionFactoryInterface $resourceMetadataFactory, private readonly PropertyNameCollectionFactoryInterface $propertyNameCollectionFactory, private readonly PropertyMetadataFactoryInterface $propertyMetadataFactory, private readonly UrlGeneratorInterface $urlGenerator, private readonly ?IriConverterInterface $iriConverter = null, private readonly ?NameConverterInterface $nameConverter = null)
+    public function __construct(private readonly ResourceNameCollectionFactoryInterface $resourceNameCollectionFactory, private readonly ResourceMetadataCollectionFactoryInterface $resourceMetadataFactory, private readonly PropertyNameCollectionFactoryInterface $propertyNameCollectionFactory, private readonly PropertyMetadataFactoryInterface $propertyMetadataFactory, private readonly UrlGeneratorInterface $urlGenerator, private readonly ?IriConverterInterface $iriConverter = null, private readonly ?NameConverterInterface $nameConverter = null, private readonly bool $enableDocs = true)
     {
     }
 
@@ -44,10 +44,15 @@ final class ContextBuilder implements AnonymousContextBuilderInterface
      */
     public function getBaseContext(int $referenceType = UrlGeneratorInterface::ABS_URL): array
     {
-        return [
-            '@vocab' => $this->urlGenerator->generate('api_doc', ['_format' => self::FORMAT], UrlGeneratorInterface::ABS_URL).'#',
-            'hydra' => self::HYDRA_NS,
+        $context = [
+            'hydra' => self::HYDRA_NS
         ];
+
+        if($this->enableDocs) {
+            $context['@vocab'] = $this->urlGenerator->generate('api_doc', ['_format' => self::FORMAT], UrlGeneratorInterface::ABS_URL).'#';
+        }
+
+        return $context;
     }
 
     /**
