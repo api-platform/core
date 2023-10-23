1d49e2764614c102db65c678cac30010b43126ff
soyuka <soyuka@users.noreply.github.com>
2023-10-23 10:21:15 +0200

    fix(symfony): swagger ui should use base url
    fixes #5904

---
+++ tests/Symfony/Bundle/SwaggerUi/SwaggerUiProviderTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Tests\Symfony\Bundle\SwaggerUi;
+
+use ApiPlatform\Documentation\Documentation;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\OpenApi\Factory\OpenApiFactoryInterface;
+use ApiPlatform\OpenApi\Model\Info;
+use ApiPlatform\OpenApi\Model\Paths;
+use ApiPlatform\OpenApi\OpenApi;
+use ApiPlatform\State\ProviderInterface;
+use ApiPlatform\Symfony\Bundle\SwaggerUi\SwaggerUiProvider;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpFoundation\Request;
+
+class SwaggerUiProviderTest extends TestCase
+{
+    public function testProvideWithBaseUrl(): void
+    {
+        $openapiFactory = $this->createMock(OpenApiFactoryInterface::class);
+        $request = $this->createStub(Request::class);
+        $request->attributes = new ParameterBag();
+        $request->method('getRequestFormat')->willReturn('html');
+        $request->method('getBaseUrl')->willReturn('test');
+        $decorated = $this->createStub(ProviderInterface::class);
+        $provider = new SwaggerUiProvider($decorated, $openapiFactory);
+        $openapiFactory->expects($this->once())->method('__invoke')->with(['base_url' => 'test'])->willReturn(new OpenApi(new Info('test', '1'), [], new Paths()));
+        $provider->provide(new Get(class: Documentation::class), [], ['request' => $request]);
+    }
+}
