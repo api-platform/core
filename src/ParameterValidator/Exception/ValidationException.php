718d67ae26b37bfb9738bf750224c232c3bd2bb2
soyuka <soyuka@users.noreply.github.com>
2024-01-04 09:17:01 +0100

    fix(parametervalidator): throws problem exception

--- src/ParameterValidator/Exception/ValidationException.php
+++ src/ParameterValidator/Exception/ValidationException.php
@@ -13,20 +13,55 @@ declare(strict_types=1);
 
 namespace ApiPlatform\ParameterValidator\Exception;
 
+use ApiPlatform\Metadata\Exception\ProblemExceptionInterface;
+
 /**
  * Filter validation exception.
  *
  * @author Julien DENIAU <julien.deniau@gmail.com>
  */
-final class ValidationException extends \Exception implements ValidationExceptionInterface
+final class ValidationException extends \Exception implements ValidationExceptionInterface, ProblemExceptionInterface
 {
+    /**
+     * @param string[] $constraintViolationList
+     */
     public function __construct(private readonly array $constraintViolationList, string $message = '', int $code = 0, \Exception $previous = null)
     {
         parent::__construct($message ?: $this->__toString(), $code, $previous);
     }
 
+    public function getConstraintViolationList(): array
+    {
+        return $this->constraintViolationList;
+    }
+
     public function __toString(): string
     {
         return implode("\n", $this->constraintViolationList);
+    }
+
+    public function getType(): string
+    {
+        return '/parameter_validation/'.$this->code;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->message ?: $this->__toString();
+    }
+
+    public function getStatus(): ?int
+    {
+        return 400;
+    }
+
+    public function getDetail(): ?string
+    {
+        return $this->message ?: $this->__toString();
+    }
+
+    public function getInstance(): ?string
+    {
+        return null;
     }
 }
