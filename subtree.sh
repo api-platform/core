4cbf57167bd403432314119a42a83b7377e260fd
aegypius <innercircle@aegypius.com>
2024-01-25 09:58:45 +0100

    ci: make subtree split fail if any command fails

--- subtree.sh
+++ subtree.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+set -e # Exit immediately if a command exits with a non-zero status.
 set -x
 
-# Subtree split on tag this script gets called using find:
-# find src -maxdepth 2 -name composer.json -print0 | xargs -I '{}' -n 1 -0 bash subtree.sh {} refs/tags/3.1.5
-# find src -maxdepth 2 -name composer.json -print0 | xargs -I '{}' -n 1 -0 bash subtree.sh {} refs/heads/3.1
-# See the subtree workflow
-package=$(jq -r .name $1)
-directory=$(dirname $1)
-repository="https://github.com/$package"
-git remote add $package $repository
-sha=$(splitsh-lite --prefix=$directory)
-git push $package $sha:$2
+GITHUB_REF="$1"
+
+if [[ -z "${GITHUB_REF}" ]]; then
+    echo "Usage: $0 GITHUB_REF"
+    exit 129
+fi
+
+while IFS= read -r -d '' file
+do
+
+  package=$(jq -r .name "${file}")
+  directory=$(dirname "${file}")
+  repository="https://github.com/${package}"
+
+  git remote add "${package}" "${repository}"
+  sha=$(splitsh-lite --prefix="${directory}")
+  git push "${package}" "${sha}:${GITHUB_REF}"
+
+done < <(find src -maxdepth 3 -name composer.json -not -path '*/vendor/*' -print0)
