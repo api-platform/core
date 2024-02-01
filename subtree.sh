7e05f453f137375a9c50d2e426ecfaf8a9b8d38b
Antoine Bluchet <soyuka@users.noreply.github.com>
2024-02-01 15:18:36 +0100

    ci: subtree split exit code (#6128)

--- subtree.sh
+++ subtree.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+set -xe
 
 # Subtree split on tag this script gets called using find:
 # find src -maxdepth 2 -name composer.json -print0 | xargs -I '{}' -n 1 -0 bash subtree.sh {} refs/tags/3.1.5
