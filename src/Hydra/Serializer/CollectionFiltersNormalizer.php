674a1203ea8934dc623ce931b8ee64b022229a24
aegypius <innercircle@aegypius.com>
2024-02-12 17:52:38 +0000

    fixup! fix(hydra): restore legacy filter support

--- src/Hydra/Serializer/CollectionFiltersNormalizer.php
+++ src/Hydra/Serializer/CollectionFiltersNormalizer.php
@@ -162,7 +162,7 @@ final class CollectionFiltersNormalizer implements NormalizerInterface, Normaliz
     /**
      * Gets a filter with a backward compatibility.
      */
-    private function getFilter(string $filterId): FilterInterface|null
+    private function getFilter(string $filterId): LegacyFilterInterface|FilterInterface|null
     {
         if ($this->filterLocator && $this->filterLocator->has($filterId)) {
             return $this->filterLocator->get($filterId);
