8499dff561b2278e285621f8dc327182de9a159f
Stephen Fegel <stephen@bouwpas.nl>
2024-03-17 11:06:53 +0100

    fix class_exisist calls without argument

--- src/Api/QueryParameterValidator/Validator/Pattern.php
+++ src/Api/QueryParameterValidator/Validator/Pattern.php
@@ -34,6 +34,6 @@ final class Pattern implements ValidatorInterface
     }
 }
 
-if (!class_exists()) {
+if (!class_exists(\ApiPlatform\Core\Filter\Validator\Pattern::class)) {
     class_alias(Pattern::class, \ApiPlatform\Core\Filter\Validator\Pattern::class);
 }
