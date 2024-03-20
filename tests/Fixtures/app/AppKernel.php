5a2600f22b6a38b58c21079456564c447fd7bf20
Antoine Bluchet <soyuka@users.noreply.github.com>
2024-03-20 16:15:28 +0100

    chore: doctrine deprecations (#6237)

--- tests/Fixtures/app/AppKernel.php
+++ tests/Fixtures/app/AppKernel.php
@@ -273,6 +273,7 @@ class AppKernel extends Kernel
             $c->prependExtensionConfig('doctrine', [
                 'orm' => [
                     'report_fields_where_declared' => true,
+                    'controller_resolver' => ['auto_mapping' => true],
                     'enable_lazy_ghost_objects' => true,
                 ],
             ]);
