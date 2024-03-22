2b3b267dba7dfa6a18e30bc7d2ddcd31d1d1fe38
soyuka <soyuka@users.noreply.github.com>
2024-03-22 09:52:28 +0100

    fix(serializer): collection property in an output dto
    fixes #6211

---
+++ tests/Fixtures/TestBundle/ApiResource/Issue6211/ArrayPropertyDtoOperation.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Tests\Fixtures\TestBundle\ApiResource\Issue6211;
+
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Tests\Fixtures\TestBundle\Dto\ArrayPropertyDto;
+
+#[Get(provider: [ArrayPropertyDtoOperation::class, 'provide'], output: ArrayPropertyDto::class)]
+class ArrayPropertyDtoOperation
+{
+    public static function provide(): ArrayPropertyDto
+    {
+        $d = new ArrayPropertyDto();
+        $d->name = 'test';
+        $c = new ArrayPropertyDto();
+        $c->name = 'test2';
+        $d->greetings = [$c];
+
+        return $d;
+    }
+}
