52322048cfccdb68ddf2df6a7952efb3e23ffece
soyuka <soyuka@users.noreply.github.com>
2024-03-26 19:09:21 +0100

    feat(state): provide parameter values

---
+++ src/State/ParameterProviderInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\Metadata\Parameter;
+
+/**
+ * Optionnaly transforms request parameters and provides modification to the current Operation.
+ *
+ * @experimental
+ */
+interface ParameterProviderInterface
+{
+    /**
+     * @param array<string, mixed>                                                                         $parameters
+     * @param array<string, mixed>|array{request?: Request, resource_class?: string, operation: Operation} $context
+     */
+    public function provide(Parameter $parameter, array $parameters = [], array $context = []): ?Operation;
+}
