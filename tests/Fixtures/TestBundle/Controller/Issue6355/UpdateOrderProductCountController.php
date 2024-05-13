0b29435e02f3aaaabf3fbff9a08061a1cb697fc5
soyuka <soyuka@users.noreply.github.com>
2024-05-13 09:51:05 +0200

    test: PATCH on controller w/ dto

--- tests/Fixtures/TestBundle/Controller/Issue6355/UpdateOrderProductCountController.php
+++ tests/Fixtures/TestBundle/Controller/Issue6355/UpdateOrderProductCountController.php
@@ -14,7 +14,6 @@ declare(strict_types=1);
 namespace ApiPlatform\Tests\Fixtures\TestBundle\Controller\Issue6355;
 
 use ApiPlatform\Tests\Fixtures\TestBundle\ApiResource\Issue6355\OrderDto;
-use ApiPlatform\Tests\Fixtures\TestBundle\ApiResource\Issue6355\OrderProductCount;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Attribute\AsController;
@@ -22,7 +21,7 @@ use Symfony\Component\HttpKernel\Attribute\AsController;
 #[AsController]
 class UpdateOrderProductCountController extends AbstractController
 {
-    public function __invoke(OrderProductCount $data, Request $request): OrderDto
+    public function __invoke(): OrderDto
     {
         $dto = new OrderDto();
         $dto->id = 1;
