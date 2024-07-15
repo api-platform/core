84df4678618869f439a7995a2534440c031e10f3
Antoine Bluchet <soyuka@users.noreply.github.com>
2024-07-15 10:40:10 +0200

    fix(jsonld): remove request uri variables when denormalizing output (#6467)

---
+++ tests/Functional/JsonLd.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Tests\Functional;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use ApiPlatform\Tests\Fixtures\TestBundle\Entity\Issue6465\Bar;
+use ApiPlatform\Tests\Fixtures\TestBundle\Entity\Issue6465\Foo;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\SchemaTool;
+
+class JsonLd extends ApiTestCase
+{
+    /**
+     * The input DTO denormalizes an existing Doctrine entity.
+     */
+    public function testIssue6465(): void
+    {
+        $container = static::getContainer();
+        if ('mongodb' === $container->getParameter('kernel.environment')) {
+            $this->markTestSkipped();
+        }
+
+        $response = self::createClient()->request('POST', '/foo/1/validate', [
+            'json' => ['bar' => '/bar6465s/2'],
+        ]);
+
+        $res = $response->toArray();
+        $this->assertEquals('Bar two', $res['title']);
+    }
+
+    protected function setUp(): void
+    {
+        self::bootKernel();
+
+        $container = static::getContainer();
+        $registry = $container->get('doctrine');
+        $manager = $registry->getManager();
+        if (!$manager instanceof EntityManagerInterface) {
+            return;
+        }
+
+        $classes = [];
+        foreach ([Foo::class, Bar::class] as $entityClass) {
+            $classes[] = $manager->getClassMetadata($entityClass);
+        }
+
+        $schemaTool = new SchemaTool($manager);
+        @$schemaTool->createSchema($classes);
+
+        $foo = new Foo();
+        $foo->title = 'Foo';
+        $manager->persist($foo);
+        $bar = new Bar();
+        $bar->title = 'Bar one';
+        $manager->persist($bar);
+        $bar2 = new Bar();
+        $bar2->title = 'Bar two';
+        $manager->persist($bar2);
+        $manager->flush();
+    }
+
+    protected function tearDown(): void
+    {
+        $container = static::getContainer();
+        $registry = $container->get('doctrine');
+        $manager = $registry->getManager();
+        if (!$manager instanceof EntityManagerInterface) {
+            return;
+        }
+
+        $classes = [];
+        foreach ([Foo::class, Bar::class] as $entityClass) {
+            $classes[] = $manager->getClassMetadata($entityClass);
+        }
+
+        $schemaTool = new SchemaTool($manager);
+        @$schemaTool->dropSchema($classes);
+        parent::tearDown();
+    }
+}
