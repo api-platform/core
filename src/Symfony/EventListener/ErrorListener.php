b80a48cd440b2a2cd25f1b5a7200820b360b2b92
soyuka <soyuka@users.noreply.github.com>
2024-09-09 11:10:17 +0200

    fix(hydra): error hydra prefix

--- src/Symfony/EventListener/ErrorListener.php
+++ src/Symfony/EventListener/ErrorListener.php
@@ -110,7 +110,7 @@ final class ErrorListener extends SymfonyErrorListener
             $operation = $operation->withProvider('api_platform.state.error_provider');
         }
 
-        $normalizationContext = $operation->getNormalizationContext() ?? [];
+        $normalizationContext = ($operation->getNormalizationContext() ?? []) + ($apiOperation?->getNormalizationContext() ?? []);
         if (!($normalizationContext['api_error_resource'] ?? false)) {
             $normalizationContext += ['api_error_resource' => true];
         }
