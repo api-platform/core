2549a10ea3678066308d600ed43e7e8fd38fc349
soyuka <soyuka@users.noreply.github.com>
2024-09-12 10:41:59 +0200

    refactor(metadata): move parameter validation to the validator component

--- src/Metadata/Resource/Factory/ParameterResourceMetadataCollectionFactory.php
+++ src/Metadata/Resource/Factory/ParameterResourceMetadataCollectionFactory.php
@@ -15,33 +15,17 @@ namespace ApiPlatform\Metadata\Resource\Factory;
 
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\FilterInterface;
-use ApiPlatform\Metadata\HttpOperation;
 use ApiPlatform\Metadata\JsonSchemaFilterInterface;
 use ApiPlatform\Metadata\OpenApiParameterFilterInterface;
 use ApiPlatform\Metadata\Parameter;
 use ApiPlatform\Metadata\Parameters;
 use ApiPlatform\Metadata\Property\Factory\PropertyMetadataFactoryInterface;
 use ApiPlatform\Metadata\Property\Factory\PropertyNameCollectionFactoryInterface;
-use ApiPlatform\Metadata\QueryParameter;
 use ApiPlatform\Metadata\Resource\ResourceMetadataCollection;
 use ApiPlatform\OpenApi\Model\Parameter as OpenApiParameter;
 use ApiPlatform\Serializer\Filter\FilterInterface as SerializerFilterInterface;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Serializer\NameConverter\NameConverterInterface;
-use Symfony\Component\Validator\Constraints\Choice;
-use Symfony\Component\Validator\Constraints\Count;
-use Symfony\Component\Validator\Constraints\DivisibleBy;
-use Symfony\Component\Validator\Constraints\GreaterThan;
-use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\LessThan;
-use Symfony\Component\Validator\Constraints\LessThanOrEqual;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\NotNull;
-use Symfony\Component\Validator\Constraints\Regex;
-use Symfony\Component\Validator\Constraints\Type;
-use Symfony\Component\Validator\Constraints\Unique;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * Prepares Parameters documentation by reading its filter details and declaring an OpenApi parameter.
@@ -67,8 +51,6 @@ final class ParameterResourceMetadataCollectionFactory implements ResourceMetada
         $properties = [];
         foreach ($this->propertyNameCollectionFactory->create($resourceClass) as $i => $property) {
             $propertyMetadata = $this->propertyMetadataFactory->create($resourceClass, $property);
-            if ('author' === $property) {
-            }
             if ($propertyMetadata->isReadable()) {
                 $propertyNames[] = $property;
                 $properties[$property] = $propertyMetadata;
@@ -87,7 +69,7 @@ final class ParameterResourceMetadataCollectionFactory implements ResourceMetada
                             $converted = $this->nameConverter?->denormalize($property) ?? $property;
                             $propertyParameter = $this->setDefaults($converted, $parameter, $resourceClass, $properties);
                             $priority = $propertyParameter->getPriority() ?? $internalPriority--;
-                            $parameters->add($converted, $this->addFilterMetadata($propertyParameter->withPriority($priority)->withKey($converted)));
+                            $parameters->add($converted, $propertyParameter->withPriority($priority)->withKey($converted));
                         }
 
                         $parameters->remove($key, $parameter::class);
@@ -107,12 +89,7 @@ final class ParameterResourceMetadataCollectionFactory implements ResourceMetada
 
                     $parameter = $this->setDefaults($key, $parameter, $resourceClass, $properties);
                     $priority = $parameter->getPriority() ?? $internalPriority--;
-                    $parameters->add($key, $this->addFilterMetadata($parameter->withPriority($priority)));
-                }
-
-                // As we deprecate the parameter validator, we declare a parameter for each filter transfering validation to the new system
-                if ($operation->getFilters() && 0 === $parameters->count()) {
-                    $parameters = $this->addFilterValidation($operation);
+                    $parameters->add($key, $parameter->withPriority($priority));
                 }
 
                 if (\count($parameters) > 0) {
@@ -222,119 +199,6 @@ final class ParameterResourceMetadataCollectionFactory implements ResourceMetada
             $parameter = $parameter->withOpenApi($openApi);
         }
 
-        $schema = $parameter->getSchema() ?? (($openApi = $parameter->getOpenApi()) ? $openApi->getSchema() : null);
-
-        // Only add validation if the Symfony Validator is installed
-        if (interface_exists(ValidatorInterface::class) && !$parameter->getConstraints()) {
-            $parameter = $this->addSchemaValidation($parameter, $schema, $parameter->getRequired() ?? $description['required'] ?? false, $parameter->getOpenApi() ?: null);
-        }
-
-        return $parameter;
-    }
-
-    private function addSchemaValidation(Parameter $parameter, ?array $schema = null, bool $required = false, ?OpenApiParameter $openApi = null): Parameter
-    {
-        $assertions = [];
-
-        if ($required && false !== ($allowEmptyValue = $openApi?->getAllowEmptyValue())) {
-            $assertions[] = new NotNull(message: \sprintf('The parameter "%s" is required.', $parameter->getKey()));
-        }
-
-        if (false === ($allowEmptyValue ?? $openApi?->getAllowEmptyValue())) {
-            $assertions[] = new NotBlank(allowNull: !$required);
-        }
-
-        if (isset($schema['exclusiveMinimum'])) {
-            $assertions[] = new GreaterThan(value: $schema['exclusiveMinimum']);
-        }
-
-        if (isset($schema['exclusiveMaximum'])) {
-            $assertions[] = new LessThan(value: $schema['exclusiveMaximum']);
-        }
-
-        if (isset($schema['minimum'])) {
-            $assertions[] = new GreaterThanOrEqual(value: $schema['minimum']);
-        }
-
-        if (isset($schema['maximum'])) {
-            $assertions[] = new LessThanOrEqual(value: $schema['maximum']);
-        }
-
-        if (isset($schema['pattern'])) {
-            $assertions[] = new Regex($schema['pattern']);
-        }
-
-        if (isset($schema['maxLength']) || isset($schema['minLength'])) {
-            $assertions[] = new Length(min: $schema['minLength'] ?? null, max: $schema['maxLength'] ?? null);
-        }
-
-        if (isset($schema['minItems']) || isset($schema['maxItems'])) {
-            $assertions[] = new Count(min: $schema['minItems'] ?? null, max: $schema['maxItems'] ?? null);
-        }
-
-        if (isset($schema['multipleOf'])) {
-            $assertions[] = new DivisibleBy(value: $schema['multipleOf']);
-        }
-
-        if ($schema['uniqueItems'] ?? false) {
-            $assertions[] = new Unique();
-        }
-
-        if (isset($schema['enum'])) {
-            $assertions[] = new Choice(choices: $schema['enum']);
-        }
-
-        if (isset($schema['type']) && 'array' === $schema['type']) {
-            $assertions[] = new Type(type: 'array');
-        }
-
-        if (!$assertions) {
-            return $parameter;
-        }
-
-        if (1 === \count($assertions)) {
-            return $parameter->withConstraints($assertions[0]);
-        }
-
-        return $parameter->withConstraints($assertions);
-    }
-
-    private function addFilterValidation(HttpOperation $operation): Parameters
-    {
-        $parameters = new Parameters();
-        $internalPriority = -1;
-
-        foreach ($operation->getFilters() as $filter) {
-            if (!$this->filterLocator->has($filter)) {
-                continue;
-            }
-
-            $filter = $this->filterLocator->get($filter);
-            foreach ($filter->getDescription($operation->getClass()) as $parameterName => $definition) {
-                $key = $parameterName;
-                $required = $definition['required'] ?? false;
-                $schema = $definition['schema'] ?? null;
-
-                $openApi = null;
-                if (isset($definition['openapi']) && $definition['openapi'] instanceof OpenApiParameter) {
-                    $openApi = $definition['openapi'];
-                }
-
-                // The query parameter validator forced this, lets maintain BC on filters
-                if (true === $required && !$openApi) {
-                    $openApi = new OpenApiParameter(name: $key, in: 'query', allowEmptyValue: false);
-                }
-
-                $parameters->add($key, $this->addSchemaValidation(
-                    // we disable openapi and hydra on purpose as their docs comes from filters see the condition for addFilterValidation above
-                    new QueryParameter(key: $key, property: $definition['property'] ?? null, priority: $internalPriority--, schema: $schema, openApi: false, hydra: false),
-                    $schema,
-                    $required,
-                    $openApi
-                ));
-            }
-        }
-
-        return $parameters;
+        return $this->addFilterMetadata($parameter);
     }
 }
