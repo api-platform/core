afc3997a07f69e5cad65c8d1c4851795fbd4f42a
soyuka <soyuka@users.noreply.github.com>
2024-10-04 16:47:40 +0200

    chore: improve changelog generator

--- generate-changelog.sh
+++ generate-changelog.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 # usage: generate-changelog.sh previous_tag next_tag
 # example: generate-changelog.sh v2.7.2 v2.7.3 > CHANGELOG.new.md
-log=$(git log "$1..HEAD" --pretty='format:* [%h](https://github.com/api-platform/core/commit/%H) %s' --no-merges)
+lowerbranch=$(git branch --merged HEAD | grep '[[:digit:]]\.[[:digit:]]' | grep -v '*' | sort -rg | head -n 1)
+log=$(git log "$1..HEAD" --no-merges --not $lowerbranch --pretty='format:* [%h](https://github.com/api-platform/core/commit/%H) %s')
 
 diff=$(
 printf "# Changelog\n\n"
 printf "## %s\n\n" "$2"
 
-if [[ 0 != $(echo "$log" | grep fix | grep -v chore | wc -l) ]];
+fixes=$(echo "$log" | grep 'fix(\|fix:')
+if [[ 0 != $(echo "$fixes" | wc -l) ]];
 then
     printf "### Bug fixes\n\n"
-    printf "$log" | grep fix | grep -v chore | sort
+    printf "$fixes" | sort
     printf "\n\n"
 fi
 
-if [[ 0 != $(echo "$log" | grep feat | grep -v chore | wc -l) ]];
+feat=$(echo "$log" | grep 'feat(\|feat:')
+if [[ 0 != $(echo "$feat" | wc -l) ]];
 then
     printf "### Features\n\n"
-    printf "$log" | grep feat | grep -v chore | sort
+    printf "$feat" | sort
 fi
 )
 
