3ca599158139d56fbd6ee66f2de3e586120d735c
Antoine Bluchet <soyuka@users.noreply.github.com>
2024-10-09 11:00:06 +0200

    fix(hydra): hydra_prefix on errors (#6704)
    * fix(hydra): hydra_prefix on errors
    
    * doc: remove warning from changelog
    
    * review

---
+++ src/JsonLd/Serializer/ErrorNormalizer.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\JsonLd\Serializer;
+
+use ApiPlatform\State\ApiResource\Error;
+use ApiPlatform\Symfony\Validator\Exception\ValidationException as SymfonyValidationException;
+use ApiPlatform\Validator\Exception\ValidationException;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+final class ErrorNormalizer implements NormalizerInterface
+{
+    use HydraPrefixTrait;
+
+    public function __construct(private readonly NormalizerInterface $inner, private readonly array $defaultContext = [])
+    {
+    }
+
+    public function normalize(mixed $object, ?string $format = null, array $context = []): array|string|int|float|bool|\ArrayObject|null
+    {
+        $normalized = $this->inner->normalize($object, $format, $context);
+        $hydraPrefix = $this->getHydraPrefix($context + $this->defaultContext);
+        if (!$hydraPrefix) {
+            return $normalized;
+        }
+
+        if (isset($normalized['description'])) {
+            $normalized['hydra:description'] = $normalized['description'];
+        }
+
+        if (isset($normalized['title'])) {
+            $normalized['hydra:title'] = $normalized['title'];
+        }
+
+        return $normalized;
+    }
+
+    public function supportsNormalization(mixed $data, ?string $format = null, array $context = []): bool
+    {
+        return $this->inner->supportsNormalization($data, $format, $context)
+            && (is_a($data, Error::class) || is_a($data, ValidationException::class) || is_a($data, SymfonyValidationException::class));
+    }
+
+    public function getSupportedTypes(?string $format): array
+    {
+        return $this->inner->getSupportedTypes($format);
+    }
+}
