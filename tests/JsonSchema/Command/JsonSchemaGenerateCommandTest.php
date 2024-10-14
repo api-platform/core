550f283f78c8789c874ad5deb8f94a1df03cc423
Martin Chudoba <martin.chudoba@seznam.cz>
2024-10-14 11:57:35 +0200

    fix(jsonschema): handle @id when genId is false
    add test

--- tests/JsonSchema/Command/JsonSchemaGenerateCommandTest.php
+++ tests/JsonSchema/Command/JsonSchemaGenerateCommandTest.php
@@ -365,4 +365,12 @@ class JsonSchemaGenerateCommandTest extends KernelTestCase
             $properties['genders']
         );
     }
+
+    public function testGenId(): void
+    {
+        $this->tester->run(['command' => 'api:json-schema:generate', 'resource' => 'ApiPlatform\Tests\Fixtures\TestBundle\Entity\DisableIdGeneration', '--type' => 'output', '--format' => 'jsonld']);
+        $result = $this->tester->getDisplay();
+        $json = json_decode($result, associative: true);
+        $this->assertArrayNotHasKey('@id', $json['definitions']['DisableIdGenerationItem.jsonld']['properties']);
+    }
 }
