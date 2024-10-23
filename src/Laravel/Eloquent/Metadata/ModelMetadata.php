b6068fc52b61b045c6ebbe10dbd17a3c86c9caa7
Amer Chaudhary <amermchaudry@gmail.com>
2024-10-23 18:33:41 +0000

    perf(laravel) fixing the return type for phpstan

--- src/Laravel/Eloquent/Metadata/ModelMetadata.php
+++ src/Laravel/Eloquent/Metadata/ModelMetadata.php
@@ -66,11 +66,11 @@ final class ModelMetadata
         $connection = $model->getConnection();
         $schema = $connection->getSchemaBuilder();
         $table = $model->getTable();
-        /** @var array $columns */
+        /** @var array<int, mixed> $columns */
         $columns = Cache::flexible('api-platform.tables.'.$table, [5, 10], function () use ($schema, $table) {
             return $schema->getColumns($table);
         });
-        /** @var array $indexes */
+        /** @var array<int, mixed> $indexes */
         $indexes = Cache::flexible('api-platform.indexes.'.$table, [5, 10], function () use ($schema, $table) {
             return $schema->getIndexes($table);
         });
