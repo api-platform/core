f251ed152d8c3709cef177a6ce5e0f34c7abb680
soyuka <soyuka@users.noreply.github.com>
2024-11-15 10:41:19 +0100

    chore: phpstan fixes

--- tests/Functional/HALCircularReference.php
+++ tests/Functional/HALCircularReference.php
@@ -14,23 +14,13 @@ declare(strict_types=1);
 namespace ApiPlatform\Tests\Functional;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
-use ApiPlatform\Tests\Fixtures\TestBundle\ApiResource\Issue4358\ResourceA;
-use ApiPlatform\Tests\Fixtures\TestBundle\ApiResource\Issue4358\ResourceB;
-use ApiPlatform\Tests\SetupClassResourcesTrait;
 
 class HALCircularReference extends ApiTestCase
 {
-    use SetupClassResourcesTrait;
-
     public function testIssue4358(): void
     {
         $r1 = self::createClient()->request('GET', '/resource_a', ['headers' => ['Accept' => 'application/hal+json']]);
         self::assertResponseIsSuccessful();
         self::assertEquals('{"_links":{"self":{"href":"\/resource_a"},"b":{"href":"\/resource_b"}},"_embedded":{"b":{"_links":{"self":{"href":"\/resource_b"},"a":{"href":"\/resource_a"}},"_embedded":{"a":{"_links":{"self":{"href":"\/resource_a"}}}}}}}', $r1->getContent());
-    }
-
-    public static function getResources(): array
-    {
-        return [ResourceA::class, ResourceB::class];
     }
 }
