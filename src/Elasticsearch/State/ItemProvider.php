79eb593f48628a803d74807ea0ef38ab93f349e9
darthf1 <17253332+darthf1@users.noreply.github.com>
2024-11-26 09:56:36 +0100

    feat(elasticsearch): introduce v7 support

--- src/Elasticsearch/State/ItemProvider.php
+++ src/Elasticsearch/State/ItemProvider.php
@@ -69,7 +69,7 @@ final class ItemProvider implements ProviderInterface
             throw new Error(status: $response->getStatusCode(), detail: (string) $response->getBody(), title: $response->getReasonPhrase(), originalTrace: $e->getTrace());
         }
 
-        if ($this->client instanceof Client && $documents instanceof Elasticsearch) {
+        if (class_exists(Elastic\Elasticsearch\Response\Elasticsearch::class) && $documents instanceof Elasticsearch) {
             $document = $document->asArray();
         }
 
