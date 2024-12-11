f2b2d72ecba8b4523df85b4ef51c367327b8f0fe
soyuka <soyuka@users.noreply.github.com>
2024-12-11 12:06:37 +0100

    test(laravel): class AddAssetSymlinkFolders from TestBench namespace

--- src/Laravel/workbench/app/Providers/WorkbenchServiceProvider.php
+++ src/Laravel/workbench/app/Providers/WorkbenchServiceProvider.php
@@ -18,8 +18,8 @@ use Illuminate\Console\View\Components\Factory;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
 use Orchestra\Testbench\Foundation\Events\ServeCommandStarted;
+use Orchestra\Testbench\Workbench\Actions\AddAssetSymlinkFolders;
 use Orchestra\Testbench\Workbench\Workbench;
-use Orchestra\Workbench\Listeners\AddAssetSymlinkFolders;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\NullOutput;
 
@@ -40,7 +40,7 @@ class WorkbenchServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $assets = new AddAssetSymlinkFolders(Workbench::configuration(), $this->app->make(Filesystem::class));
+        $assets = new AddAssetSymlinkFolders($this->app->make(Filesystem::class), Workbench::configuration());
         $input = new ArrayInput([]);
         $output = new NullOutput();
         $assets->handle(new ServeCommandStarted($input, $output, new Factory(new OutputStyle($input, $output))));
