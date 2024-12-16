e0da7569112ccb8af159d31cc7bbf35897d37eb5
soyuka <soyuka@users.noreply.github.com>
2024-12-16 15:31:31 +0100

    fix(laravel): jsonapi query parameters (page, sort, fields and include)

---
+++ src/JsonApi/Filter/SparseFieldset.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\JsonApi\Filter;
+
+use ApiPlatform\Metadata\JsonSchemaFilterInterface;
+use ApiPlatform\Metadata\OpenApiParameterFilterInterface;
+use ApiPlatform\Metadata\Parameter as MetadataParameter;
+use ApiPlatform\Metadata\ParameterProviderFilterInterface;
+use ApiPlatform\Metadata\PropertiesFilterInterface;
+use ApiPlatform\Metadata\QueryParameter;
+use ApiPlatform\OpenApi\Model\Parameter;
+
+final class SparseFieldset implements OpenApiParameterFilterInterface, JsonSchemaFilterInterface, ParameterProviderFilterInterface, PropertiesFilterInterface
+{
+    public function getSchema(MetadataParameter $parameter): array
+    {
+        return [
+            'type' => 'array',
+            'items' => [
+                'type' => 'string',
+            ],
+        ];
+    }
+
+    public function getOpenApiParameters(MetadataParameter $parameter): Parameter|array|null
+    {
+        $example = \sprintf(
+            '%1$s[]={propertyName}&%1$s[]={anotherPropertyName}',
+            $parameter->getKey()
+        );
+
+        return new Parameter(
+            name: $parameter->getKey().'[]',
+            in: $parameter instanceof QueryParameter ? 'query' : 'header',
+            description: 'Allows you to reduce the response to contain only the properties you need. If your desired property is nested, you can address it using nested arrays. Example: '.$example
+        );
+    }
+
+    public static function getParameterProvider(): string
+    {
+        return SparseFieldsetParameterProvider::class;
+    }
+}
