bcfcdf84628236a0b02afb9e330dfb39259dcc2d
LaurentHuzard <laurent@les-tilleuls.coop>
2025-01-14 15:37:20 +0100

    feat(state): add headers to comply with LDP specification
    To ensure compliance with the LDP specification (https://www.w3.org/TR/ldp/):
    - Added the "Accept-Post" header with the value "text/turtle, application/ld+json".
    - Added the "Allow" header with values based on the allowed operations on the queried resources.

---
+++ tests/Fixtures/TestBundle/ApiResource/DummyGetPostDeleteOperation.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Tests\Fixtures\TestBundle\ApiResource;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+
+#[ApiResource(operations: [new Get(), new GetCollection(), new Post(), new Delete()])]
+class DummyGetPostDeleteOperation
+{
+    public ?int $id;
+
+    public ?string $name = null;
+}
