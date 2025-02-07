5124d8c571bc4324aa060e4ff808d48f0ffa8d73
Jon Erickson <jon@deschutesdesigngroup.com>
2025-02-07 02:32:52 -0800

    fix(laravel): Prevent overwriting existing routes on the router (#6941)
    * Prevent overwriting existing routes on the router
    * Move routes to its own routes file and add domain support
    * Fix tests and stan. Run cs fixer
    * Revert laravel pint changes

--- src/Laravel/config/api-platform.php
+++ src/Laravel/config/api-platform.php
@@ -10,6 +10,7 @@ return [
     'version' => '1.0.0',
 
     'routes' => [
+        'domain' => null,
         // Global middleware applied to every API Platform routes
         // 'middleware' => []
     ],
@@ -43,20 +44,20 @@ return [
         'pagination_enabled' => true,
         'pagination_partial' => false,
         'pagination_client_enabled' => false,
-		'pagination_client_items_per_page' => false,
-		'pagination_client_partial' => false,
-		'pagination_items_per_page' => 30,
-		'pagination_maximum_items_per_page' => 30,
+        'pagination_client_items_per_page' => false,
+        'pagination_client_partial' => false,
+        'pagination_items_per_page' => 30,
+        'pagination_maximum_items_per_page' => 30,
         'route_prefix' => '/api',
         'middleware' => [],
     ],
 
-	'pagination' => [
-		'page_parameter_name' => 'page',
-		'enabled_parameter_name' => 'pagination',
-		'items_per_page_parameter_name' => 'itemsPerPage',
-		'partial_parameter_name' => 'partial',
-	],
+    'pagination' => [
+        'page_parameter_name' => 'page',
+        'enabled_parameter_name' => 'pagination',
+        'items_per_page_parameter_name' => 'itemsPerPage',
+        'partial_parameter_name' => 'partial',
+    ],
 
     'graphql' => [
         'enabled' => false,
