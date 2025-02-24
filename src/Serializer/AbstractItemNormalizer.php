f408b494baa9ac5944c626ad0553f5d79b579c07
Aleksey Polyvanyi <alexndlm@gmail.com>
2025-02-24 13:33:53 +0100

    fix(serializer): throw NotNormalizableValueException if resource not found or invalid IRI

--- src/Serializer/AbstractItemNormalizer.php
+++ src/Serializer/AbstractItemNormalizer.php
@@ -247,29 +247,13 @@ abstract class AbstractItemNormalizer extends AbstractObjectNormalizer
                     throw new UnexpectedValueException($e->getMessage(), $e->getCode(), $e);
                 }
 
-                throw NotNormalizableValueException::createForUnexpectedDataType(
-                    \sprintf('The type of the "%s" resource "string" (IRI), "%s" given.', $resourceClass, \gettype($data)),
-                    $data,
-                    [$resourceClass],
-                    $context['deserialization_path'] ?? null,
-                    true,
-                    $e->getCode(),
-                    $e
-                );
+                throw NotNormalizableValueException::createForUnexpectedDataType(\sprintf('The type of the "%s" resource "string" (IRI), "%s" given.', $resourceClass, \gettype($data)), $data, [$resourceClass], $context['deserialization_path'] ?? null, true, $e->getCode(), $e);
             } catch (InvalidArgumentException $e) {
                 if (!isset($context['not_normalizable_value_exceptions'])) {
                     throw new UnexpectedValueException(\sprintf('Invalid IRI "%s".', $data), $e->getCode(), $e);
                 }
 
-                throw NotNormalizableValueException::createForUnexpectedDataType(
-                    \sprintf('The type of the "%s" resource "string" (IRI), "%s" given.', $resourceClass, \gettype($data)),
-                    $data,
-                    [$resourceClass],
-                    $context['deserialization_path'] ?? null,
-                    true,
-                    $e->getCode(),
-                    $e
-                );
+                throw NotNormalizableValueException::createForUnexpectedDataType(\sprintf('The type of the "%s" resource "string" (IRI), "%s" given.', $resourceClass, \gettype($data)), $data, [$resourceClass], $context['deserialization_path'] ?? null, true, $e->getCode(), $e);
             }
         }
 
