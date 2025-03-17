55943d57f6c1d6df3cc2499c142a2916dbc92982
Vincent Chalamon <407859+vincentchalamon@users.noreply.github.com>
2025-03-17 12:38:18 +0100

    feat: support Laravel on update-js.sh

--- update-js.sh
+++ update-js.sh
@@ -2,10 +2,12 @@
 rm -f package.lock
 rm -f package-lock.json
 echo "{}" > package.json
-// /!\ UMD is removed since react@19: https://react.dev/blog/2024/04/25/react-19-upgrade-guide#umd-builds-removed
+# /!\ UMD is removed since react@19: https://react.dev/blog/2024/04/25/react-19-upgrade-guide#umd-builds-removed
 npm i @fontsource/open-sans swagger-ui es6-promise fetch react@18 react-dom@18 graphiql graphql-playground-react redoc
 
-dest=src/Symfony/Bundle/Resources/public/fonts/open-sans/
+for public in src/Symfony/Bundle/Resources/public/ src/Laravel/public/; do
+
+dest="${public}fonts/open-sans/"
 if [[ -d "$dest" ]]; then
 rm -Rf "$dest"
 fi
@@ -53,7 +55,7 @@ cp node_modules/@fontsource/open-sans/files/open-sans-hebrew-700-normal.woff "${
 cp node_modules/@fontsource/open-sans/files/open-sans-math-700-normal.woff "${dest}files/"
 cp node_modules/@fontsource/open-sans/files/open-sans-symbols-700-normal.woff "${dest}files/"
 
-dest=src/Symfony/Bundle/Resources/public/swagger-ui/
+dest="${public}swagger-ui/"
 if [[ -d "$dest" ]]; then
 rm -Rf "$dest"
 fi
@@ -64,7 +66,7 @@ cp node_modules/swagger-ui/dist/swagger-ui.css "$dest"
 cp node_modules/swagger-ui/dist/swagger-ui.css.map "$dest"
 cp node_modules/swagger-ui/dist/oauth2-redirect.html "$dest"
 
-dest=src/Symfony/Bundle/Resources/public/react/
+dest="${public}react/"
 if [[ -d "$dest" ]]; then
 rm -Rf "$dest"
 fi
@@ -72,7 +74,7 @@ mkdir -p "$dest"
 cp node_modules/react/umd/react.production.min.js "$dest"
 cp node_modules/react-dom/umd/react-dom.production.min.js "$dest"
 
-dest=src/Symfony/Bundle/Resources/public/graphiql/
+dest="${public}graphiql/"
 if [[ -d "$dest" ]]; then
 rm -Rf "$dest"
 fi
@@ -80,7 +82,7 @@ mkdir -p "$dest"
 cp node_modules/graphiql/graphiql.min.js "$dest"
 cp node_modules/graphiql/graphiql.css "$dest"
 
-dest=src/Symfony/Bundle/Resources/public/graphql-playground/
+dest="${public}graphql-playground/"
 if [[ -d "$dest" ]]; then
 rm -Rf "$dest"
 fi
@@ -88,12 +90,13 @@ mkdir -p "$dest"
 cp node_modules/graphql-playground-react/build/static/js/middleware.js "$dest"
 cp node_modules/graphql-playground-react/build/static/css/index.css "$dest"
 
-dest=src/Symfony/Bundle/Resources/public/redoc/
+dest="${public}redoc/"
 if [[ -d "$dest" ]]; then
 rm -Rf "$dest"
 fi
 mkdir -p "$dest"
 cp node_modules/redoc/bundles/redoc.standalone.js "$dest"
 
+done
+
 rm -Rf package.json node_modules/
-# TODO Laravel public files
