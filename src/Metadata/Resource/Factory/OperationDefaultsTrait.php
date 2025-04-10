39169e4711e54da1a4cd26f2c9adc14e7472b1d9
Thibault G. <gattolliat.thibault@gmail.com>
2025-04-10 15:28:48 +0200

    fix(symfony): don't add global defaults for internal resources instead

--- src/Metadata/Resource/Factory/OperationDefaultsTrait.php
+++ src/Metadata/Resource/Factory/OperationDefaultsTrait.php
@@ -17,7 +17,6 @@ use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\CollectionOperationInterface;
 use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Error;
-use ApiPlatform\Metadata\ErrorResource;
 use ApiPlatform\Metadata\Exception\RuntimeException;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
@@ -34,6 +33,7 @@ use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Post;
 use ApiPlatform\Metadata\Util\CamelCaseToSnakeCaseNameConverter;
 use ApiPlatform\State\CreateProvider;
+use ApiPlatform\Validator\Exception\ValidationException;
 use Psr\Log\LoggerInterface;
 
 trait OperationDefaultsTrait
@@ -42,20 +42,35 @@ trait OperationDefaultsTrait
     private array $defaults = [];
     private LoggerInterface $logger;
 
+    /**
+     * @param  class-string<object>  $class
+     */
+    private static function isAnInternalResource(string $class): bool
+    {
+        /**
+         * @var  list<class-string<object>>  $internalResources
+         */
+        $internalResources = [
+            Error::class,
+            ValidationException::class,
+        ];
+
+        return array_any(
+            $internalResources,
+            fn (string $resource): bool => is_a($class, $resource, true),
+        );
+    }
+
     private function addGlobalDefaults(ApiResource|Operation $operation): ApiResource|Operation
     {
+        if (self::isAnInternalResource($operation->getClass())) {
+            return $operation;
+        }
+        
         $extraProperties = $this->defaults['extra_properties'] ?? [];
 
         foreach ($this->defaults as $key => $value) {
             if ('operations' === $key) {
-                continue;
-            }
-
-            // Error operations shouldn't inherit security, as they must be
-            // publicly accessible by default. If stricter access control is
-            // needed, configure it at the firewall level.
-            // @see https://github.com/api-platform/core/issues/7072
-            if ('security' === $key && ($operation instanceof ErrorResource || $operation instanceof Error)) {
                 continue;
             }
 
