e2847050baacf14d9c19f93cc558bb3bddf1f06c
Aleksey Polyvanyi <alexndlm@gmail.com>
2025-05-04 17:11:12 +0200

    -refactoring

--- src/Serializer/AbstractItemNormalizer.php
+++ src/Serializer/AbstractItemNormalizer.php
@@ -42,6 +42,7 @@ use Symfony\Component\Serializer\NameConverter\NameConverterInterface;
 use Symfony\Component\Serializer\Normalizer\AbstractObjectNormalizer;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Serializer;
 
 /**
  * Base item normalizer.
