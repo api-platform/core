3a38113cfc272c5b5c4e3e8fb9cbb6314dbc861f
Maxime Hélias <maximehelias16@gmail.com>
2025-05-30 18:20:17 +0200

    chore: remove 3.4 deprecation

--- src/Metadata/Operation.php
+++ src/Metadata/Operation.php
@@ -47,18 +47,17 @@ abstract class Operation extends Metadata
      *     class?: string|null,
      *     name?: string,
      * }|string|false|null $output {@see https://api-platform.com/docs/core/dto/#specifying-an-input-or-an-output-data-representation}
-     * @param string|array|bool|null   $mercure       {@see https://api-platform.com/docs/core/mercure}
-     * @param string|bool|null         $messenger     {@see https://api-platform.com/docs/core/messenger/#dispatching-a-resource-through-the-message-bus}
-     * @param bool|null                $elasticsearch {@see https://api-platform.com/docs/core/elasticsearch/}
-     * @param bool|null                $read          {@see https://api-platform.com/docs/core/events/#the-event-system}
-     * @param bool|null                $deserialize   {@see https://api-platform.com/docs/core/events/#the-event-system}
-     * @param bool|null                $validate      {@see https://api-platform.com/docs/core/events/#the-event-system}
-     * @param bool|null                $write         {@see https://api-platform.com/docs/core/events/#the-event-system}
-     * @param bool|null                $serialize     {@see https://api-platform.com/docs/core/events/#the-event-system}
-     * @param bool|null                $fetchPartial  {@see https://api-platform.com/docs/core/performance/#fetch-partial}
-     * @param bool|null                $forceEager    {@see https://api-platform.com/docs/core/performance/#force-eager}
-     * @param string|callable|null     $provider      {@see https://api-platform.com/docs/core/state-providers/#state-providers}
-     * @param string|callable|null     $processor     {@see https://api-platform.com/docs/core/state-processors/#state-processors}
+     * @param string|array|bool|null   $mercure      {@see https://api-platform.com/docs/core/mercure}
+     * @param string|bool|null         $messenger    {@see https://api-platform.com/docs/core/messenger/#dispatching-a-resource-through-the-message-bus}
+     * @param bool|null                $read         {@see https://api-platform.com/docs/core/events/#the-event-system}
+     * @param bool|null                $deserialize  {@see https://api-platform.com/docs/core/events/#the-event-system}
+     * @param bool|null                $validate     {@see https://api-platform.com/docs/core/events/#the-event-system}
+     * @param bool|null                $write        {@see https://api-platform.com/docs/core/events/#the-event-system}
+     * @param bool|null                $serialize    {@see https://api-platform.com/docs/core/events/#the-event-system}
+     * @param bool|null                $fetchPartial {@see https://api-platform.com/docs/core/performance/#fetch-partial}
+     * @param bool|null                $forceEager   {@see https://api-platform.com/docs/core/performance/#force-eager}
+     * @param string|callable|null     $provider     {@see https://api-platform.com/docs/core/state-providers/#state-providers}
+     * @param string|callable|null     $processor    {@see https://api-platform.com/docs/core/state-processors/#state-processors}
      * @param array<string, Parameter> $parameters
      */
     public function __construct(
@@ -786,7 +785,6 @@ abstract class Operation extends Metadata
          * @var string|bool|null
          */
         protected $messenger = null,
-        protected ?bool $elasticsearch = null,
         protected ?int $urlGenerationStrategy = null,
         protected ?bool $read = null,
         protected ?bool $deserialize = null,
@@ -826,7 +824,6 @@ abstract class Operation extends Metadata
             collectDenormalizationErrors: $collectDenormalizationErrors,
             validationContext: $validationContext,
             filters: $filters,
-            elasticsearch: $elasticsearch,
             mercure: $mercure,
             messenger: $messenger,
             input: $input,
