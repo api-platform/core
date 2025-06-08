f71aa2b798a17385a4dae7633a88eff54119be24
Vincent Amstoutz <vincent.amstoutz@outlook.com>
2025-06-08 14:59:06 +0200

    feat(doctrine): add ODM ExactFilter
    Continues the work at #7079 and before at #6865

---
+++ src/Doctrine/Odm/Filter/ExactFilter.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Doctrine\Odm\Filter;
+
+use ApiPlatform\Metadata\OpenApiParameterFilterInterface;
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\Metadata\Parameter;
+use ApiPlatform\OpenApi\Model\Parameter as OpenApiParameter;
+use Doctrine\ODM\MongoDB\Aggregation\Builder;
+
+final class ExactFilter implements FilterInterface, OpenApiParameterFilterInterface
+{
+    public function apply(Builder $aggregationBuilder, string $resourceClass, ?Operation $operation = null, array &$context = []): void
+    {
+        if (!$parameter = $context['parameter'] ?? null) {
+            return;
+        }
+
+        \assert($parameter instanceof Parameter);
+
+        $values = (array) $parameter->getValue();
+        if ([] === $values) {
+            return;
+        }
+
+        $matchField = $parameter->getProperty();
+        $fieldQuery = $aggregationBuilder->match()->field($matchField);
+
+        if (\count($values) > 1) {
+            $fieldQuery->in($values);
+        } else {
+            $fieldQuery->equals(reset($values));
+        }
+    }
+
+    public function getOpenApiParameters(Parameter $parameter): OpenApiParameter|array|null
+    {
+        return new OpenApiParameter(name: $parameter->getKey().'[]', in: 'query', style: 'deepObject', explode: true);
+    }
+
+    public function getDescription(string $resourceClass): array
+    {
+        return [];
+    }
+}
