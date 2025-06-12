0142f0c52a4910cbf8f6f716a635d8e6ef78a17d
Maxime Hélias <maximehelias16@gmail.com>
2025-06-12 15:53:51 +0200

    chore: remove 3.4 deprecation

--- src/Metadata/Resource/Factory/PhpDocResourceMetadataCollectionFactory.php
+++ src/Metadata/Resource/Factory/PhpDocResourceMetadataCollectionFactory.php
@@ -15,9 +15,6 @@ namespace ApiPlatform\Metadata\Resource\Factory;
 
 use ApiPlatform\Metadata\Operations;
 use ApiPlatform\Metadata\Resource\ResourceMetadataCollection;
-use phpDocumentor\Reflection\DocBlockFactory;
-use phpDocumentor\Reflection\DocBlockFactoryInterface;
-use phpDocumentor\Reflection\Types\ContextFactory;
 use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocTextNode;
 use PHPStan\PhpDocParser\Lexer\Lexer;
@@ -34,29 +31,14 @@ use PHPStan\PhpDocParser\ParserConfig;
  */
 final class PhpDocResourceMetadataCollectionFactory implements ResourceMetadataCollectionFactoryInterface
 {
-    private readonly ?DocBlockFactoryInterface $docBlockFactory;
-    private readonly ?ContextFactory $contextFactory;
     private readonly ?PhpDocParser $phpDocParser;
     private readonly ?Lexer $lexer;
 
     /** @var array<string, PhpDocNode> */
     private array $docBlocks = [];
 
-    public function __construct(private readonly ResourceMetadataCollectionFactoryInterface $decorated, ?DocBlockFactoryInterface $docBlockFactory = null)
+    public function __construct(private readonly ResourceMetadataCollectionFactoryInterface $decorated)
     {
-        $contextFactory = null;
-        if ($docBlockFactory instanceof DocBlockFactoryInterface) {
-            trigger_deprecation('api-platform/core', '3.1', 'Using a 2nd argument to PhpDocResourceMetadataCollectionFactory is deprecated.');
-        }
-        if (class_exists(DocBlockFactory::class) && class_exists(ContextFactory::class)) {
-            $docBlockFactory = $docBlockFactory ?? DocBlockFactory::createInstance();
-            $contextFactory = new ContextFactory();
-        }
-        $this->docBlockFactory = $docBlockFactory;
-        $this->contextFactory = $contextFactory;
-        if (class_exists(DocBlockFactory::class) && !class_exists(PhpDocParser::class)) {
-            trigger_deprecation('api-platform/core', '3.1', 'Using phpdocumentor/reflection-docblock is deprecated. Require phpstan/phpdoc-parser instead.');
-        }
         $phpDocParser = null;
         $lexer = null;
         if (class_exists(PhpDocParser::class) && class_exists(ParserConfig::class)) {
@@ -83,21 +65,7 @@ final class PhpDocResourceMetadataCollectionFactory implements ResourceMetadataC
                 continue;
             }
 
-            $description = null;
-
-            // Deprecated path. To remove in API Platform 4.
-            if (!$this->phpDocParser instanceof PhpDocParser && $this->docBlockFactory instanceof DocBlockFactoryInterface && $this->contextFactory) {
-                $reflectionClass = new \ReflectionClass($resourceClass);
-
-                try {
-                    $docBlock = $this->docBlockFactory->create($reflectionClass, $this->contextFactory->createFromReflector($reflectionClass));
-                    $description = $docBlock->getSummary();
-                } catch (\InvalidArgumentException) {
-                    // Ignore empty DocBlocks
-                }
-            } else {
-                $description = $this->getShortDescription($resourceClass);
-            }
+            $description = $this->getShortDescription($resourceClass);
 
             if (!$description) {
                 return $resourceMetadataCollection;
