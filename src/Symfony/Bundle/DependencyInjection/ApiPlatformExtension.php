7f8aa4f7fdb01b73efdc53b969234603da07ecc6
Loïc Frémont <lc.fremont@gmail.com>
2025-06-13 16:17:49 +0200

    feat(metadata) Customize Resource & operations

--- src/Symfony/Bundle/DependencyInjection/ApiPlatformExtension.php
+++ src/Symfony/Bundle/DependencyInjection/ApiPlatformExtension.php
@@ -31,6 +31,7 @@ use ApiPlatform\GraphQl\Resolver\QueryCollectionResolverInterface;
 use ApiPlatform\GraphQl\Resolver\QueryItemResolverInterface;
 use ApiPlatform\GraphQl\Type\Definition\TypeInterface as GraphQlTypeInterface;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\AsResourceMutator;
 use ApiPlatform\Metadata\FilterInterface;
 use ApiPlatform\Metadata\UriVariableTransformerInterface;
 use ApiPlatform\Metadata\UrlGeneratorInterface;
@@ -187,6 +188,13 @@ final class ApiPlatformExtension extends Extension implements PrependExtensionIn
                 ->addTag('api_platform.resource')
                 ->addTag('container.excluded', ['source' => 'by #[ApiResource] attribute']);
         });
+        $container->registerAttributeForAutoconfiguration(AsResourceMutator::class,
+            static function (ChildDefinition $definition, AsResourceMutator $attribute): void {
+                $definition->addTag('api_platform.resource_mutator', [
+                    'resourceClass' => $attribute->resourceClass,
+                ]);
+            },
+        );
 
         if (!$container->has('api_platform.state.item_provider')) {
             $container->setAlias('api_platform.state.item_provider', 'api_platform.state_provider.object');
@@ -345,6 +353,7 @@ final class ApiPlatformExtension extends Extension implements PrependExtensionIn
         $loader->load('metadata/property.xml');
         $loader->load('metadata/resource.xml');
         $loader->load('metadata/operation.xml');
+        $loader->load('metadata/mutator.xml');
 
         $container->getDefinition('api_platform.metadata.resource_extractor.xml')->replaceArgument(0, $xmlResources);
         $container->getDefinition('api_platform.metadata.property_extractor.xml')->replaceArgument(0, $xmlResources);
