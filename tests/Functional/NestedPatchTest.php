296ab1dfa80d346bb109d1f253d9602d415dbefc
Callum Browne <callum@callumbrowne.me>
2025-07-01 17:48:49 +0100

    fix(serializer): allow "ID" field even when allow_extra_attributes=false

---
+++ tests/Functional/NestedPatchTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace ApiPlatform\Tests\Functional;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use ApiPlatform\Tests\Fixtures\TestBundle\Entity\Issue6225\Bar6225;
+use ApiPlatform\Tests\Fixtures\TestBundle\Entity\Issue6225\Foo6225;
+use ApiPlatform\Tests\RecreateSchemaTrait;
+use ApiPlatform\Tests\SetupClassResourcesTrait;
+
+class NestedPatchTest extends ApiTestCase
+{
+    use RecreateSchemaTrait;
+    use SetupClassResourcesTrait;
+
+    public static function getResources(): array {
+        return [Foo6225::class, Bar6225::class];
+    }
+
+    public function testIssue6225(): void
+    {
+        $this->recreateSchema(self::getResources());
+
+        $response = self::createClient()->request('POST', '/foo6225s', [
+            'json' => [
+                'bar' => [
+                    'someProperty' => 'abc'
+                ]
+            ],
+            'headers' => [
+                'accept' => 'application/json'
+            ]
+        ]);
+        static::assertResponseIsSuccessful();
+        $responseContent = json_decode($response->getContent(), true);
+        $createdFooId = $responseContent['id'];
+        $createdBarId = $responseContent['bar']['id'];
+
+        $patchResponse = self::createClient()->request('PATCH', '/foo6225s/'.$createdFooId, [
+            'json' => [
+                'bar' => [
+                    'id' => $createdBarId,
+                    'someProperty' => 'def'
+                ]
+            ],
+            'headers' => [
+                'accept' => 'application/json',
+                'content-type' => 'application/merge-patch+json'
+            ]
+        ]);
+        static::assertResponseIsSuccessful();
+        static::assertEquals([
+            'id' => $createdFooId,
+            'bar' => [
+                'id' => $createdBarId,
+                'someProperty' => 'def'
+            ]
+        ], json_decode($patchResponse->getContent(), true));
+    }
+}
