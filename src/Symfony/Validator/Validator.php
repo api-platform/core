3ed6f30f5ce60ac45a1a1df6bac9372a7b712d51
Aleksey Polyvanyi <alexndlm@gmail.com>
2025-07-03 10:18:21 +0200

    fix(symfony): fix resolving groups in ValidationGroupsExtractorTrait when GroupSequence (#7272)

--- src/Symfony/Validator/Validator.php
+++ src/Symfony/Validator/Validator.php
@@ -16,6 +16,7 @@ namespace ApiPlatform\Symfony\Validator;
 use ApiPlatform\Validator\Exception\ValidationException;
 use ApiPlatform\Validator\ValidatorInterface;
 use Psr\Container\ContainerInterface;
+use Symfony\Component\Validator\Constraints\GroupSequence;
 use Symfony\Component\Validator\Validator\ValidatorInterface as SymfonyValidatorInterface;
 
 /**
@@ -25,11 +26,8 @@ use Symfony\Component\Validator\Validator\ValidatorInterface as SymfonyValidator
  */
 final class Validator implements ValidatorInterface
 {
-    use ValidationGroupsExtractorTrait;
-
-    public function __construct(private readonly SymfonyValidatorInterface $validator, ?ContainerInterface $container = null)
+    public function __construct(private readonly SymfonyValidatorInterface $validator, private readonly ?ContainerInterface $container = null)
     {
-        $this->container = $container;
     }
 
     /**
@@ -37,7 +35,25 @@ final class Validator implements ValidatorInterface
      */
     public function validate(object $data, array $context = []): void
     {
-        $violations = $this->validator->validate($data, null, $this->getValidationGroups($context['groups'] ?? null, $data));
+        if (null !== $validationGroups = $context['groups'] ?? null) {
+            if (
+                $this->container
+                && \is_string($validationGroups)
+                && $this->container->has($validationGroups)
+                && ($service = $this->container->get($validationGroups))
+                && \is_callable($service)
+            ) {
+                $validationGroups = $service($data);
+            } elseif (\is_callable($validationGroups)) {
+                $validationGroups = $validationGroups($data);
+            }
+
+            if (!$validationGroups instanceof GroupSequence) {
+                $validationGroups = (array) $validationGroups;
+            }
+        }
+
+        $violations = $this->validator->validate($data, null, $validationGroups);
         if (0 !== \count($violations)) {
             throw new ValidationException($violations);
         }
