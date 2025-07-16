f3a54a239e21c18716967e579d2cd2120a52ece1
Antoine Bluchet <soyuka@users.noreply.github.com>
2025-07-16 16:01:52 +0200

    fix: json formatted resource should not get xml errors #7287 (#7297)

--- src/Symfony/EventListener/ErrorListener.php
+++ src/Symfony/EventListener/ErrorListener.php
@@ -202,6 +202,7 @@ final class ErrorListener extends SymfonyErrorListener
             'jsonproblem' => '_api_errors_problem',
             'jsonld' => '_api_errors_hydra',
             'jsonapi' => '_api_errors_jsonapi',
+            'xml' => '_api_errors_xml',
             'html' => '_api_errors_problem', // This will be intercepted by the SwaggerUiProvider
             default => '_api_errors_problem',
         };
