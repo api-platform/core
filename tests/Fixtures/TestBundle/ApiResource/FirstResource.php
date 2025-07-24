3058b8382a49f5814d464519a8edf8b8bd251d73
mrossard <manuel.rossard@u-bordeaux.fr>
2025-07-24 14:24:13 +0200

    fix(symfony): explicitly set the target class when mapping entities to resources

---
+++ tests/Fixtures/TestBundle/ApiResource/FirstResource.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Tests\Fixtures\TestBundle\ApiResource;
+
+use ApiPlatform\Doctrine\Orm\State\Options;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Tests\Fixtures\TestBundle\Entity\SameEntity;
+use Symfony\Component\ObjectMapper\Attribute\Map;
+
+#[ApiResource(
+    shortName: 'First',
+    operations: [new GetCollection()],
+    stateOptions: new Options(entityClass: SameEntity::class)
+)]
+#[Map(target: SameEntity::class)]
+final class FirstResource
+{
+    #[Map(if: false)]
+    public ?int $id = null;
+
+    public string $name;
+}
