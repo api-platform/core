510fa5523802f9c173d852c218317f95f9ef2d90
Antoine Bluchet <soyuka@users.noreply.github.com>
2025-09-16 16:07:52 +0200

    ci: laravel e2e installation (#7388)

--- subtree.sh
+++ subtree.sh
@@ -2,13 +2,42 @@
 
 set -xe
 
-# Subtree split on tag this script gets called using find:
-# find src -maxdepth 2 -name composer.json -print0 | xargs -I '{}' -n 1 -0 bash subtree.sh {} refs/tags/3.1.5
-# find src -maxdepth 2 -name composer.json -print0 | xargs -I '{}' -n 1 -0 bash subtree.sh {} refs/heads/3.1
-# See the subtree workflow
-package=$(jq -r .name $1)
-directory=$(dirname $1)
+FORCE_FLAG=""
+
+while getopts "f" opt; do
+  case $opt in
+    f)
+      FORCE_FLAG="-f"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+
+# The first argument is now the path to composer.json
+COMPOSER_JSON_PATH=$1
+# The second argument is the git ref
+GIT_REF=$2
+
+if [ -z "$COMPOSER_JSON_PATH" ] || [ -z "$GIT_REF" ]; then
+    echo "Usage: $0 [-f] <path-to-composer.json> <git-ref>"
+    exit 1
+fi
+
+package=$(jq -r .name "$COMPOSER_JSON_PATH")
+directory=$(dirname "$COMPOSER_JSON_PATH")
 repository="https://github.com/$package"
-git remote add $package $repository
-sha=$(splitsh-lite --prefix=$directory)
-git push $package $sha:$2
+
+# Add the remote only if it doesn't exist already
+if ! git remote get-url "$package" > /dev/null 2>&1; then
+    git remote add "$package" "$repository"
+fi
+
+sha=$(splitsh-lite --prefix="$directory")
+git push $FORCE_FLAG "$package" "$sha":"$GIT_REF"
+
+
