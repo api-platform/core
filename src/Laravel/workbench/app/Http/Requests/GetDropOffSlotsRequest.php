c6f0922788f974779cd20c6bb30245935e9f9de7
soyuka <soyuka@users.noreply.github.com>
2025-09-26 11:48:31 +0200

    test(laravel): #7337

--- src/Laravel/workbench/app/Http/Requests/GetDropOffSlotsRequest.php
+++ src/Laravel/workbench/app/Http/Requests/GetDropOffSlotsRequest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace Workbench\App\Http\Requests;
@@ -23,8 +32,8 @@ class GetDropOffSlotsRequest extends FormRequest
             'pickupDate' => $this->query('pickupDate'),
             'pickupSlotId' => $this->query('pickupSlotId'),
         ]);
-
     }
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -42,14 +51,9 @@ class GetDropOffSlotsRequest extends FormRequest
     protected function failedValidation(Validator $validator): void
     {
         $violations = collect($validator->errors())
-            ->map(fn($m, $f) => ['propertyPath' => $f, 'message' => $m[0]]) //** @phpstan-ignore-line */
+            ->map(fn ($m, $f) => ['propertyPath' => $f, 'message' => $m[0]]) // ** @phpstan-ignore-line */
             ->values()->all();
 
-        throw new \ApiPlatform\Laravel\ApiResource\ValidationError(
-            $violations[0]['message'] ?? 'Validation failed.',
-            hash('xxh3', implode(',', array_column($violations, 'propertyPath'))),
-            new \Illuminate\Validation\ValidationException($validator),
-            $violations
-        );
+        throw new \ApiPlatform\Laravel\ApiResource\ValidationError($violations[0]['message'] ?? 'Validation failed.', hash('xxh3', implode(',', array_column($violations, 'propertyPath'))), new \Illuminate\Validation\ValidationException($validator), $violations);
     }
 }
--- src/Laravel/workbench/app/Models/Slot.php
+++ src/Laravel/workbench/app/Models/Slot.php
@@ -14,16 +14,41 @@ declare(strict_types=1);
 namespace Workbench\App\Models;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Workbench\App\Http\Requests\StoreSlotRequest;
 
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new GetCollection(),
+        new Get(),
+        new Post(
+            rules: StoreSlotRequest::class,
+        ),
+        new Put(),
+        new Patch(),
+        new Delete(),
+    ],
+)]
 class Slot extends Model
 {
     use HasFactory;
+    protected $table = 'slots';
 
     protected $fillable = [
         'name',
-        'note',
+        'area_id',
     ];
+
+    public function area(): BelongsTo
+    {
+        return $this->belongsTo(Area::class);
+    }
 }
