90f4ca77c811e072be99d86731f7265a0b4d6b3c
Aleksey Polyvanyi <alexndlm@gmail.com>
2025-10-08 14:24:57 +0200

    feature/do-not-load-docs-routes-if-docs-disabled

--- src/Symfony/Routing/ApiLoader.php
+++ src/Symfony/Routing/ApiLoader.php
@@ -127,7 +127,10 @@ final class ApiLoader extends Loader
      */
     private function loadExternalFiles(RouteCollection $routeCollection): void
     {
-        $routeCollection->addCollection($this->fileLoader->load('docs.xml'));
+        if ($this->docsEnabled) {
+            $routeCollection->addCollection($this->fileLoader->load('docs.xml'));
+        }
+
         $routeCollection->addCollection($this->fileLoader->load('genid.xml'));
         $routeCollection->addCollection($this->fileLoader->load('errors.xml'));
 
