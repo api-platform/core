9045b1a12144c70d12eefd565474137f5a25e19f
eliot lauger <eliot@rezo-zero.com>
2025-10-09 17:46:37 +0200

    chore(metadata): Expand `phpstan-return` type annotations in `SerializerContextBuilderInterface`

--- src/State/SerializerContextBuilderInterface.php
+++ src/State/SerializerContextBuilderInterface.php
@@ -34,7 +34,31 @@ interface SerializerContextBuilderInterface
      *
      * @return array<string, mixed>
      *
-     * @phpstan-return array<string, mixed>
+     * @phpstan-return array{
+     *    groups?: string[]|string,
+     *    operation_name?: string,
+     *    operation?: HttpOperation,
+     *    resource_class?: class-string,
+     *    skip_null_values?: bool,
+     *    iri_only?: bool,
+     *    request_uri?: string,
+     *    uri?: string,
+     *    input?: array{class: class-string|null},
+     *    output?: array{class: class-string|null},
+     *    item_uri_template?: string,
+     *    types?: string[],
+     *    uri_variables?: array<string, string>,
+     *    force_resource_class?: class-string,
+     *    api_allow_update?: bool,
+     *    deep_object_to_populate?: bool,
+     *    collect_denormalization_errors?: bool,
+     *    exclude_from_cache_key?: string[],
+     *    api_included?: bool,
+     *    attributes?: string[],
+     *    deserializer_type?: string,
+     *    api_assign_object_to_populate?: bool,
+     *    ...<string, mixed>
+     *  }
      *
      * @psalm-return array{
      *   groups?: string[]|string,
