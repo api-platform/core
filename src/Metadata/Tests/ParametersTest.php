05712d334e99ca48658afab71c55a1705d0c2a0c
Takashi Kanemoto <ttskch@gmail.com>
2025-10-30 16:36:06 +0900

    test: 💍 add tests

--- src/Metadata/Tests/ParametersTest.php
+++ src/Metadata/Tests/ParametersTest.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace ApiPlatform\Metadata\Tests;
 
+use ApiPlatform\Metadata\HeaderParameter;
 use ApiPlatform\Metadata\Parameters;
 use ApiPlatform\Metadata\QueryParameter;
 use PHPUnit\Framework\TestCase;
@@ -24,5 +25,28 @@ class ParametersTest extends TestCase
         $r = new QueryParameter();
         $parameters = new Parameters(['a' => $r]);
         $this->assertSame($r, $parameters->get('a'));
+    }
+
+    public function testDuplicated(): void
+    {
+        $r1 = new QueryParameter(key: 'a');
+        $r2 = new QueryParameter(key: 'b');
+        $r3 = new HeaderParameter(key: 'a');
+        $r4 = new HeaderParameter(key: 'b');
+        $parameters = new Parameters([$r1, $r2, $r3, $r4]);
+        $this->assertCount(4, $parameters);
+        $this->assertSame($r1, $parameters->get('a'));
+        $this->assertSame($r2, $parameters->get('b'));
+        $this->assertSame($r3, $parameters->get('a', HeaderParameter::class));
+        $this->assertSame($r4, $parameters->get('b', HeaderParameter::class));
+
+        $r1 = new QueryParameter(key: 'a');
+        $r2 = new QueryParameter(key: 'a');
+        $r3 = new HeaderParameter(key: 'a');
+        $r4 = new HeaderParameter(key: 'a');
+        $parameters = new Parameters([$r1, $r2, $r3, $r4]);
+        $this->assertCount(2, $parameters);
+        $this->assertSame($r2, $parameters->get('a'));
+        $this->assertSame($r4, $parameters->get('a', HeaderParameter::class));
     }
 }
