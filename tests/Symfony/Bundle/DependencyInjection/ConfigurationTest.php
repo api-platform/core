5b700bc1dd8ca8f2af0400c95a2e5ba2574d9ed0
soyuka <soyuka@users.noreply.github.com>
2025-11-08 09:26:36 +0100

    test(symfony): enable phpdoc parser default true

--- tests/Symfony/Bundle/DependencyInjection/ConfigurationTest.php
+++ tests/Symfony/Bundle/DependencyInjection/ConfigurationTest.php
@@ -227,6 +227,7 @@ class ConfigurationTest extends TestCase
             'serializer' => [
                 'hydra_prefix' => null,
             ],
+            'enable_phpdoc_parser' => true
         ], $config);
     }
 
