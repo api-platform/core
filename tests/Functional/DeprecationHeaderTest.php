11ca7d3a4c0ba667cf7e90cb2e69b0cd23b18164
aaa2000 <adev2000@gmail.com>
2025-12-05 16:03:14 +0100

    test: web links and deprecation header (#7582)

---
+++ tests/Functional/DeprecationHeaderTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Tests\Functional;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use ApiPlatform\Tests\Fixtures\TestBundle\ApiResource\DeprecationHeader;
+use ApiPlatform\Tests\SetupClassResourcesTrait;
+
+final class DeprecationHeaderTest extends ApiTestCase
+{
+    use SetupClassResourcesTrait;
+
+    protected static ?bool $alwaysBootKernel = false;
+
+    /**
+     * @return class-string[]
+     */
+    public static function getResources(): array
+    {
+        return [DeprecationHeader::class];
+    }
+
+    public function testDeprecationHeader(): void
+    {
+        $response = self::createClient()->request('GET', '/deprecation_headers');
+
+        $headers = $response->getHeaders();
+
+        $this->assertContains('@1688169599', $headers['deprecation']);
+        $this->assertContains('Sun, 30 Jun 2024 23:59:59 UTC', $headers['sunset']);
+        $this->assertStringContainsString('<https://developer.example.com/deprecation>; rel="deprecation"', $headers['link'][0]);
+    }
+}
