6dfa46efcfb3eda79c3964285f94c669b7affae4
aaa2000 <adev2000@gmail.com>
2025-12-12 17:06:26 +0100

    fix #7465 Add uuid filter

---
+++ tests/Functional/Uuid/UuidFilterBaseTestCase.php
@@ -0,0 +1,294 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Tests\Functional\Uuid;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use ApiPlatform\Tests\RecreateSchemaTrait;
+use ApiPlatform\Tests\SetupClassResourcesTrait;
+
+abstract class UuidFilterBaseTestCase extends ApiTestCase
+{
+    use RecreateSchemaTrait;
+    use SetupClassResourcesTrait;
+
+    protected static ?bool $alwaysBootKernel = false;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if ($this->isMongoDB()) {
+            $this->markTestSkipped();
+        }
+    }
+
+    /**
+     * @return class-string
+     */
+    abstract protected static function getDeviceEndpointClass(): string;
+
+    /**
+     * @return class-string
+     */
+    abstract protected static function getDeviceClass(): string;
+
+    abstract public function getUrlPrefix(): string;
+
+    abstract public function geTypePrefix(): string;
+
+    public function createDeviceEndpoint(mixed ...$args): object
+    {
+        return (new \ReflectionClass(static::getDeviceEndpointClass()))->newInstanceArgs($args);
+    }
+
+    public function createDevice(mixed ...$args): object
+    {
+        return (new \ReflectionClass(static::getDeviceClass()))->newInstanceArgs($args);
+    }
+
+    public function testSearchFilterByUuid(): void
+    {
+        $this->recreateSchema(static::getResources());
+
+        $manager = $this->getManager();
+        $manager->persist($device = $this->createDevice());
+        $manager->persist($this->createDevice());
+        $manager->flush();
+
+        $response = self::createClient()->request('GET', '/'.$this->getUrlPrefix().'_devices', [
+            'query' => [
+                'id' => $device->id->toString(),
+            ],
+        ]);
+
+        self::assertResponseIsSuccessful();
+        $json = $response->toArray();
+
+        self::assertArraySubset(['hydra:totalItems' => 1], $json);
+        self::assertArraySubset(
+            [
+                'hydra:member' => [
+                    [
+                        '@id' => '/'.$this->getUrlPrefix().'_devices/'.$device->id->toString(),
+                        '@type' => $this->geTypePrefix().'Device',
+                        'id' => $device->id->toString(),
+                    ],
+                ],
+            ],
+            $json
+        );
+    }
+
+    public function testSearchFilterByManyUuid(): void
+    {
+        $this->recreateSchema(static::getResources());
+
+        $manager = $this->getManager();
+        $manager->persist($device = $this->createDevice());
+        $manager->persist($otherDevice = $this->createDevice());
+        $manager->persist($this->createDevice());
+        $manager->flush();
+
+        $response = self::createClient()->request('GET', '/'.$this->getUrlPrefix().'_devices', [
+            'query' => [
+                'id' => [
+                    $device->id->toString(),
+                    $otherDevice->id->toString(),
+                ],
+            ],
+        ]);
+
+        self::assertResponseIsSuccessful();
+        $json = $response->toArray();
+
+        self::assertArraySubset(['hydra:totalItems' => 2], $json);
+        self::assertArraySubset(
+            [
+                'hydra:member' => [
+                    [
+                        '@id' => '/'.$this->getUrlPrefix().'_devices/'.$device->id->toString(),
+                        '@type' => $this->geTypePrefix().'Device',
+                        'id' => $device->id->toString(),
+                    ],
+                    [
+                        '@id' => '/'.$this->getUrlPrefix().'_devices/'.$otherDevice->id->toString(),
+                        '@type' => $this->geTypePrefix().'Device',
+                        'id' => $otherDevice->id->toString(),
+                    ],
+                ],
+            ],
+            $json
+        );
+    }
+
+    public function testSearchFilterByInvalidUuid(): void
+    {
+        $this->recreateSchema(static::getResources());
+
+        $manager = $this->getManager();
+        $manager->persist($this->createDevice());
+        $manager->persist($this->createDevice());
+        $manager->flush();
+
+        $response = self::createClient()->request('GET', '/'.$this->getUrlPrefix().'_devices', [
+            'query' => [
+                'id' => 'invalid-uuid',
+            ],
+        ]);
+
+        self::assertResponseIsSuccessful();
+        $json = $response->toArray();
+
+        self::assertArraySubset(['hydra:totalItems' => 0], $json);
+        self::assertArraySubset(
+            [
+                'hydra:member' => [],
+            ],
+            $json
+        );
+    }
+
+    public function testSearchFilterOnManyToOneRelationByUuid(): void
+    {
+        $this->recreateSchema(static::getResources());
+
+        $manager = $this->getManager();
+        $manager->persist($fooDevice = $this->createDevice());
+        $manager->persist($barDevice = $this->createDevice());
+        $manager->persist($this->createDeviceEndpoint(null, $fooDevice));
+        $manager->persist($barDeviceEndpoint = $this->createDeviceEndpoint(null, $barDevice));
+        $manager->flush();
+
+        $response = self::createClient()->request('GET', '/'.$this->getUrlPrefix().'_device_endpoints', [
+            'query' => [
+                'myDevice' => $barDevice->id->toString(),
+            ],
+        ]);
+
+        self::assertResponseIsSuccessful();
+        $json = $response->toArray();
+
+        self::assertArraySubset(['hydra:totalItems' => 1], $json);
+        self::assertArraySubset(
+            [
+                'hydra:member' => [
+                    [
+                        '@id' => '/'.$this->getUrlPrefix().'_device_endpoints/'.$barDeviceEndpoint->id->toString(),
+                        '@type' => $this->geTypePrefix().'DeviceEndpoint',
+                        'id' => $barDeviceEndpoint->id->toString(),
+                    ],
+                ],
+            ],
+            $json
+        );
+    }
+
+    public function testSearchFilterOnManyToOneRelationByManyUuid(): void
+    {
+        $this->recreateSchema(static::getResources());
+
+        $manager = $this->getManager();
+        $manager->persist($fooDevice = $this->createDevice());
+        $manager->persist($barDevice = $this->createDevice());
+        $manager->persist($bazDevice = $this->createDevice());
+        $manager->persist($fooDeviceEndpoint = $this->createDeviceEndpoint(null, $fooDevice));
+        $manager->persist($barDeviceEndpoint = $this->createDeviceEndpoint(null, $barDevice));
+        $manager->persist($this->createDeviceEndpoint(null, $bazDevice));
+        $manager->flush();
+
+        $response = self::createClient()->request('GET', '/'.$this->getUrlPrefix().'_device_endpoints', [
+            'query' => [
+                'myDevice' => [
+                    $fooDevice->id->toString(),
+                    $barDevice->id->toString(),
+                ],
+            ],
+        ]);
+
+        self::assertResponseIsSuccessful();
+        $json = $response->toArray();
+
+        self::assertArraySubset(['hydra:totalItems' => 2], $json);
+        self::assertArraySubset(
+            [
+                'hydra:member' => [
+                    [
+                        '@id' => '/'.$this->getUrlPrefix().'_device_endpoints/'.$fooDeviceEndpoint->id->toString(),
+                        '@type' => $this->geTypePrefix().'DeviceEndpoint',
+                        'id' => $fooDeviceEndpoint->id->toString(),
+                    ],
+                    [
+                        '@id' => '/'.$this->getUrlPrefix().'_device_endpoints/'.$barDeviceEndpoint->id->toString(),
+                        '@type' => $this->geTypePrefix().'DeviceEndpoint',
+                        'id' => $barDeviceEndpoint->id->toString(),
+                    ],
+                ],
+            ],
+            $json
+        );
+    }
+
+    public function testGetOpenApiDescription(): void
+    {
+        $response = self::createClient()->request('GET', '/docs', [
+            'headers' => ['Accept' => 'application/vnd.openapi+json'],
+        ]);
+
+        $json = $response->toArray();
+
+        self::assertContains(
+            [
+                'name' => 'id',
+                'in' => 'query',
+                'description' => '',
+                'required' => false,
+                'deprecated' => false,
+                'schema' => [
+                    'type' => 'string',
+                    'format' => 'uuid',
+                ],
+                'style' => 'form',
+                'explode' => false,
+            ],
+            $json['paths']['/'.$this->getUrlPrefix().'_device_endpoints']['get']['parameters']
+        );
+
+        self::assertContains(
+            [
+                'name' => 'id[]',
+                'in' => 'query',
+                'description' => 'One or more Uuids',
+                'required' => false,
+                'deprecated' => false,
+                'schema' => [
+                    'type' => 'array',
+                    'items' => [
+                        'type' => 'string',
+                        'format' => 'uuid',
+                    ],
+                ],
+                'style' => 'deepObject',
+                'explode' => true,
+            ],
+            $json['paths']['/'.$this->getUrlPrefix().'_device_endpoints']['get']['parameters']
+        );
+    }
+
+    protected function tearDown(): void
+    {
+        $this->recreateSchema(static::getResources());
+
+        parent::tearDown();
+    }
+}
