c2b0997c8788ae7dc69b7a6b0823c7b8144d8762
Maxcastel <maxence.nord@gmail.com>
2025-12-26 21:36:38 +0100

    test: add functional test for JsonSchema generation

---
+++ tests/Fixtures/TestBundle/ApiResource/ParentAttribute.php
@@ -0,0 +1,84 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Tests\Fixtures\TestBundle\ApiResource;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ApiResource(
+    operations: [
+        new Get(
+            normalizationContext: ['attributes' => ['name', 'child' => ['label']]]
+        ),
+    ]
+)]
+class ParentAttribute
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: 'text')]
+    private ?string $description = null;
+
+    #[ORM\ManyToOne(targetEntity: ChildAttribute::class)]
+    private ?ChildAttribute $child = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getChild(): ?ChildAttribute
+    {
+        return $this->child;
+    }
+
+    public function setChild(?ChildAttribute $child): self
+    {
+        $this->child = $child;
+
+        return $this;
+    }
+}
