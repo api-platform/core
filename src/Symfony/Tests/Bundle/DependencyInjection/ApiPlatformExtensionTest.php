0e899fa813f4b49bd7df23cc5d4979b990a03241
Maxcastel <92802347+Maxcastel@users.noreply.github.com>
2025-12-31 10:26:07 +0100

    test(symfony): ensure pagination_maximum_items_per_page defaults correctly when config defaults key is absent (#7609)

--- src/Symfony/Tests/Bundle/DependencyInjection/ApiPlatformExtensionTest.php
+++ src/Symfony/Tests/Bundle/DependencyInjection/ApiPlatformExtensionTest.php
@@ -402,4 +402,14 @@ class ApiPlatformExtensionTest extends TestCase
 
         $this->assertSame(0, $this->container->getParameter('api_platform.collection.pagination.maximum_items_per_page'));
     }
+
+    public function testPaginationMaximumItemsPerPageWhenDefaultsKeyIsMissing(): void
+    {
+        $config = self::DEFAULT_CONFIG;
+        unset($config['api_platform']['defaults']);
+        (new ApiPlatformExtension())->load($config, $this->container);
+
+        $this->assertTrue($this->container->hasParameter('api_platform.collection.pagination.maximum_items_per_page'));
+        $this->assertSame(30, $this->container->getParameter('api_platform.collection.pagination.maximum_items_per_page'));
+    }
 }
