7290cf7b845b458b84710030f12a49ec3a17069c
aaa2000 <adev2000@gmail.com>
2026-01-02 21:34:11 +0100

    Add exception if no doctrine/orm:^3.0.1

--- src/Doctrine/Orm/Filter/UuidBinaryFilter.php
+++ src/Doctrine/Orm/Filter/UuidBinaryFilter.php
@@ -13,11 +13,21 @@ declare(strict_types=1);
 
 namespace ApiPlatform\Doctrine\Orm\Filter;
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use Doctrine\DBAL\ArrayParameterType;
 use Doctrine\DBAL\ParameterType;
 
 final class UuidBinaryFilter extends AbstractUuidFilter
 {
+    public function __construct()
+    {
+        if (!InstalledVersions::satisfies(new VersionParser, 'doctrine/orm', '^3.0.1')) {
+            // @see https://github.com/doctrine/orm/pull/11287
+            throw new \LogicException('The "doctrine/orm" package version 3.0.1 or higher is required to use the UuidBinaryFilter. Please upgrade your dependencies.');
+        }
+    }
+
     protected function getDoctrineParameterType(): ParameterType
     {
         return ParameterType::BINARY;
