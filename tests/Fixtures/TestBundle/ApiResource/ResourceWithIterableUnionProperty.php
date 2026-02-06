45cb6f64a69fe30e5f88a3be2590a48d1f854d39
Jakov Knezovic <jknezovicc@gmail.com>
2026-02-06 14:37:11 +0100

    fix(jsonschema): remove unnecessary api propery attribute from test case resource

--- tests/Fixtures/TestBundle/ApiResource/ResourceWithIterableUnionProperty.php
+++ tests/Fixtures/TestBundle/ApiResource/ResourceWithIterableUnionProperty.php
@@ -13,15 +13,8 @@ declare(strict_types=1);
 
 namespace ApiPlatform\Tests\Fixtures\TestBundle\ApiResource;
 
-use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Tests\Fixtures\TestBundle\Dto\NonResourceClass;
-use Symfony\Component\TypeInfo\Type\BuiltinType;
-use Symfony\Component\TypeInfo\Type\CollectionType;
-use Symfony\Component\TypeInfo\Type\GenericType;
-use Symfony\Component\TypeInfo\Type\ObjectType;
-use Symfony\Component\TypeInfo\Type\UnionType;
-use Symfony\Component\TypeInfo\TypeIdentifier;
 
 #[ApiResource]
 final class ResourceWithIterableUnionProperty
@@ -31,21 +24,6 @@ final class ResourceWithIterableUnionProperty
      */
     public function __construct(
         public int $id,
-        #[ApiProperty(
-            nativeType: new CollectionType(
-                new GenericType(
-                    new BuiltinType(TypeIdentifier::ARRAY),
-                    new BuiltinType(TypeIdentifier::INT),
-                    new UnionType(
-                        new ObjectType(Species::class),
-                        new ObjectType(NonResourceClass::class),
-                        new BuiltinType(TypeIdentifier::INT),
-                        new BuiltinType(TypeIdentifier::STRING),
-                    ),
-                ),
-                true,
-            ),
-        )]
         public array $unionItems = [],
     ) {
     }
