13cc3950cbf89cefb0005d61d39451ed8f907717
Antoine Bluchet <soyuka@users.noreply.github.com>
2026-03-20 10:11:10 +0100

    fix(doctrine): reset nested_properties_info for non-nested properties in FreeTextQueryFilter (#7850)
    Fixes #7845

---
+++ tests/Fixtures/TestBundle/Entity/FreeTextTag.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Tests\Fixtures\TestBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+class FreeTextTag
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $content;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+}
