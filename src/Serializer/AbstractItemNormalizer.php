a45c5e5a737c4a4e894d47814561bb46b9ea1557
soyuka <soyuka@users.noreply.github.com>
2026-03-25 15:52:08 +0100

    fix(serializer): handle nullable constructor params without default value
    | Q             | A
    | ------------- | ---
    | Branch?       | 4.2
    | Tickets       | Closes #7837
    | License       | MIT
    | Doc PR        | ∅
    
    Match Symfony's AbstractNormalizer behavior by passing null for nullable
    constructor parameters that don't have an explicit default value, unless
    REQUIRE_ALL_PROPERTIES is set.

--- src/Serializer/AbstractItemNormalizer.php
+++ src/Serializer/AbstractItemNormalizer.php
@@ -378,6 +378,8 @@ abstract class AbstractItemNormalizer extends AbstractObjectNormalizer
                     $params[] = $context[static::DEFAULT_CONSTRUCTOR_ARGUMENTS][$class][$key];
                 } elseif ($constructorParameter->isDefaultValueAvailable()) {
                     $params[] = $constructorParameter->getDefaultValue();
+                } elseif (!($context[self::REQUIRE_ALL_PROPERTIES] ?? $this->defaultContext[self::REQUIRE_ALL_PROPERTIES] ?? false) && $constructorParameter->hasType() && $constructorParameter->getType()->allowsNull()) {
+                    $params[] = null;
                 } else {
                     if (!isset($context['not_normalizable_value_exceptions'])) {
                         $missingConstructorArguments[] = $constructorParameter->name;
