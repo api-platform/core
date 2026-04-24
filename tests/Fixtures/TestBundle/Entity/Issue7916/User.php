2afd1e4264f2a24fd81eb32ddcef3c714d3570b3
Maxcastel <maxence.nord@gmail.com>
2026-04-24 16:11:39 +0200

    fix(metadata): issue reproduction tests #7916

---
+++ tests/Fixtures/TestBundle/Entity/Issue7916/User.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\Tests\Fixtures\TestBundle\Entity\Issue7916;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Plain Doctrine ORM entity (NOT marked #[ApiResource]).
+ * Used to test that nested property filters work on relations to non-ApiResource entities.
+ *
+ * @see https://github.com/api-platform/core/issues/7916
+ */
+#[ORM\Entity]
+#[ORM\Table(name: 'issue_7916_user')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $email = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
