418f42c274eed73bc13ba6aeb81bb73ff39dd452
Kévin Martins <kevin.martins@me.com>
2026-04-27 14:38:21 +0200

    refactor(symfony): remove static state from EntrypointAction (#7919)

--- src/Symfony/Tests/Action/EntrypointActionTest.php
+++ src/Symfony/Tests/Action/EntrypointActionTest.php
@@ -30,13 +30,84 @@ class EntrypointActionTest extends TestCase
     public function testGetEntrypointWithProviderProcessor(): void
     {
         $expected = new Entrypoint(new ResourceNameCollection(['dummies']));
+
         $resourceNameCollectionFactory = $this->createMock(ResourceNameCollectionFactoryInterface::class);
         $resourceNameCollectionFactory->method('create')->willReturn(new ResourceNameCollection(['dummies']));
+
         $provider = $this->createMock(ProviderInterface::class);
         $provider->expects($this->once())->method('provide')->willReturn($expected);
+
         $processor = $this->createMock(ProcessorInterface::class);
         $processor->expects($this->once())->method('process')->willReturnArgument(0);
+
         $entrypoint = new EntrypointAction($resourceNameCollectionFactory, $provider, $processor);
         $this->assertEquals($expected, $entrypoint(Request::create('/')));
+    }
+
+    public function testInvokeCachesResourceNameCollection(): void
+    {
+        $resourceNameCollectionFactory = $this->createMock(ResourceNameCollectionFactoryInterface::class);
+        $resourceNameCollectionFactory->expects($this->once())
+            ->method('create')
+            ->willReturn(new ResourceNameCollection(['Dummy']));
+
+        $provider = $this->createMock(ProviderInterface::class);
+        $processor = $this->createMock(ProcessorInterface::class);
+
+        $action = new EntrypointAction($resourceNameCollectionFactory, $provider, $processor);
+
+        $request = new Request();
+
+        $provider->expects($this->exactly(2))
+            ->method('provide')
+            ->willReturn(new Entrypoint(new ResourceNameCollection(['Dummy'])));
+
+        $processor->expects($this->exactly(2))
+            ->method('process');
+
+        $action($request);
+
+        // Test that second call does not call factory again (lazy-loading/caching)
+        $action($request);
+    }
+
+    /**
+     * This test ensures that instances are isolated and don't leak state.
+     * In Worker mode (FrankenPHP/Swoole), static properties would cause a state leak between instances.
+     */
+    public function testInstancesAreIsolated(): void
+    {
+        $processor = $this->createMock(ProcessorInterface::class);
+        $provider = $this->createMock(ProviderInterface::class);
+
+        // Instance 1: configured with ResourceA
+        $factory1 = $this->createMock(ResourceNameCollectionFactoryInterface::class);
+        $factory1->method('create')->willReturn(new ResourceNameCollection(['ResourceA']));
+        $action1 = new EntrypointAction($factory1, $provider, $processor);
+
+        // Instance 2: configured with ResourceB
+        $factory2 = $this->createMock(ResourceNameCollectionFactoryInterface::class);
+        $factory2->method('create')->willReturn(new ResourceNameCollection(['ResourceB']));
+        $action2 = new EntrypointAction($factory2, $provider, $processor);
+
+        $request = new Request();
+
+        // 1. Trigger action 1
+        $action1($request);
+        // 2. Trigger action 2 (if static were used, this would overwrite action 1's state)
+        $action2($request);
+
+        // Verification of isolation:
+        $this->assertEquals(
+            new ResourceNameCollection(['ResourceA']),
+            $action1->provide()->getResourceNameCollection(),
+            "Instance 1 was polluted by Instance 2 (likely due to a static property)"
+        );
+
+        $this->assertEquals(
+            new ResourceNameCollection(['ResourceB']),
+            $action2->provide()->getResourceNameCollection(),
+            "Instance 2 has incorrect state."
+        );
     }
 }
