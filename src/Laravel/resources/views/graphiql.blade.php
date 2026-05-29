ef40bcda66c13df1cb4373ef05a1333392576fda
Antoine Bluchet <soyuka@users.noreply.github.com>
2026-05-29 16:11:29 +0200

    fix(graphiql): migrate to v5 via esm.sh CDN (#8209)

--- src/Laravel/resources/views/graphiql.blade.php
+++ src/Laravel/resources/views/graphiql.blade.php
@@ -4,7 +4,24 @@
     <meta charset="UTF-8">
     <title>{{ config('api-platform.title') }} - API Platform</title>
 
-    <link rel="stylesheet" href="/vendor/api-platform/graphiql/graphiql.css">
+    <script type="importmap">
+    {
+        "imports": {
+            "react": "https://esm.sh/react@19.2.5",
+            "react/": "https://esm.sh/react@19.2.5/",
+            "react-dom": "https://esm.sh/react-dom@19.2.5",
+            "react-dom/": "https://esm.sh/react-dom@19.2.5/",
+            "graphql": "https://esm.sh/graphql@16.13.2",
+            "graphiql": "https://esm.sh/graphiql@5.2.2?standalone&external=react,react-dom,@graphiql/react,graphql",
+            "graphiql/": "https://esm.sh/graphiql@5.2.2/",
+            "@graphiql/react": "https://esm.sh/@graphiql/react@0.37.3?standalone&external=react,react-dom,graphql,@graphiql/toolkit,@emotion/is-prop-valid",
+            "@graphiql/toolkit": "https://esm.sh/@graphiql/toolkit@0.11.3?standalone&external=graphql",
+            "@emotion/is-prop-valid": "data:text/javascript,"
+        }
+    }
+    </script>
+
+    <link rel="stylesheet" href="https://esm.sh/graphiql@5/dist/style.css">
     <link rel="stylesheet" href="/vendor/api-platform/graphiql-style.css">
 
     <script id="graphiql-data" type="application/json">{!! Illuminate\Support\Js::encode($graphiql_data) !!}</script>
@@ -12,10 +29,7 @@
 
 <body>
 <div id="graphiql">Loading...</div>
-<script src="/vendor/api-platform/react/react.production.min.js"></script>
-<script src="/vendor/api-platform/react/react-dom.production.min.js"></script>
-<script src="/vendor/api-platform/graphiql/graphiql.min.js"></script>
-<script src="/vendor/api-platform/init-graphiql.js"></script>
+<script type="module" src="/vendor/api-platform/init-graphiql.js"></script>
 
 </body>
 </html>
--- src/Symfony/Bundle/Resources/public/init-graphiql.js
+++ src/Symfony/Bundle/Resources/public/init-graphiql.js
@@ -1,81 +1,54 @@
-var initParameters = {};
-var entrypoint = null;
+import 'graphiql/setup-workers/esm.sh';
+import { createElement } from 'react';
+import { createRoot } from 'react-dom/client';
+import { GraphiQL } from 'graphiql';
+import { createGraphiQLFetcher } from '@graphiql/toolkit';
 
-function onEditQuery(newQuery) {
-    initParameters.query = newQuery;
-    updateURL();
-}
+const data = JSON.parse(document.getElementById('graphiql-data').textContent);
 
-function onEditVariables(newVariables) {
-    initParameters.variables = newVariables;
-    updateURL();
-}
-
-function onEditOperationName(newOperationName) {
-    initParameters.operationName = newOperationName;
-    updateURL();
-}
-
-function updateURL() {
-    var newSearch = '?' + Object.keys(initParameters).filter(function (key) {
-        return Boolean(initParameters[key]);
-    }).map(function (key) {
-        return encodeURIComponent(key) + '=' + encodeURIComponent(initParameters[key]);
-    }).join('&');
-    history.replaceState(null, null, newSearch);
-}
-
-function graphQLFetcher(graphQLParams, {headers}) {
-    return fetch(entrypoint, {
-        method: 'post',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            ...headers
-        },
-        body: JSON.stringify(graphQLParams),
-        credentials: 'include'
-    }).then(function (response) {
-        return response.text();
-    }).then(function (responseBody) {
-        try {
-            return JSON.parse(responseBody);
-        } catch (error) {
-            return responseBody;
-        }
-    });
-}
-
-window.onload = function() {
-    var data = JSON.parse(document.getElementById('graphiql-data').innerText);
-    entrypoint = data.entrypoint;
-
-    var search = window.location.search;
-    search.substr(1).split('&').forEach(function (entry) {
-        var eq = entry.indexOf('=');
-        if (eq >= 0) {
-            initParameters[decodeURIComponent(entry.slice(0, eq))] = decodeURIComponent(entry.slice(eq + 1));
-        }
-    });
-
-    if (initParameters.variables) {
-        try {
-            initParameters.variables = JSON.stringify(JSON.parse(initParameters.variables), null, 2);
-        } catch (e) {
-            // Do nothing, we want to display the invalid JSON as a string, rather than present an error.
-        }
+const params = new URLSearchParams(window.location.search);
+const rawVariables = params.get('variables');
+let initialVariables;
+if (rawVariables) {
+    try {
+        initialVariables = JSON.stringify(JSON.parse(rawVariables), null, 2);
+    } catch {
+        initialVariables = rawVariables;
     }
-
-    ReactDOM.render(
-        React.createElement(GraphiQL, {
-            fetcher: graphQLFetcher,
-            query: initParameters.query,
-            variables: initParameters.variables,
-            operationName: initParameters.operationName,
-            onEditQuery: onEditQuery,
-            onEditVariables: onEditVariables,
-            onEditOperationName: onEditOperationName
-        }),
-        document.getElementById('graphiql')
-    );
 }
+
+const fetcher = createGraphiQLFetcher({
+    url: data.entrypoint,
+    fetch: (url, init) => fetch(url, { ...init, credentials: 'include' }),
+});
+
+function updateUrl({ query, variables, operationName }) {
+    const next = new URLSearchParams();
+    if (query) next.set('query', query);
+    if (variables) next.set('variables', variables);
+    if (operationName) next.set('operationName', operationName);
+    const search = next.toString();
+    window.history.replaceState(null, '', search ? `?${search}` : window.location.pathname);
+}
+
+function onTabChange({ tabs, activeTabIndex }) {
+    const tab = tabs[activeTabIndex];
+    if (!tab) {
+        return;
+    }
+    updateUrl({
+        query: tab.query,
+        variables: tab.variables,
+        operationName: tab.operationName,
+    });
+}
+
+createRoot(document.getElementById('graphiql')).render(
+    createElement(GraphiQL, {
+        fetcher,
+        initialQuery: params.get('query') ?? undefined,
+        initialVariables,
+        operationName: params.get('operationName') ?? undefined,
+        onTabChange,
+    }),
+);
