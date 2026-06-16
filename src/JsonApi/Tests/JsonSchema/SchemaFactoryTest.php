api-platform/core

commit 79edced67ccca1a7b80455dd94203501d9c4fa89

fix(json-schema): share invariable sub-schemas

src/JsonApi/JsonSchema/SchemaFactory.php
@@ -13,11 +13,13 @@ declare(strict_types=1);
 
 namespace ApiPlatform\JsonApi\JsonSchema;
 
+use ApiPlatform\JsonSchema\DefinitionNameFactory;
 use ApiPlatform\JsonSchema\DefinitionNameFactoryInterface;
 use ApiPlatform\JsonSchema\ResourceMetadataTrait;
 use ApiPlatform\JsonSchema\Schema;
 use ApiPlatform\JsonSchema\SchemaFactoryAwareInterface;
 use ApiPlatform\JsonSchema\SchemaFactoryInterface;
+use ApiPlatform\JsonSchema\SchemaUriPrefixTrait;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\Metadata\Property\Factory\PropertyMetadataFactoryInterface;
 use ApiPlatform\Metadata\Resource\Factory\ResourceMetadataCollectionFactoryInterface;
@@ -37,6 +39,7 @@ use Symfony\Component\TypeInfo\Type\ObjectType;
 final class SchemaFactory implements SchemaFactoryInterface, SchemaFactoryAwareInterface
 {
     use ResourceMetadataTrait;
+    use SchemaUriPrefixTrait;
 
     /**
      * As JSON:API recommends using [includes](https://jsonapi.org/format/#fetching-includes) instead of groups
@@ -44,6 +47,8 @@ final class SchemaFactory implements SchemaFactoryInterface, SchemaFactoryAwareI
      * a serializer context.
      */
     public const DISABLE_JSON_SCHEMA_SERIALIZER_GROUPS = 'disable_json_schema_serializer_groups';
+
+    private const COLLECTION_BASE_SCHEMA_NAME = 'JsonApiCollectionBaseSchema';
 
     private const LINKS_PROPS = [
         'type' => 'object',
@@ -119,8 +124,16 @@ final class SchemaFactory implements SchemaFactoryInterface, SchemaFactoryAwareI
         ],
     ];
 
-    public function __construct(private readonly SchemaFactoryInterface $schemaFactory, private readonly PropertyMetadataFactoryInterface $propertyMetadataFactory, ResourceClassResolverInterface $resourceClassResolver, ?ResourceMetadataCollectionFactoryInterface $resourceMetadataFactory = null, private readonly ?DefinitionNameFactoryInterface $definitionNameFactory = null)
+    /**
+     * @var array<string, bool>
+     */
+    private $builtSchema = [];
+
+    public function __construct(private readonly SchemaFactoryInterface $schemaFactory, private readonly PropertyMetadataFactoryInterface $propertyMetadataFactory, ResourceClassResolverInterface $resourceClassResolver, ?ResourceMetadataCollectionFactoryInterface $resourceMetadataFactory = null, private ?DefinitionNameFactoryInterface $definitionNameFactory = null)
     {
+        if (!$definitionNameFactory) {
+            $this->definitionNameFactory = new DefinitionNameFactory();
+        }
         if ($this->schemaFactory instanceof SchemaFactoryAwareInterface) {
             $this->schemaFactory->setSchemaFactory($this);
         }
@@ -136,59 +149,104 @@ final class SchemaFactory implements SchemaFactoryInterface, SchemaFactoryAwareI
         if ('jsonapi' !== $format) {
             return $this->schemaFactory->buildSchema($className, $format, $type, $operation, $schema, $serializerContext, $forceCollection);
         }
-        // We don't use the serializer context here as JSON:API doesn't leverage serializer groups for related resources.
-        // That is done by query parameter. @see https://jsonapi.org/format/#fetching-includes
-        $serializerContext ??= $this->getSerializerContext($operation ?? $this->findOperation($className, $type, $operation, $serializerContext, $format), $type);
-        $jsonApiSerializerContext = !($serializerContext[self::DISABLE_JSON_SCHEMA_SERIALIZER_GROUPS] ?? true) ? $serializerContext : [];
-        $schema = $this->schemaFactory->buildSchema($className, $format, $type, $operation, $schema, $jsonApiSerializerContext, $forceCollection);
 
-        if (($key = $schema->getRootDefinitionKey()) || ($key = $schema->getItemsDefinitionKey())) {
-            $definitions = $schema->getDefinitions();
-            $properties = $definitions[$key]['properties'] ?? [];
-
-            if (Error::class === $className && !isset($properties['errors'])) {
-                $definitions[$key]['properties'] = [
-                    'errors' => [
-                        'type' => 'object',
-                        'properties' => $properties,
-                    ],
-                ];
-
-                return $schema;
-            }
-
-            // Prevent reapplying
-            if (isset($properties['id'], $properties['type']) || isset($properties['data']) || isset($properties['errors'])) {
-                return $schema;
-            }
-
-            $definitions[$key]['properties'] = $this->buildDefinitionPropertiesSchema($key, $className, $format, $type, $operation, $schema, []);
-
-            if ($schema->getRootDefinitionKey()) {
-                return $schema;
-            }
+        if (!$this->isResourceClass($className)) {
+            $operation = null;
+            $inputOrOutputClass = null;
+            $serializerContext ??= [];
+        } else {
+            $operation = $this->findOperation($className, $type, $operation, $serializerContext, $format);
+            $inputOrOutputClass = $this->findOutputClass($className, $type, $operation, $serializerContext);
+            $serializerContext ??= $this->getSerializerContext($operation, $type);
         }
 
-        if (($schema['type'] ?? '') === 'array') {
-            // data
-            $items = $schema['items'];
-            unset($schema['items']);
+        if (null === $inputOrOutputClass) {
+            // input or output disabled
+            return $this->schemaFactory->buildSchema($className, $format, $type, $operation, $schema, $serializerContext, $forceCollection);
+        }
 
-            $schema['type'] = 'object';
-            $schema['properties'] = [
-                'links' => self::LINKS_PROPS,
-                'meta' => self::META_PROPS,
-                'data' => [
-                    'type' => 'array',
-                    'items' => $items,
-                ],
-            ];
-            $schema['required'] = [
-                'data',
-            ];
+        // We don't use the serializer context here as JSON:API doesn't leverage serializer groups for related resources.
+        // That is done by query parameter. @see https://jsonapi.org/format/#fetching-includes
+        $jsonApiSerializerContext = $serializerContext;
+        if (true === ($serializerContext[self::DISABLE_JSON_SCHEMA_SERIALIZER_GROUPS] ?? true) && $inputOrOutputClass === $className) {
+            unset($jsonApiSerializerContext['groups']);
+        }
+
+        $schema = $this->schemaFactory->buildSchema($className, 'json', $type, $operation, $schema, $jsonApiSerializerContext, $forceCollection);
+        $definitionName = $this->definitionNameFactory->create($inputOrOutputClass, $format, $className, $operation, $jsonApiSerializerContext);
+        $prefix = $this->getSchemaUriPrefix($schema->getVersion());
+        $definitions = $schema->getDefinitions();
+        $collectionKey = $schema->getItemsDefinitionKey();
+
+        // Already computed
+        if (!$collectionKey && isset($definitions[$definitionName])) {
+            $schema['$ref'] = $prefix.$definitionName;
 
             return $schema;
         }
+
+        $key = $schema->getRootDefinitionKey() ?? $collectionKey;
+        $properties = $definitions[$definitionName]['properties'] ?? [];
+
+        if (Error::class === $className && !isset($properties['errors'])) {
+            $definitions[$definitionName]['properties'] = [
+                'errors' => [
+                    'type' => 'array',
+                    'items' => [
+                        'allOf' => [
+                            ['$ref' => $prefix.$key],
+                            ['type' => 'object', 'properties' => ['source' => ['type' => 'object'], 'status' => ['type' => 'string']]],
+                        ],
+                    ],
+                ],
+            ];
+
+            $schema['$ref'] = $prefix.$definitionName;
+
+            return $schema;
+        }
+
+        if (!$collectionKey) {
+            $definitions[$definitionName]['properties'] = $this->buildDefinitionPropertiesSchema($key, $className, $format, $type, $operation, $schema, []);
+            $schema['$ref'] = $prefix.$definitionName;
+
+            return $schema;
+        }
+
+        if (($schema['type'] ?? '') !== 'array') {
+            return $schema;
+        }
+
+        if (!isset($definitions[self::COLLECTION_BASE_SCHEMA_NAME])) {
+            $definitions[self::COLLECTION_BASE_SCHEMA_NAME] = [
+                'type' => 'object',
+                'properties' => [
+                    'links' => self::LINKS_PROPS,
+                    'meta' => self::META_PROPS,
+                    'data' => [
+                        'type' => 'array',
+                    ],
+                ],
+                'required' => ['data'],
+            ];
+        }
+
+        unset($schema['items']);
+        unset($schema['type']);
+
+        $properties = $this->buildDefinitionPropertiesSchema($key, $className, $format, $type, $operation, $schema, []);
+        $properties['data']['properties']['attributes']['$ref'] = $prefix.$key;
+
+        $schema['description'] = "$definitionName collection.";
+        $schema['allOf'] = [
+            ['$ref' => $prefix.self::COLLECTION_BASE_SCHEMA_NAME],
+            ['type' => 'object', 'properties' => [
+                'data' => [
+                    'type' => 'array',
+                    'items' => $properties['data'],
+                ],
+            ]],
+        ];
 
         return $schema;
     }
@@ -217,12 +275,27 @@ final class SchemaFactory implements SchemaFactoryInterface, SchemaFactoryAwareI
                         continue;
                     }
 
-                    $operation = $this->findOperation($relatedClassName, $type, $operation, $serializerContext);
+                    $operation = $this->findOperation($relatedClassName, $type, null, $serializerContext);
                     $inputOrOutputClass = $this->findOutputClass($relatedClassName, $type, $operation, $serializerContext);
                     $serializerContext ??= $this->getSerializerContext($operation, $type);
                     $definitionName = $this->definitionNameFactory->create($relatedClassName, $format, $inputOrOutputClass, $operation, $serializerContext);
-                    $ref = Schema::VERSION_OPENAPI === $schema->getVersion() ? '#/components/schemas/'.$definitionName : '#/definitions/'.$definitionName;
-                    $refs[$ref] = '$ref';
+
+                    // to avoid recursion
+                    if ($this->builtSchema[$definitionName] ?? false) {
+                        $refs[$this->getSchemaUriPrefix($schema->getVersion()).$definitionName] = '$ref';
+                        continue;
+                    }
+
+                    if (!isset($definitions[$definitionName])) {
+                        $this->builtSchema[$definitionName] = true;
+                        $subSchema = new Schema($schema->getVersion());
+                        $subSchema->setDefinitions($schema->getDefinitions());
+                        $subSchema = $this->buildSchema($relatedClassName, $format, $type, $operation, $subSchema, $serializerContext + [self::FORCE_SUBSCHEMA => true], false);
+                        $schema->setDefinitions($subSchema->getDefinitions());
+                        $definitions = $schema->getDefinitions();
+                    }
+
+                    $refs[$this->getSchemaUriPrefix($schema->getVersion()).$definitionName] = '$ref';
                 }
                 $relatedDefinitions[$propertyName] = array_flip($refs);
                 if ($isOne) {
@@ -235,15 +308,18 @@ final class SchemaFactory implements SchemaFactoryInterface, SchemaFactoryAwareI
                 ];
                 continue;
             }
+
             if ('id' === $propertyName) {
+                // should probably be renamed "lid" and moved to the above node
                 $attributes['_id'] = $property;
                 continue;
             }
             $attributes[$propertyName] = $property;
         }
 
+        $currentRef = $this->getSchemaUriPrefix($schema->getVersion()).$schema->getRootDefinitionKey();
         $replacement = self::PROPERTY_PROPS;
-        $replacement['attributes']['properties'] = $attributes;
+        $replacement['attributes'] = ['$ref' => $currentRef];
 
         $included = [];
         if (\count($relationships) > 0) {
@@ -264,19 +340,6 @@ final class SchemaFactory implements SchemaFactoryInterface, SchemaFactoryAwareI
                     ],
                 ],
             ];
-        }
-
-        if ($required = $definitions[$key]['required'] ?? null) {
-            foreach ($required as $require) {
-                if (isset($replacement['attributes']['properties'][$require])) {
-                    $replacement['attributes']['required'][] = $require;
-                    continue;
-                }
-                if (isset($relationships[$require])) {
-                    $replacement['relationships']['required'][] = $require;
-                }
-            }
-            unset($definitions[$key]['required']);
         }
 
         return [

commit 454a259c8d8fd8e7ce0b8252fec1f40ee68de655

fix(jsonapi): exclude relations from openapi attributes schema
Relation properties were documented twice in the generated JSON:API
OpenAPI schema: correctly under `relationships`, and incorrectly as
full nested objects under `attributes`. The `attributes` variant never
appears in a real JSON:API response.

Regression from 79edced67 which replaced the relation-excluding
attributes array with a blanket `$ref` to the full resource schema.
Restore the built `$attributes` array (relations skipped, `id` renamed
to `_id`) and drop the collection-path `$ref` override that re-leaked
relations.

Fixes #8308

src/JsonApi/JsonSchema/SchemaFactory.php
@@ -258,7 +258,6 @@ final class SchemaFactory implements SchemaFactoryInterface, SchemaFactoryAwareI
         unset($schema['type']);
 
         $properties = $this->buildDefinitionPropertiesSchema($key, $className, $format, $type, $operation, $schema, []);
-        $properties['data']['properties']['attributes']['$ref'] = $prefix.$key;
 
         $properties['data'] = [
             'type' => 'array',
@@ -347,9 +346,8 @@ final class SchemaFactory implements SchemaFactoryInterface, SchemaFactoryAwareI
             $attributes[$propertyName] = $property;
         }
 
-        $currentRef = $this->getSchemaUriPrefix($schema->getVersion()).$schema->getRootDefinitionKey();
         $replacement = self::PROPERTY_PROPS;
-        $replacement['attributes'] = ['$ref' => $currentRef];
+        $replacement['attributes']['properties'] = $attributes;
 
         $included = [];
         if (\count($relationships) > 0) {

src/JsonApi/Tests/JsonSchema/SchemaFactoryTest.php
@@ -15,9 +15,11 @@ namespace ApiPlatform\JsonApi\Tests\JsonSchema;
 
 use ApiPlatform\JsonApi\JsonSchema\SchemaFactory;
 use ApiPlatform\JsonApi\Tests\Fixtures\Dummy;
+use ApiPlatform\JsonApi\Tests\Fixtures\RelatedDummy;
 use ApiPlatform\JsonSchema\DefinitionNameFactory;
 use ApiPlatform\JsonSchema\Schema;
 use ApiPlatform\JsonSchema\SchemaFactory as BaseSchemaFactory;
+use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
@@ -31,7 +33,9 @@ use ApiPlatform\Metadata\Resource\Factory\ResourceMetadataCollectionFactoryInter
 use ApiPlatform\Metadata\Resource\ResourceMetadataCollection;
 use ApiPlatform\Metadata\ResourceClassResolverInterface;
 use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
 use Prophecy\PhpUnit\ProphecyTrait;
+use Symfony\Component\TypeInfo\Type;
 
 class SchemaFactoryTest extends TestCase
 {
@@ -112,7 +116,8 @@ class SchemaFactoryTest extends TestCase
                         'type' => 'string',
                     ],
                     'attributes' => [
-                        '$ref' => '#/definitions/Dummy',
+                        'type' => 'object',
+                        'properties' => [],
                     ],
                 ],
                 'required' => [
@@ -155,7 +160,8 @@ class SchemaFactoryTest extends TestCase
         $this->assertArrayHasKey('data', $objectSchema['properties']);
 
         $this->assertArrayHasKey('items', $objectSchema['properties']['data']);
-        $this->assertArrayHasKey('$ref', $objectSchema['properties']['data']['items']['properties']['attributes']);
+        $this->assertArrayNotHasKey('$ref', $objectSchema['properties']['data']['items']['properties']['attributes']);
+        $this->assertSame('object', $objectSchema['properties']['data']['items']['properties']['attributes']['type']);
 
         $properties = $objectSchema['properties'];
         $this->assertArrayHasKey('data', $properties);
@@ -198,5 +204,90 @@ class SchemaFactoryTest extends TestCase
 
         $data = $definitions[$rootDefinitionKey]['properties']['data'];
         $this->assertSame(['type', 'id'], $data['required']);
+    }
+
+    public function testRelationIsExcludedFromAttributes(): void
+    {
+        $schemaFactory = $this->buildSchemaFactoryWithRelation();
+        $resultSchema = $schemaFactory->buildSchema(Dummy::class, 'jsonapi');
+
+        $definitions = $resultSchema->getDefinitions();
+        $rootDefinitionKey = $resultSchema->getRootDefinitionKey();
+        $dataProperties = $definitions[$rootDefinitionKey]['properties']['data']['properties'];
+
+        $this->assertArrayHasKey('attributes', $dataProperties);
+        $this->assertArrayNotHasKey('$ref', $dataProperties['attributes']);
+        $this->assertSame('object', $dataProperties['attributes']['type']);
+
+        $attributes = $dataProperties['attributes']['properties'];
+        $this->assertArrayHasKey('name', $attributes);
+        $this->assertArrayNotHasKey('relatedDummy', $attributes, 'relations must not be documented as attributes');
+
+        $this->assertArrayHasKey('_id', $attributes, 'id is exposed as _id in the JSON:API attributes');
+        $this->assertArrayNotHasKey('id', $attributes);
+
+        $this->assertArrayHasKey('relationships', $dataProperties);
+        $this->assertArrayHasKey('relatedDummy', $dataProperties['relationships']['properties']);
+    }
+
+    private function buildSchemaFactoryWithRelation(): SchemaFactory
+    {
+        $dummyOperation = (new Get())->withName('get')->withShortName('Dummy');
+        $relatedOperation = (new Get())->withName('get')->withShortName('RelatedDummy');
+
+        $resourceMetadataFactory = $this->prophesize(ResourceMetadataCollectionFactoryInterface::class);
+        $resourceMetadataFactory->create(Dummy::class)->willReturn(
+            new ResourceMetadataCollection(Dummy::class, [
+                (new ApiResource())->withOperations(new Operations(['get' => $dummyOperation])),
+            ])
+        );
+        $resourceMetadataFactory->create(RelatedDummy::class)->willReturn(
+            new ResourceMetadataCollection(RelatedDummy::class, [
+                (new ApiResource())->withOperations(new Operations(['get' => $relatedOperation])),
+            ])
+        );
+
+        $propertyNameCollectionFactory = $this->prophesize(PropertyNameCollectionFactoryInterface::class);
+        $propertyNameCollectionFactory->create(Dummy::class, Argument::type('array'))->willReturn(new PropertyNameCollection(['id', 'name', 'relatedDummy']));
+        $propertyNameCollectionFactory->create(RelatedDummy::class, Argument::type('array'))->willReturn(new PropertyNameCollection(['id', 'name']));
+
+        $propertyMetadataFactory = $this->prophesize(PropertyMetadataFactoryInterface::class);
+        $propertyMetadataFactory->create(Dummy::class, 'id', Argument::type('array'))->willReturn(
+            (new ApiProperty())->withNativeType(Type::int())->withReadable(true)->withSchema(['type' => 'integer'])
+        );
+        $propertyMetadataFactory->create(Dummy::class, 'name', Argument::type('array'))->willReturn(
+            (new ApiProperty())->withNativeType(Type::string())->withReadable(true)->withSchema(['type' => 'string'])
+        );
+        $propertyMetadataFactory->create(Dummy::class, 'relatedDummy', Argument::type('array'))->willReturn(
+            (new ApiProperty())->withNativeType(Type::object(RelatedDummy::class))->withReadable(true)->withSchema(['type' => Schema::UNKNOWN_TYPE])
+        );
+        $propertyMetadataFactory->create(RelatedDummy::class, 'id', Argument::type('array'))->willReturn(
+            (new ApiProperty())->withNativeType(Type::int())->withReadable(true)->withSchema(['type' => 'integer'])
+        );
+        $propertyMetadataFactory->create(RelatedDummy::class, 'name', Argument::type('array'))->willReturn(
+            (new ApiProperty())->withNativeType(Type::string())->withReadable(true)->withSchema(['type' => 'string'])
+        );
+
+        $resourceClassResolver = $this->prophesize(ResourceClassResolverInterface::class);
+        $resourceClassResolver->isResourceClass(Dummy::class)->willReturn(true);
+        $resourceClassResolver->isResourceClass(RelatedDummy::class)->willReturn(true);
+
+        $definitionNameFactory = new DefinitionNameFactory(null);
+
+        $baseSchemaFactory = new BaseSchemaFactory(
+            resourceMetadataFactory: $resourceMetadataFactory->reveal(),
+            propertyNameCollectionFactory: $propertyNameCollectionFactory->reveal(),
+            propertyMetadataFactory: $propertyMetadataFactory->reveal(),
+            resourceClassResolver: $resourceClassResolver->reveal(),
+            definitionNameFactory: $definitionNameFactory,
+        );
+
+        return new SchemaFactory(
+            schemaFactory: $baseSchemaFactory,
+            propertyMetadataFactory: $propertyMetadataFactory->reveal(),
+            resourceClassResolver: $resourceClassResolver->reveal(),
+            resourceMetadataFactory: $resourceMetadataFactory->reveal(),
+            definitionNameFactory: $definitionNameFactory,
+        );
     }
 }

tests/Functional/JsonSchema/JsonApiJsonSchemaTest.php
@@ -59,28 +59,19 @@ class JsonApiJsonSchemaTest extends ApiTestCase
     {
         $speciesSchema = $this->schemaFactory->buildSchema(Issue6317::class, 'jsonapi', Schema::TYPE_OUTPUT);
         $this->assertEquals('#/definitions/Issue6317.jsonapi', $speciesSchema['$ref']);
-        $this->assertEquals([
-            'properties' => [
-                'data' => [
-                    'type' => 'object',
-                    'properties' => [
-                        'id' => [
-                            'type' => 'string',
-                        ],
-                        'type' => [
-                            'type' => 'string',
-                        ],
-                        'attributes' => [
-                            '$ref' => '#/definitions/Issue6317',
-                        ],
-                    ],
-                    'required' => [
-                        'type',
-                        'id',
-                    ],
-                ],
-            ],
-        ], $speciesSchema['definitions']['Issue6317.jsonapi']);
+        $data = $speciesSchema['definitions']['Issue6317.jsonapi']['properties']['data'];
+        $this->assertSame('object', $data['type']);
+        $this->assertSame(['type' => 'string'], $data['properties']['id']);
+        $this->assertSame(['type' => 'string'], $data['properties']['type']);
+        $this->assertSame(['type', 'id'], $data['required']);
+
+        $attributes = $data['properties']['attributes'];
+        $this->assertArrayNotHasKey('$ref', $attributes);
+        $this->assertSame('object', $attributes['type']);
+        $this->assertEqualsCanonicalizing(
+            ['name', 'value', '_id', 'ordinal', 'cardinal'],
+            array_keys($attributes['properties'])
+        );
     }
 
     public function testJsonApiIncludesSchemaForQuestion(): void
@@ -150,7 +141,7 @@ class JsonApiJsonSchemaTest extends ApiTestCase
                                             'type' => 'string',
                                         ],
                                         'attributes' => [
-                                            '$ref' => '#/definitions/Species',
+                                            'type' => 'object',
                                         ],
                                     ],
                                     'required' => [