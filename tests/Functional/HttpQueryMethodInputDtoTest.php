f96d0bd67a31995ad5e157e12568653bda678be5
Mislav Jakopović <mislavjakopovic6@gmail.com>
2026-09-16 12:31:55 +0200

    chore: allow phpunit ^13.0

--- tests/Functional/HttpQueryMethodInputDtoTest.php
+++ tests/Functional/HttpQueryMethodInputDtoTest.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 
 namespace ApiPlatform\Tests\Functional;
 
-use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use ApiPlatform\Test\ApiTestCase;
 use ApiPlatform\Tests\Fixtures\TestBundle\ApiResource\PostInputDtoQueryParameterIsolation\PostInputDtoQueryParameterIsolation;
 use ApiPlatform\Tests\Fixtures\TestBundle\Entity\QueryInputDummy;
 use ApiPlatform\Tests\RecreateSchemaTrait;
--- tests/Functional/Security/SecurityFromContainerParameterTest.php
+++ tests/Functional/Security/SecurityFromContainerParameterTest.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 
 namespace ApiPlatform\Tests\Functional\Security;
 
-use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use ApiPlatform\Test\ApiTestCase;
 use ApiPlatform\Tests\Fixtures\TestBundle\Model\SecurityFromParameter;
 use ApiPlatform\Tests\SetupClassResourcesTrait;
 use Symfony\Component\Security\Core\User\InMemoryUser;
